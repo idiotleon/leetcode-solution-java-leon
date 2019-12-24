6de584e8b2f18c881fb8a3e6355dada6aef389bd
Leon <yanglyu.leon.7@gmail.com>
2019-12-23 17:20:41 -0800

    209: Minimum Size Subarray Sum

---
+++ src/main/java/com/idiotleon/java/lc209/Solution.java
@@ -0,0 +1,44 @@
+/**
+ * Given an array of n positive integers and a positive integer s, find the
+ * minimal length of a contiguous subarray of which the sum ≥ s. If there isn't
+ * one, return 0 instead.
+ * 
+ * Example: Input: s = 7, nums = [2,3,1,2,4,3] Output: 2 Explanation: the
+ * subarray [4,3] has the minimal length under the problem constraint. Follow
+ * up: If you have figured out the O(n) solution, try coding another solution of
+ * which the time complexity is O(n log n).
+ * 
+ * https://leetcode.com/problems/minimum-size-subarray-sum/
+ */
+package com.idiotleon.java.lc209;
+
+import java.lang.Math;
+
+class Solution {
+    /**
+     * #SlidingWindow
+     */
+    public int minSubArrayLen(int s, int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+
+        int sum = 0, count = 0, ans = Integer.MAX_VALUE;
+        for (int j = 0, i = 0; j < nums.length; j++) {
+            sum += nums[j];
+            count++;
+            if (sum >= s) {
+                while (i < j && sum - nums[i] >= s) {
+                    sum -= nums[i];
+                    count--;
+                    i++;
+                }
+                ans = Math.min(ans, count);
+            }
+        }
+
+        if (sum < s)
+            return 0;
+
+        return ans;
+    }
+}
---
+++ src/test/java/com/idiotleon/java/lc209/SolutionTests.java
@@ -0,0 +1,15 @@
+package com.idiotleon.java.lc209;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SolutionTests {
+    private final Solution solution = new Solution();
+
+    @DisplayName("The very 1st test case")
+    @Test
+    void test() {
+        assertEquals(2, solution.minSubArrayLen(7, new int[] { 2, 3, 1, 2, 4, 3 }));
+    }
+}
