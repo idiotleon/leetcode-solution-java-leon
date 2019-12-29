3096cdfc4fb1f5e4aa710bac0d870ec95ed05a16
Leon <yanglyu.leon.7@gmail.com>
2019-12-29 12:17:31 -0800

    Search in a Rotated Sorted Array i&ii

---
+++ src/main/java/com/idiotleon/java/lc0033/SolutionApproachBinarySearch.java
@@ -0,0 +1,44 @@
+/**
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+ * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+ * You are given a target value to search. If found in the array return its index, otherwise return -1.
+ * You may assume no duplicate exists in the array.
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ * Example 1:
+ * Input: nums = [4,5,6,7,0,1,2], target = 0
+ * Output: 4
+ * 
+ * Example 2:
+ * Input: nums = [4,5,6,7,0,1,2], target = 3
+ * Output: -1
+ */
+package com.idiotleon.java.lc0033;
+
+class SolutionApproachBinarySearch {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0)
+            return -1;
+
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (target == nums[mid])
+                return mid;
+            else if (nums[left] <= nums[mid]) {
+                if (target >= nums[left] && target <= nums[mid])
+                    right = mid;
+                else
+                    left = mid + 1;
+            } else {
+                if (target >= nums[mid] && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid;
+            }
+        }
+
+        return nums[left] == target ? left : -1;
+    }
+}
