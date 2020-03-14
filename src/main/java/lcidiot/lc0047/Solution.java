6d51e4b09cea9b0acb4cf09f9f5b331ad6f8257e
Leon <yanglyu.leon.7@gmail.com>
2020-03-14 15:29:54 -0700

    lc0047: Permutations II

---
+++ src/main/java/lcidiot/lc0047/Solution.java
@@ -0,0 +1,38 @@
+/**
+ * https://leetcode.com/problems/permutations-ii/
+ */
+
+package main.java.lcidiot.lc0047;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution{
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        dfs(ans, new ArrayList<Integer>(), nums, new boolean[nums.length]);
+        return ans;
+    }
+    
+    private void dfs(List<List<Integer>> ans, 
+                     List<Integer> intermediate, 
+                     int[] nums, 
+                     boolean[] visited){
+        
+        if(intermediate.size() == nums.length){
+            ans.add(new ArrayList(intermediate));
+            return;
+        }
+        
+        for(int i = 0; i < nums.length; i++){
+            if(visited[i] || i > 0 && nums[i] == nums[i - 1] && !visited[i - 1]) continue;
+            visited[i] = true;
+            intermediate.add(nums[i]);
+            dfs(ans, intermediate, nums, visited);
+            visited[i] = false;
+            intermediate.remove(intermediate.size() - 1);
+        }
+    }
+}
