8a7c107559a38cc44e4eef62cc2932430bf2fb57
Leon <yanglyu.leon.7@gmail.com>
2020-03-13 23:25:10 -0700

    lc1110: Delete Nodes and Return Forest

---
+++ src/main/java/lcidiot/lc1110/SolutionApproachRecursion.java
@@ -0,0 +1,37 @@
+/**
+ * https://leetcode.com/problems/delete-nodes-and-return-forest/
+ */
+
+package lcidiot.lc1110;
+
+import lcidiot.utils.TreeNode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+
+class Solution{
+    private Set<Integer> toDeleteSet;
+    private List<TreeNode> res;
+    
+    public List<TreeNode> delNodes(TreeNode root, int[] toDelete) {
+        toDeleteSet = new HashSet<>();
+        res = new ArrayList<>();
+        for(int i : toDelete) toDeleteSet.add(i);
+        helper(root, true);
+        return res;
+    }
+    
+    private TreeNode helper(TreeNode node, boolean isRoot){
+        if(node == null) return null;
+        
+        boolean deleted = toDeleteSet.contains(node.val);
+        if(isRoot && !deleted) res.add(node);
+        node.left = helper(node.left, deleted);
+        node.right = helper(node.right, deleted);
+        
+        return deleted ? null : node;
+    }
+}
--- src/main/java/lcidiot/utils/ListNode.java
+++ src/main/java/lcidiot/utils/ListNode.java
@@ -3,6 +3,7 @@ package lcidiot.utils;
 public class ListNode{
     public int val;
     public ListNode next;
+    
     public ListNode(int x){
         val = x;
     }
