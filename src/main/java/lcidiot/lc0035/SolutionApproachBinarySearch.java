c33b3b858b09cd47745feae952f879c3f5fb5e90
Leon <yanglyu.leon.7@gmail.com>
2020-03-14 09:46:59 -0700

    lc0035: Search Insertion Position

---
+++ src/main/java/lcidiot/lc0035/SolutionApproachBinarySearch.java
@@ -0,0 +1,22 @@
+/**
+ * https://leetcode.com/problems/search-insert-position/
+ */
+
+package lcidiot.lc0035;
+
+class Solution{
+    public int searchInsert(int[] nums, int target) {
+        if(nums == null || nums.length == 0) return 0;
+        
+        int left = 0, right = nums.length;
+        
+        while(left < right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target) return mid;
+            else if(target < nums[mid]) right = mid;
+            else left = mid + 1;
+        }
+        
+        return left;
+    }
+}
