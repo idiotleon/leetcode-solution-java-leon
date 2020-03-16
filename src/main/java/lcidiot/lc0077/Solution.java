ed409feb21b5865713db07001a84461b979831da
Leon <yanglyu.leon.7@gmail.com>
2020-03-16 16:26:02 -0700

    lc0077: Combinations

---
+++ src/main/java/lcidiot/lc0077/Solution.java
@@ -0,0 +1,33 @@
+/**
+ * https://leetcode.com/problems/combinations/
+ */
+
+package main.java.lcidiot.lc0077;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        dfs(ans, new ArrayList<Integer>(), 1, n, k);
+        return ans;
+    }
+    
+    private void dfs(List<List<Integer>> ans, 
+                     List<Integer> intermediate, 
+                     int start, 
+                     int n,
+                     int k){
+        if(k == 0){
+            ans.add(new ArrayList<Integer>(intermediate));
+            return;
+        }
+        
+        for(int i = start; i <= n; i++){
+            intermediate.add(i);
+            dfs(ans, intermediate, i + 1, n, k-1);
+            intermediate.remove(intermediate.size() - 1);
+        }
+    }
+}
