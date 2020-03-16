7543985f7a3b6ec9f6cb0ed4baaee9332bf1a869
Leon <yanglyu.leon.7@gmail.com>
2020-03-16 15:53:50 -0700

    lc0071: Simplify Path

---
+++ src/main/java/lcidiot/lc0071/Solution.java
@@ -0,0 +1,40 @@
+/**
+ * https://leetcode.com/problems/simplify-path/
+ */
+
+package main.java.lcidiot.lc0071;
+
+import java.util.Stack;
+
+class Solution {
+    public String simplifyPath(String path) {
+        if(path == null || path.length() == 0) return path;
+        
+        String[] splitted = path.split("/", -1);
+        Stack<String> stack = new Stack<String>();
+        
+        for(int i = 0; i < splitted.length; i++){
+            if(splitted[i].trim().length() == 0)
+                continue;
+            
+            if(splitted[i].equals("..")){
+                if(!stack.isEmpty()) stack.pop();
+                
+                continue;
+            }
+            
+            if(!splitted[i].equals("."))
+                stack.push(splitted[i]);
+        }
+        
+        if(stack.isEmpty()) return "/";
+        
+        String output = "";
+        while(!stack.isEmpty()){
+            String t = stack.pop();
+            output = "/" + t + output;
+        }
+        
+        return output;
+    }
+}
