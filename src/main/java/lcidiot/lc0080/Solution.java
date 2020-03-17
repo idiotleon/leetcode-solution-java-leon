eac6fdff4eeb3a41808ffc5531f4b6c0b9ec22d8
Leon <yanglyu.leon.7@gmail.com>
2020-03-16 17:16:10 -0700

    lc0080: Remove Duplicates from Sorted Array II

---
+++ src/main/java/lcidiot/lc0080/Solution.java
@@ -0,0 +1,15 @@
+/**
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+ */
+package main.java.lcidiot.lc0080;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        for(int num : nums)
+            if(i < 2 || num > nums[i - 2])
+                nums[i++] = num;
+        
+        return i;
+    }
+}
