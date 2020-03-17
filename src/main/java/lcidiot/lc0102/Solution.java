c059eda3054882d750f7a68222705b75bd054aff
Leon <yanglyu.leon.7@gmail.com>
2020-03-17 14:18:31 -0700

    lc0102: Binary Tree Level Order Traversal

---
+++ src/main/java/lcidiot/lc0102/Solution.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package main.java.lcidiot.lc0102;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import main.java.lcidiot.utils.TreeNode;
+
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        if(root == null) return ans;
+        
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.add(root);
+        List<Integer> intermediate = new ArrayList<Integer>();
+        
+        while(!queue.isEmpty()){
+            intermediate.clear();
+            int size = queue.size();
+            for(int i = 0; i < size; i++){
+                TreeNode cur = queue.poll();
+                intermediate.add(cur.val);
+                if(cur.left != null) queue.add(cur.left);
+                if(cur.right != null) queue.add(cur.right);
+            }
+            
+            ans.add(new ArrayList<Integer>(intermediate));
+        }
+        
+        return ans;
+    }
+}
