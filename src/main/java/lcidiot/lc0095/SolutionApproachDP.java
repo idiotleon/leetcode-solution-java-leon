16935022b682462790d24c772741fb6b627a0b5f
Leon <yanglyu.leon.7@gmail.com>
2020-03-17 11:34:31 -0700

    lc0095: Unique Binary Search Trees II

---
+++ src/main/java/lcidiot/lc0095/SolutionApproachDP.java
@@ -0,0 +1,43 @@
+/**
+ * https://leetcode.com/problems/unique-binary-search-trees-ii/
+ * 
+ * https://leetcode.com/problems/unique-binary-search-trees-ii/discuss/31493/Java-Solution-with-DP
+ */
+package main.java.lcidiot.lc0095;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import main.java.lcidiot.utils.TreeNode;
+
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        List<TreeNode>[] result = new List[n + 1];
+        result[0] = new ArrayList<TreeNode>();
+        if(n == 0) return result[0];
+        
+        result[0].add(null);
+        for(int len = 1; len <= n; len++){
+            result[len] = new ArrayList<TreeNode>();
+            for(int j = 0; j < len; j++)
+                for(TreeNode nodeLeft : result[j])
+                    for(TreeNode nodeRight : result[len - j - 1]){
+                        TreeNode node = new TreeNode(j + 1);
+                        node.left = nodeLeft;
+                        node.right = clone(nodeRight, j + 1);
+                        result[len].add(node);
+                    }
+        }
+        
+        return result[n];
+    }
+    
+    private static TreeNode clone(TreeNode n, int offset){
+        if(n == null) return null;
+        
+        TreeNode node = new TreeNode(n.val + offset);
+        node.left = clone(n.left, offset);
+        node.right = clone(n.right, offset);
+        return node;
+    }
+}
---
+++ src/main/java/lcidiot/lc0095/SolutionApproachRecursion.java
@@ -0,0 +1,35 @@
+/**
+ * https://leetcode.com/problems/unique-binary-search-trees-ii/
+ */
+package main.java.lcidiot.lc0095;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import main.java.lcidiot.utils.TreeNode;
+
+class SolutionApproachRecursion {
+    public List<TreeNode> generateTrees(int n) {
+        if(n == 0) return new ArrayList<TreeNode>();
+        return generateTrees(1, n);
+    }
+    
+    private List<TreeNode> generateTrees(int start, int end){
+        List<TreeNode> list = new ArrayList<TreeNode>();
+        if(start > end) list.add(null);
+        
+        for(int idx = start; idx <= end; idx++){
+            List<TreeNode> leftList = generateTrees(start, idx - 1);
+            List<TreeNode> rightList = generateTrees(idx + 1, end);
+            for(TreeNode left : leftList)
+                for(TreeNode right : rightList){
+                    TreeNode root = new TreeNode(idx);
+                    root.left = left;
+                    root.right = right;
+                    list.add(root);
+                }
+        }
+        
+        return list;
+    }
+}
