c097048214ee1c570303c2bd616cd21563b7eb40
Leon <yanglyu.leon.7@gmail.com>
2020-03-18 14:55:06 -0700

    lc0116 Populating Next Right Pointers in Each Node

---
+++ src/main/java/lcidiot/utils/Node.java
@@ -0,0 +1,12 @@
+package main.java.lcidiot.utils;
+
+public class Node{
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+    
+    public Node(int x){
+        val = x;
+    }
+}
