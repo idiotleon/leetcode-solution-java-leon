e3fa7c25d7b23351577b6a53464c2602677c0f6d
Leon <yanglyu.leon.7@gmail.com>
2020-03-17 19:32:55 -0700

    lc0105: Construct Binary Tree from Preorder and Inorder Traversal

---
+++ src/main/java/lcidiot/lc0105/Solution.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package main.java.lcidiot.lc0105;
+
+import java.util.HashMap;
+
+import main.java.lcidiot.utils.TreeNode;
+
+class Solution {
+    private int preIdx = 0;
+    private int[] preorder;
+    private HashMap<Integer, Integer> idxMap = new HashMap<Integer, Integer>();
+    
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        this.preorder = preorder;
+        
+        for(int i = 0; i < inorder.length; i++)
+            idxMap.put(inorder[i], i);
+        
+        return helper(0, inorder.length);
+    }
+    
+    private TreeNode helper(int inorderLeft, int inorderRight){
+        if(inorderLeft == inorderRight) return null;
+        
+        int rootVal = this.preorder[preIdx];
+        TreeNode root = new TreeNode(rootVal);
+        int index = idxMap.get(rootVal);
+        
+        preIdx++;
+        root.left = helper(inorderLeft, index);
+        root.right = helper(index + 1, inorderRight);
+        
+        return root;
+    }
+}
