927de1a9a5c658fa4f3eb9d69e42b65c5b4da9e1
Leon <yanglyu.leon.7@gmail.com>
2020-03-19 14:28:01 -0700

    lc0449: Serialize and Deserialize BST

---
+++ src/main/java/lcidiot/lc0449/SolutionApproachBinaryTree.java
@@ -0,0 +1,45 @@
+/**
+ * https://leetcode.com/problems/serialize-and-deserialize-bst/
+ */
+package main.java.lcidiot.lc0449;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import main.java.lcidiot.utils.TreeNode;
+
+public class SolutionApproachBinaryTree {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder builder = new StringBuilder();
+        serialize(root, builder);
+        return builder.toString();
+    }
+    
+    private void serialize(TreeNode root, StringBuilder builder){
+        if(root == null)
+            builder.append("#").append(",");
+        else{
+            builder.append(root.val).append(",");
+            serialize(root.left, builder);
+            serialize(root.right, builder);
+        }
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        Queue<String> queue = new LinkedList<String>(Arrays.asList(data.split(",")));
+        return deserialize(queue);
+    }
+    
+    private TreeNode deserialize(Queue<String> queue){
+        String str = queue.poll();
+        if(str.equals("#")) return null;
+        TreeNode root = new TreeNode(Integer.parseInt(str));
+        root.left = deserialize(queue);
+        root.right = deserialize(queue);
+        return root;
+    }
+}
