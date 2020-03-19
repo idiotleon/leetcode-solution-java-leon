c99cf7b1d805deb1d67f50514a3877f2c586185f
Leon <yanglyu.leon.7@gmail.com>
2020-03-19 14:44:27 -0700

    lc0636: Exclusive Time of Functions

---
+++ src/main/java/lcidiot/lc0636/Solution.java
@@ -0,0 +1,33 @@
+/**
+ * https://leetcode.com/problems/exclusive-time-of-functions/
+ */
+package main.java.lcidiot.lc0636;
+
+import java.util.List;
+import java.util.Stack;
+
+class Solution {
+    public int[] exclusiveTime(int n, List<String> logs) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int[] res = new int[n];
+        String[] str = logs.get(0).split(":");
+        stack.push(Integer.parseInt(str[0]));
+        int prev = Integer.parseInt(str[2]);
+        
+        for(int i = 1; i < logs.size(); i++){
+            str = logs.get(i).split(":");
+            if(str[1].equals("start")){
+                if(!stack.isEmpty())
+                    res[stack.peek()] += Integer.parseInt(str[2]) - prev;
+                stack.push(Integer.parseInt(str[0]));
+                prev = Integer.parseInt(str[2]);
+            }else{
+                res[stack.peek()] += Integer.parseInt(str[2]) - prev + 1;
+                stack.pop();
+                prev = Integer.parseInt(str[2]) + 1;
+            }
+        }
+        
+        return res;
+    }
+}
