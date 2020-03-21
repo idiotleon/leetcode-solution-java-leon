6a17fdba62b76c1344d579b5d81068741af1d3db
Leon <yanglyu.leon.7@gmail.com>
2020-03-21 14:41:51 -0700

    lc0137: Single Number II

---
+++ src/main/java/lcidiot/lc0137/SolutionApproachHashmap.java
@@ -0,0 +1,21 @@
+/**
+ * https://leetcode.com/problems/single-number-ii/
+ */
+package main.java.lcidiot.lc0137;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for(int num : nums)
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        
+        for(int key : map.keySet())
+            if(map.get(key) == 1)
+                return key;
+        
+        return -1;
+    }
+}
