2a0d4757294e30f15addf3767f5a97e0a0900e84
Leon <yanglyu.leon.7@gmail.com>
2020-03-22 01:05:28 -0700

    lc0142: Linked List Cycle II

---
+++ src/main/java/lcidiot/lc0142/SolutionApproachHashMap.java
@@ -0,0 +1,25 @@
+/**
+ * https://leetcode.com/problems/linked-list-cycle-ii/
+ */
+package main.java.lcidiot.lc0142;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import main.java.lcidiot.utils.ListNode;
+
+public class SolutionApproachHashMap {
+    public ListNode detectCycle(ListNode head) {
+        Set<ListNode> visited = new HashSet<ListNode>();
+        
+        ListNode node = head;
+        while(node != null){
+            if(visited.contains(node))
+                return node;
+            visited.add(node);
+            node = node.next;
+        }
+        
+        return null;
+    }
+}
