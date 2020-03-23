96dfb8efbaa8496f2c674a72836c0d53619c7897
Leon <yanglyu.leon.7@gmail.com>
2020-03-23 15:46:23 -0700

    lc0173: Binary Search Tree Iterator

---
+++ src/main/java/lcidiot/lc0173/SolutionApproachControlledRecursion.java
@@ -0,0 +1,46 @@
+/**
+ * https://leetcode.com/problems/binary-search-tree-iterator/
+ * 
+ * Time Complexity:
+ *  hasNext()   O(1)
+ *  next()      O(1)
+ * 
+ * Space Complexity: O(h)
+ */
+package main.java.lcidiot.lc0173;
+
+import java.util.Stack;
+
+import main.java.lcidiot.utils.TreeNode;
+
+class SolutionApproachControlledRecursion {
+    
+    private Stack<TreeNode> stack;
+
+    public SolutionApproachControlledRecursion(TreeNode root) {
+        this.stack = new Stack<TreeNode>();
+        this.leftmostInorder(root);
+    }
+    
+    private void leftmostInorder(TreeNode root){
+        while(root != null){
+            this.stack.push(root);
+            root = root.left;
+        }
+    }
+    
+    /** @return the next smallest number */
+    public int next() {
+        TreeNode topmostNode = this.stack.pop();
+        
+        if(topmostNode.right != null)
+            this.leftmostInorder(topmostNode.right);
+        
+        return topmostNode.val;
+    }
+    
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return this.stack.size() > 0;
+    }
+}
---
+++ src/main/java/lcidiot/lc0173/SolutionApproachFlattenBST.java
@@ -0,0 +1,43 @@
+/**
+ * https://leetcode.com/problems/binary-search-tree-iterator/
+ * 
+ * Time Complexity: O(N)
+ *  next()      O(1)
+ *  hasNext()   O(1)
+ * 
+ * Space Complexity: O(N)
+ */
+package main.java.lcidiot.lc0173;
+
+import java.util.ArrayList;
+
+import main.java.lcidiot.utils.TreeNode;
+
+class SolutionApproachFlattenBST{
+    private ArrayList<Integer> nodesSorted;
+    private int index;
+
+    public SolutionApproachFlattenBST(TreeNode root) {
+        this.nodesSorted = new ArrayList<Integer>();
+        this.index = -1;
+        this.inorder(root);
+    }
+    
+    private void inorder(TreeNode root){
+        if(root == null) return;
+        
+        this.inorder(root.left);
+        this.nodesSorted.add(root.val);
+        this.inorder(root.right);
+    }
+    
+    /** @return the next smallest number */
+    public int next() {
+        return this.nodesSorted.get(++this.index);
+    }
+    
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        return this.index + 1 < this.nodesSorted.size();
+    }
+}
