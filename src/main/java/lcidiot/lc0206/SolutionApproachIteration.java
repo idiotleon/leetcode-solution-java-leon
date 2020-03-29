e72416ea20ec36c979a2075a10585266f4aa1a14
Leon <yanglyu.leon.7@gmail.com>
2020-03-29 00:21:02 -0700

    lc0206: Reverse Linked List

---
+++ src/main/java/lcidiot/lc0206/SolutionApproachIteration.java
@@ -0,0 +1,21 @@
+/**
+ * https://leetcode.com/problems/reverse-linked-list/
+ */
+package main.java.lcidiot.lc0206;
+
+import main.java.lcidiot.utils.ListNode;
+
+class SolutionApproachIteration {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        
+        while(head != null){
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        
+        return prev;
+    }
+}
