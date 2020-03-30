5afa4ab288003257377c088d5c74dddd048976c4
Leon <yanglyu.leon.7@gmail.com>
2020-03-29 19:02:21 -0700

    lc0124: Binary Tree Maximum Path Sum

---
+++ src/main/java/lcidiot/lc0124/SolutionApproachRecursion.java
@@ -0,0 +1,26 @@
+/**
+ * https://leetcode.com/problems/binary-tree-maximum-path-sum/
+ * 
+ * https://leetcode.com/problems/binary-tree-maximum-path-sum/discuss/39775/Accepted-short-solution-in-Java
+ */
+package main.java.lcidiot.lc0124;
+
+import main.java.lcidiot.utils.TreeNode;
+
+class SolutionApproachRecursion {
+    private int maxValue;
+    
+    public int maxPathSum(TreeNode root) {
+        maxValue = Integer.MIN_VALUE;
+        maxPathDown(root);
+        return maxValue;
+    }
+    
+    private int maxPathDown(TreeNode node){
+        if(node == null) return 0;
+        int left = Math.max(0, maxPathDown(node.left));
+        int right = Math.max(0, maxPathDown(node.right));
+        maxValue = Math.max(maxValue, left + right + node.val);
+        return Math.max(left, right) + node.val;
+    }
+}
