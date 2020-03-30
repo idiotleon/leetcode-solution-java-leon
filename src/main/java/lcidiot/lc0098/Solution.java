9ce38a389cba0673aabc5b3a69cc59574223cbe4
Leon <yanglyu.leon.7@gmail.com>
2020-03-30 12:17:46 -0700

    lc0098: Validate Binary Search Tree

--- src/main/java/lcidiot/lc0098/Solution.java
+++ src/main/java/lcidiot/lc0098/Solution.java
@@ -7,20 +7,18 @@ import main.java.lcidiot.utils.TreeNode;
 
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return helper(root, null, null);
+        return isValidBST(root, null, null);
     }
     
-    private boolean helper(TreeNode root, 
-                           Integer lower, 
-                           Integer upper){
+    private boolean isValidBST(TreeNode root, Integer lower, Integer upper){
         if(root == null) return true;
         
         int val = root.val;
         if(lower != null && lower >= val) return false;
         if(upper != null && upper <= val) return false;
         
-        if(!helper(root.left, lower, val)) return false;
-        if(!helper(root.right, val, upper)) return false;
+        if(!isValidBST(root.left, lower, val)) return false;
+        if(!isValidBST(root.right, val, upper)) return false;
         
         return true;
     }
