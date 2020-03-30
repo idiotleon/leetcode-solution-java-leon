f8ac4e5d4d38bb338a7ead017307cc987aa2aaff
Leon <yanglyu.leon.7@gmail.com>
2020-03-29 23:09:20 -0700

    lc0252: Meeting Rooms

--- src/main/java/lcidiot/lc0252/Solution.java
+++ src/main/java/lcidiot/lc0252/Solution.java
@@ -9,9 +9,11 @@ class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         
-        for(int i = 1; i < intervals.length; i++)
-            if(intervals[i][0] < intervals[i - 1][1])
+        for(int i = 1; i < intervals.length; i++){
+            if(intervals[i][0] < intervals[i - 1][1]){
                 return false;
+            }
+        }
         
         return true;
     }
