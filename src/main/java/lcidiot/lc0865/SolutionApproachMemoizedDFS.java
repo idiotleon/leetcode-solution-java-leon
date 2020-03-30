9f892b41fffc518835a729a11d8f72f6d1c0e19c
Leon <yanglyu.leon.7@gmail.com>
2020-03-29 23:02:19 -0700

    lc0865: Smallest Subtree with all the Deepest Nodes

---
+++ src/main/java/lcidiot/lc0865/SolutionApproachMemoizedDFS.java
@@ -0,0 +1,33 @@
+/**
+ * https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/
+ */
+package main.java.lcidiot.lc0865;
+
+import java.util.HashMap;
+
+import main.java.lcidiot.utils.TreeNode;
+
+class SolutionApproachMemoizedDFS {
+    public TreeNode subtreeWithAllDeepest(TreeNode root) {
+        if(root == null) return null;
+        HashMap<TreeNode, Integer> map = new HashMap<TreeNode, Integer>();
+        depth(root, map);
+        return dfs(root, map);
+    }
+    
+    private TreeNode dfs(TreeNode root, HashMap<TreeNode, Integer> map){
+        int left = depth(root.left, map);
+        int right = depth(root.right, map);
+        if(left == right) return root;
+        if(left > right) return dfs(root.left, map);
+        return dfs(root.right, map);
+    }
+    
+    private int depth(TreeNode root, HashMap<TreeNode, Integer> map){
+        if(root == null) return 0;
+        if(map.containsKey(root)) return map.get(root);
+        int max = Math.max(depth(root.left, map), depth(root.right, map)) + 1;
+        map.put(root, max);
+        return max;
+    }
+}
