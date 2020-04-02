5aacb8578285b16c8bd587e2666a04ba164de11f
Leon <yanglyu.leon.7@gmail.com>
2020-04-01 23:32:34 -0700

    lc0953: Verifying an Alien Dictionary

--- src/main/java/lcidiot/lc0953/Solution.java
+++ src/main/java/lcidiot/lc0953/Solution.java
@@ -4,15 +4,14 @@
 package main.java.lcidiot.lc0953;
 
 class Solution {
-    private int[] mapping = new int[26];
-    
     public boolean isAlienSorted(String[] words, String order) {
+        int[] dict = new int[order.length()];
         for(int i = 0; i < order.length(); i++){
-            mapping[order.charAt(i) - 'a'] = i;
+            dict[order.charAt(i) - 'a'] = i;
         }
         
         for(int i = 1; i < words.length; i++){
-            if(bigger(words[i - 1], words[i])){
+            if(bigger(words[i - 1], words[i], dict)){
                 return false;
             }
         }
@@ -20,15 +19,16 @@ class Solution {
         return true;
     }
     
-    private boolean bigger(String str1, String str2){
-        int m = str1.length(), n = str2.length();
-        for(int i = 0; i < m && i < n; ++i){
-            if(str1.charAt(i) != str2.charAt(i)){
-                return mapping[str1.charAt(i) - 'a'] > mapping[str2.charAt(i) - 'a'];
+    private boolean bigger(String word1, String word2, int[] dict){
+        final int M = word1.length(), N = word2.length();
+        
+        for(int i = 0; i < Math.min(M, N); i++){
+            if(word1.charAt(i) != word2.charAt(i)){
+                return dict[word1.charAt(i) - 'a'] > dict[word2.charAt(i) - 'a'];
             }
         }
         
-        return m > n;
+        return M > N;
     }
 
     public boolean isAlienSorted2(String[] words, String order) {
