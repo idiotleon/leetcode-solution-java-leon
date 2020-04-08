9d432497b5be8971fb85bf31c07d1cc4e2c051ad
Leon <yanglyu.leon.7@gmail.com>
2020-04-08 15:35:43 -0700

    lc0129: Sum Root to Leaf Numbers

--- src/main/java/lcidiot/lc0129/SolutionApproachDFSConstructingString.java
+++ src/main/java/lcidiot/lc0129/SolutionApproachDFSConstructingString.java
@@ -13,38 +13,29 @@ import main.java.lcidiot.utils.TreeNode;
 
 public class SolutionApproachDFSConstructingString {
     public int sumNumbers(TreeNode root) {
-        List<String> nums = new ArrayList<String>();
+        if(root == null) return 0;
+        List<Integer> nums = new ArrayList<Integer>();
         dfs(root, "", nums);
         
         int sum = 0;
-        for(String num : nums){
-            sum += Integer.parseInt(num);
+        for(int num : nums){
+            sum += num;
         }
         
         return sum;
     }
     
-    private void dfs(TreeNode node, 
-                     String intermediate, 
-                     List<String> nums){
+    private void dfs(TreeNode node, String intermediate, List<Integer> nums){
         if(node == null) return;
         
         intermediate += Integer.toString(node.val);
-
+        
         if(node.left == null && node.right == null){
-            nums.add(intermediate);
+            nums.add(Integer.parseInt(intermediate));
             return;
-        }else{
-            dfs(node.left, intermediate, nums);
-            dfs(node.right, intermediate, nums);
         }
         
-        // if(node.left != null){
-        //     dfs(node.left, intermediate, nums);
-        // }
-        
-        // if(node.right != null){
-        //     dfs(node.right, intermediate, nums);
-        // }
+        dfs(node.left, intermediate, nums);
+        dfs(node.right, intermediate, nums);
     }
 }
