eeeac611fa216fd4329e6de37c5e8e9515547186
Leon <yanglyu.leon.7@gmail.com>
2020-04-10 22:39:06 -0700

    lc0725: Split Linked List in Parts

---
+++ src/main/java/lcidiot/lc0725/Solution.java
@@ -0,0 +1,40 @@
+/**
+ * https://leetcode.com/problems/split-linked-list-in-parts/
+ */
+package main.java.lcidiot.lc0725;
+
+import main.java.lcidiot.utils.ListNode;
+
+public class Solution {
+    public ListNode[] splitListToParts(ListNode root, int k) {
+        ListNode[] parts = new ListNode[k];
+        int len = 0;
+        
+        ListNode cur = root;
+        while(cur != null){
+            len++;
+            cur = cur.next;
+        }
+        
+        int n = len / k, r = len % k;
+        // to reset the pointer
+        cur = root;
+        // to break the list later on
+        ListNode prev = null;
+        
+        for(int i = 0; cur != null && i < k; i++, r--){
+            parts[i] = cur;
+            
+            for(int j = 0; j < n + (r > 0 ? 1 : 0); j++){
+                prev = cur;
+                cur = cur.next;
+            }
+            
+            // to break the list
+            prev.next = null;
+        }
+        
+        
+        return parts;
+    }
+}
