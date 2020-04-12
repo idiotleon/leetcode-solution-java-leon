73f3d27ac871fd36aca39730f343f37eba3fb047
Leon <yanglyu.leon.7@gmail.com>
2020-04-11 17:20:56 -0700

    lc0253: Meeting Rooms II

--- src/main/java/lcidiot/lc0253/Solution.java
+++ src/main/java/lcidiot/lc0253/Solution.java
@@ -8,18 +8,17 @@ import java.util.PriorityQueue;
 
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
-        if(intervals.length == 0) return 0;
+        if(intervals == null || intervals.length == 0) return 0;
         
-        PriorityQueue<Integer> queue = new PriorityQueue<Integer>(intervals.length, (a, b) -> a - b);
-        
+        PriorityQueue<Integer> heap = new PriorityQueue<Integer>(intervals.length, (a, b) -> a - b);
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
-        queue.add(intervals[0][1]);
+        heap.add(intervals[0][1]);
         
         for(int i = 1; i < intervals.length; i++){
-            if(intervals[i][0] >= queue.peek()) queue.poll();
-            queue.add(intervals[i][1]);
+            if(intervals[i][0] >= heap.peek()) heap.poll();
+            heap.add(intervals[i][1]);
         }
         
-        return queue.size();
+        return heap.size();
     }
 }
