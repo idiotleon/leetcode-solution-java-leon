0960910d7de52cf4a590bf663cb1cb1fa60b7b89
Leon <yanglyu.leon.7@gmail.com>
2020-04-15 16:29:23 -0700

    lc0561: Array Partition I

---
+++ src/main/java/lcidiot/lc0561/SolutionApproachSort.java
@@ -0,0 +1,22 @@
+/**
+ * https://leetcode.com/problems/array-partition-i/
+ * 
+ * Time Complexity: O(Nlg(N))
+ * Space Complexity: O(1)
+ */
+package main.java.lcidiot.lc0561;
+
+import java.util.Arrays;
+
+public class SolutionApproachSort {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        
+        int sum = 0;
+        for(int i = 0; i < nums.length; i += 2){
+            sum += nums[i];
+        }
+        
+        return sum;
+    }
+}
