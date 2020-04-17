1377ab3270ea9359ffffcad1a4afd0883ae8f7ff
Leon <yanglyu.leon.7@gmail.com>
2020-04-17 16:16:23 -0700

    lc0159: Longest Substring with at most two Distinct Characters

--- src/main/java/lcidiot/lc0159/SolutionApproachSlidingWindow.java
+++ src/main/java/lcidiot/lc0159/SolutionApproachSlidingWindow.java
@@ -2,6 +2,9 @@
  * https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-characters/
  * 
  * https://leetcode.com/problems/longest-substring-with-at-most-two-distinct-characters/discuss/49708/Sliding-Window-algorithm-template-to-solve-all-the-Leetcode-substring-search-problem.
+ * 
+ * Time Complexity: O(N)
+ * Space Complexity: O(1)
  */
 package main.java.lcidiot.lc0159;
 
@@ -11,21 +14,23 @@ import java.util.Map;
 class SolutionApproachSlidingWindow {
     public int lengthOfLongestSubstringTwoDistinct(String s) {
         Map<Character, Integer> map = new HashMap<Character, Integer>();
-        int start = 0, end = 0, counter = 0, len = 0;
-        
-        while(end < s.length()){
-            char c = s.charAt(end);
+        int left = 0, right = 0, counter = 0, len = 0;
+        while(right < s.length()){
+            char c = s.charAt(right);
             map.put(c, map.getOrDefault(c, 0) + 1);
-            if(map.get(c) == 1) counter++;  // the new char
-            end++;
+            if(map.get(c) == 1) counter++;
+            right++;
             while(counter > 2){
-                char tempChar = s.charAt(start);
-                map.put(tempChar, map.get(tempChar) - 1);
-                if(map.get(tempChar) == 0) counter--;
-                start++;
+                char temp = s.charAt(left);
+                map.put(temp, map.get(temp) - 1);
+                if(map.get(temp) == 0){
+                    counter--;
+                }
+                
+                left++;
             }
             
-            len = Math.max(len, end - start);
+            len = Math.max(len, right - left);
         }
         
         return len;
