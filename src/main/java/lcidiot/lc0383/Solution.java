8bbeee35dfc514a72063cd44352079ee3b998447
Leon <yanglyu.leon.7@gmail.com>
2020-04-18 16:18:02 -0700

    lc0383: Ransom Note

---
+++ src/main/java/lcidiot/lc0383/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * https://leetcode.com/problems/ransom-note/
+ * 
+ * Time Complexity: O(N)
+ * Space Complexity: O(1)
+ */
+package main.java.lcidiot.lc0383;
+
+public class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if(ransomNote == null || ransomNote.length() == 0) return true;
+                
+        int[] freq = new int[26];
+        for(int i = 0; i < magazine.length(); i++){
+            ++freq[magazine.charAt(i) - 'a'];
+        }
+        
+        for(int i = 0; i < ransomNote.length(); i++){
+            if(--freq[ransomNote.charAt(i) - 'a'] < 0) 
+                return false;
+        }
+        
+        return true;
+    }
+}
