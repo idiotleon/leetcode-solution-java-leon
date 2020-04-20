0e5a7ec6e9bb9f8eecd221bd2600cb01f42e8233
Leon <yanglyu.leon.7@gmail.com>
2020-04-19 22:08:36 -0700

    lc0459: Repeated Substring Pattern

---
+++ src/main/java/lcidiot/lc0459/SolutionApproachKMP.java
@@ -0,0 +1,28 @@
+/**
+ * https://leetcode.com/problems/repeated-substring-pattern/
+ * 
+ * Time Complexity: O(N)
+ * Space Complexity: O(N)
+ */
+package main.java.lcidiot.lc0459;
+
+public class SolutionApproachKMP {
+    public boolean repeatedSubstringPattern(String s) {
+        if(s == null || s.isEmpty()) return true;
+        
+        final int L = s.length();
+        int[] kmpTable = new int[L];
+        int i = 1, j = 0;
+        while(i < L){
+            if(s.charAt(j) == s.charAt(i)){
+                kmpTable[i++] = ++j;
+            }else if(j == 0){
+                i++;
+            }else{
+                j = kmpTable[j - 1];
+            }
+        }
+        
+        return kmpTable[L - 1] != 0 && L % (L - kmpTable[L - 1]) == 0;
+    }
+}
