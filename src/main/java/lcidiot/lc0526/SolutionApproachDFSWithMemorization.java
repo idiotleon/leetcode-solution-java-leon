4e41a4e727daecdce731bc40c5cf3bf6dd930d17
Leon <yanglyu.leon.7@gmail.com>
2020-04-22 10:44:57 -0700

    refactor: re-organizing the code

--- src/main/java/lcidiot/lc0526/SolutionApproachDFSWithMemorization.java
+++ src/main/java/lcidiot/lc0526/SolutionApproachDFSWithMemorization.java
@@ -10,27 +10,25 @@ import java.util.Map;
 
 public class SolutionApproachDFSWithMemorization {
     public int countArrangement(int N) {
-        Map<Integer, Integer> cache = new HashMap<Integer, Integer>();
-        return countArrangement(N, 0, 0, cache);
+        return countArrangement(N, 0, 0, new HashMap<Integer, Integer>());
     }
     
     private int countArrangement(int N, 
                                  int index, 
                                  int visited, 
                                  Map<Integer, Integer> cache){
-        int result = 0;
-        
         if(index == N) return 1;
-        
         if(cache.containsKey(visited)) return cache.get(visited);
         
+        int result = 0;
         for(int i = 1; i <= N; i++){
-            int bit = (1 << (i - 1));
-            if(((index + 1) % i != 0) && (i % (index + 1) != 0)) continue;
+            int bit = 1 << (i - 1);
+            if(((index + 1) % i) != 0 && i % (index + 1) != 0) continue;
             if((visited & bit) == 0){
                 result += countArrangement(N, index + 1, visited | bit, cache);
             }
         }
+        
         cache.put(visited, result);
         return result;
     }
