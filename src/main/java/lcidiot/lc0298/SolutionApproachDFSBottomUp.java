b44764904f4497a3b5cdc2b289ff02c5eba2184e
Leon <yanglyu.leon.7@gmail.com>
2020-04-22 18:38:16 -0700

    lc0298: Binary Tree Longest Consecutive Sequence

---
+++ src/main/java/lcidiot/lc0298/SolutionApproachDFSBottomUp.java
@@ -0,0 +1,24 @@
+/**
+ * https://leetcode.com/problems/binary-tree-longest-consecutive-sequence/
+ */
+package main.java.lcidiot.lc0298;
+
+import main.java.lcidiot.data_structure.tree.TreeNode;
+
+public class SolutionApproachDFSBottomUp {
+    public int longestConsecutive(TreeNode root) {
+        if(root == null) return 0;
+        
+        return Math.max(dfs(root.left, 1, root.val + 1), dfs(root.right, 1, root.val + 1));
+    }
+    
+    private int dfs(TreeNode node, int sequence, int target){
+        if(node == null) return sequence;
+        
+        sequence = (node.val == target) ? sequence + 1 : 1;
+        int left = dfs(node.left, sequence, node.val + 1);
+        int right = dfs(node.right, sequence, node.val + 1);
+        
+        return Math.max(sequence, Math.max(left, right));
+    }
+}
