59015ee943a0732a3603e411a03acd418d316840
Leon <yanglyu.leon.7@gmail.com>
2020-04-25 01:35:52 -0700

    lc0543: Diameter of Binary Tree

---
+++ src/main/java/com/null7ptr/lc/lvl3/lc0543/SolutionApproachDFSBottomUp.java
@@ -0,0 +1,27 @@
+/**
+ * https://leetcode.com/problems/diameter-of-binary-tree/
+ */
+package com.null7ptr.lc.lvl3.lc0543;
+
+import com.null7ptr.data_structure.tree.TreeNode;
+
+public class SolutionApproachDFSBottomUp {
+    private int max;
+    
+    public int diameterOfBinaryTree(TreeNode root) {
+        this.max = 0;
+        dfs(root);
+        return max;
+    }
+    
+    private int dfs(TreeNode node){
+        if(node == null) return 0;
+        
+        int left = dfs(node.left);
+        int right = dfs(node.right);
+        
+        max = Math.max(max, left + right);
+        
+        return Math.max(left, right) + 1;
+    }
+}
