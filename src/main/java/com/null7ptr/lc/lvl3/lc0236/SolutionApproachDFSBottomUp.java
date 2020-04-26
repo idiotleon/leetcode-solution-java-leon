aaf84d19867d89ed47d4a2a4d87175adee8927fd
Leon <yanglyu.leon.7@gmail.com>
2020-04-25 19:18:39 -0700

    lc0237: Lowest Common Ancestor of a Binary Tree

--- src/main/java/com/null7ptr/lc/lvl3/lc0236/SolutionApproachDFSBottomUp.java
+++ src/main/java/com/null7ptr/lc/lvl3/lc0236/SolutionApproachDFSBottomUp.java
@@ -1,5 +1,9 @@
 /**
  * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+ * 
+ * https://www.youtube.com/watch?v=13m9ZCB8gjw
+ * Time Complexity: O(N)
+ * Space Complexity: O(H)
  */
 package com.null7ptr.lc.lvl3.lc0236;
 
@@ -7,13 +11,14 @@ import com.null7ptr.data_structure.tree.TreeNode;
 
 public class SolutionApproachDFSBottomUp {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null || root == p || root == q) return root;
+        if(root == null) return null;
+        if(root == p || root == q) return root;
         
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
         
-        if(left == null) return right;
-        else if(right == null) return left;
-        else return root;
+        if(left != null && right != null) return root;
+        
+        return left == null ? right : left;
     }
 }
--- src/main/java/com/null7ptr/lc/lvl3/lc0236/SolutionApproachDFSBottomUp1.java
+++ src/main/java/com/null7ptr/lc/lvl3/lc0236/SolutionApproachDFSBottomUp1.java
@@ -1,8 +1,5 @@
 /**
  * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
- * 
- * Time Complexity: O(N)
- * Space Complexity: O(H)
  */
 package com.null7ptr.lc.lvl3.lc0236;
 
@@ -10,14 +7,13 @@ import com.null7ptr.data_structure.tree.TreeNode;
 
 public class SolutionApproachDFSBottomUp1 {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null) return null;
-        if(root == p || root == q) return root;
+        if(root == null || root == p || root == q) return root;
         
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
         
-        if(left != null && right != null) return root;
-        
-        return left == null ? right : left;
+        if(left == null) return right;
+        else if(right == null) return left;
+        else return root;
     }
 }
