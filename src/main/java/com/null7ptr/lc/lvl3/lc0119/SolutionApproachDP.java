fe069942f807e4513189ae99b9fc45bc258eef59
Leon <yanglyu.leon.7@gmail.com>
2020-04-26 10:51:00 -0700

    lc119: Pascal's Triangle II

--- src/main/java/com/null7ptr/lc/lvl3/lc0119/SolutionApproachDP.java
+++ src/main/java/com/null7ptr/lc/lvl3/lc0119/SolutionApproachDP.java
@@ -6,16 +6,17 @@ package com.null7ptr.lc.lvl3.lc0119;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Solution {
+public class SolutionApproachDP {
     public List<Integer> getRow(int rowIndex) {
         List<Integer> ans = new ArrayList<Integer>();
         // sanity check
         if(rowIndex < 0) return ans;
         
-        for(int i = 0; i < rowIndex + 1; i++){
+        for(int i = 0; i <= rowIndex; i++){
             ans.add(0, 1);
-            for(int j = 1; j < ans.size() - 1; j++)
+            for(int j = 1; j < ans.size() - 1; j++){
                 ans.set(j, ans.get(j) + ans.get(j + 1));
+            }
         }
         
         return ans;
