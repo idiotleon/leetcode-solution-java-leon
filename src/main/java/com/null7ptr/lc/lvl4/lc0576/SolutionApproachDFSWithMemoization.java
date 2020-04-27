25811796053d5b72f4d0f0a0ed57dddbb49b7aea
Leon <yanglyu.leon.7@gmail.com>
2020-04-26 17:33:43 -0700

    lc0576: Out of Boundary Paths

---
+++ src/main/java/com/null7ptr/lc/lvl4/lc0576/SolutionApproachDFSWithMemoization.java
@@ -0,0 +1,32 @@
+/**
+ * https://leetcode.com/problems/out-of-boundary-paths/
+ * 
+ * Time Complexity: O(mnN)
+ * Space Complexity: O(mnN)
+ */
+package com.null7ptr.lc.lvl4.lc0576;
+
+public class SolutionApproachDFSWithMemoization {
+    private static int[] DIRS = {0, 1, 0, -1, 0};
+    private static int MOD = 1000000000 + 7;
+    
+    public int findPaths(int m, int n, int N, int i, int j) {
+        Long[][][] memo = new Long[m][n][N + 1];
+        
+        return (int)(dfs(m, n, N, i, j, memo) % MOD);
+    }
+    
+    private long dfs(int m, int n, int N, int row, int col, Long[][][] memo){
+        if(row < 0 || row >= m || col < 0 || col >= n) return 1;
+        
+        if(N == 0) return 0;
+        if(memo[row][col][N] != null) return memo[row][col][N];
+        
+        memo[row][col][N] = 0L;
+        for(int d = 0; d < 4; d++){
+            int r = row + DIRS[d], c = col + DIRS[d + 1];
+            memo[row][col][N] += dfs(m, n, N - 1, r, c, memo) % MOD;
+        }
+        return memo[row][col][N];
+    }
+}
