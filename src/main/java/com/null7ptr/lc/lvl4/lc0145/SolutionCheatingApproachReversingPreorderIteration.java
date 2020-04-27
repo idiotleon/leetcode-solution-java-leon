ede47428197e0f87c2269f80342bb9aae770571c
Leon <yanglyu.leon.7@gmail.com>
2020-04-27 12:01:47 -0700

    lc0145: Binary Tree Postorder Traversal

--- src/main/java/com/null7ptr/lc/lvl4/lc0145/SolutionCheatingApproachReversingPreorderIteration.java
+++ src/main/java/com/null7ptr/lc/lvl4/lc0145/SolutionCheatingApproachReversingPreorderIteration.java
@@ -5,10 +5,11 @@ package com.null7ptr.lc.lvl4.lc0145;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 import com.null7ptr.data_structure.tree.TreeNode;
 
-public class SolutionApproachIteration {
+public class SolutionCheatingApproachReversingPreorderIteration {
     public List<Integer> postorderTraversal(TreeNode root) {
         LinkedList<TreeNode> stack = new LinkedList<TreeNode>();
         LinkedList<Integer> output = new LinkedList<Integer>();
@@ -18,6 +19,22 @@ public class SolutionApproachIteration {
         while(!stack.isEmpty()){
             TreeNode node = stack.pollLast();
             output.addFirst(node.val);
+            if(node.left != null) stack.add(node.left);
+            if(node.right != null) stack.add(node.right);
+        }
+        
+        return output;
+    }
+
+    public List<Integer> postorderTraversal2(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        LinkedList<Integer> output = new LinkedList<Integer>();
+        if(root == null) return output;
+        
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            output.push(node.val);
             if(node.left != null) stack.add(node.left);
             if(node.right != null) stack.add(node.right);
         }
