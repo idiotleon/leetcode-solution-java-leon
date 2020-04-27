b4243b8923b6b6d1ddc5242fa233e81ee9f80aa7
Leon <yanglyu.leon.7@gmail.com>
2020-04-27 11:09:04 -0700

    lc0977: Squares of a Sorted Array

---
+++ src/main/java/com/null7ptr/lc/lvl1/lc0977/SolutionApproachTwoPtrs.java
@@ -0,0 +1,25 @@
+package com.null7ptr.lc.lvl1.lc0977;
+
+public class SolutionApproachTwoPtrs {
+    public int[] sortedSquares(int[] A) {
+        // sanity check
+        if(A == null || A.length == 0) return new int[0];
+        
+        final int L = A.length;
+        int[] ans = new int[L];
+        
+        int left = 0, right = L - 1, idx = L - 1;
+        
+        while(left <= right){
+            if(Math.abs(A[left]) < Math.abs(A[right])){
+                ans[idx--] = A[right] * A[right];
+                right--;
+            }else{
+                ans[idx--] = A[left] * A[left];
+                left++;
+            }
+        }
+        
+        return ans;
+    }
+}
