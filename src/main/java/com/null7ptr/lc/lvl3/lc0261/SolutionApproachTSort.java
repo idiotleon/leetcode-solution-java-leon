dd954c53395ccb66a3ac250614cf50071a4764d4
Leon <yanglyu.leon.7@gmail.com>
2020-05-01 18:52:02 -0700

    lc0261: Graph Valid Tree

--- src/main/java/com/null7ptr/lc/lvl3/lc0261/SolutionApproachTSort.java
+++ src/main/java/com/null7ptr/lc/lvl3/lc0261/SolutionApproachTSort.java
@@ -1,53 +1,52 @@
 /**
  * https://leetcode.com/problems/graph-valid-tree/
  * 
- * Time Complexity: O(V + E)
- * Space Complexity: O(V + E)
+ * Time Complexity: O(N + M)
+ * Space Complexity: O(N + M)
  */
 package com.null7ptr.lc.lvl3.lc0261;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Set;
 
 public class SolutionApproachTSort {
     public boolean validTree(int n, int[][] edges) {
-        // to builde up the graph
-        Map<Integer, Set<Integer>> graph = new HashMap<Integer, Set<Integer>>();
-        LinkedList<Integer> queue = new LinkedList<Integer>();
+        Set<Integer>[] graph = new HashSet[n];
         
         for(int i = 0; i < n; i++){
-            graph.put(i, new HashSet<Integer>());
+            graph[i] = new HashSet<Integer>();
         }
         
         for(int[] edge : edges){
-            graph.get(edge[0]).add(edge[1]);
-            graph.get(edge[1]).add(edge[0]);
+            graph[edge[0]].add(edge[1]);
+            graph[edge[1]].add(edge[0]);
         }
         
-        for(int key : graph.keySet()){
-            if(graph.get(key).size() == 1){
-                queue.add(key);
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+        for(int i = 0; i < n; i++){
+            // isolated nodes, if ever been found
+            // (1, []) is expected to return true
+            // if(graph[i].size() == 0) return false;
+            // eligible (leaf) nodes, with only 1 parent
+            if(graph[i].size() == 1){
+                queue.add(i);
             }
         }
         
-        // to topological sort
         int count = 0;
         while(!queue.isEmpty()){
             int node = queue.removeFirst();
-            if(graph.get(node).size() == 0) continue;
-            int parent = graph.get(node).iterator().next();
-            graph.get(parent).remove(node);
-            graph.remove(node);
-            if(graph.get(parent).size() == 1){
+            if(graph[node].size() == 0) continue;
+            int parent = graph[node].iterator().next();
+            // --indegree[i]
+            graph[parent].remove(node);
+            if(graph[parent].size() == 1){
                 queue.add(parent);
             }
             count++;
         }
         
-        if(count == n - 1) return true;
-        return false;
+        return count == n - 1;
     }
 }
