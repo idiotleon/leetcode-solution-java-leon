4683d6d3a4f859bb7fbb0a7ed86c1a94ef4fc8f2
Leon <yanglyu.leon.7@gmail.com>
2020-05-03 13:17:54 -0700

    algorithm: Quick Sort and Quick Select

---
+++ src/main/java/com/null7ptr/algorithm/sort/quick_sort/QuickSelectWithRandomPivot.java
@@ -0,0 +1,41 @@
+/**
+ * Time Complexity: 
+ *  Best:       O(N)
+ *  Averagely:  O(N)
+ *  Worst:      O(N ^ 2)
+ */
+package com.null7ptr.algorithm.sort.quick_sort;
+
+import java.util.Random;
+
+public class QuickSelectWithRandomPivot {
+    public int quickSelect(int[] nums, int left, int right, int k){
+        if(left <= right){
+            int pivot = partition(nums, left, right);
+            if(pivot == k) return nums[k];
+            else if(pivot > k) return quickSelect(nums, left, pivot - 1, k);
+            else return quickSelect(nums, pivot + 1, right, k);
+        }
+        
+        return Integer.MIN_VALUE;
+    }
+    
+    private int partition(int[] nums, int left, int right){
+        int pivot = left + new Random().nextInt(right - left + 1);
+        swap(nums, pivot, right);
+        for(int i = left; i < right; i++){
+            if(nums[i] > nums[right]){
+                swap(nums, i, left);
+                left++;
+            }
+        }
+        swap(nums, left, right);
+        return left;
+    }
+    
+    private void swap(int[] nums, int left, int right){
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+}
