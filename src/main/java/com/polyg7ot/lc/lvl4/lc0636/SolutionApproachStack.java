6e4bfd15c55b821b8565287c9c79ee8fccc9dc3f
Leon <yanglyu.leon.7@gmail.com>
2020-05-05 11:36:35 -0700

    lc0636: Exclusive Time of Functions

---
+++ src/main/java/com/polyg7ot/lc/lvl4/lc0636/SolutionApproachStack.java
@@ -0,0 +1,37 @@
+/**
+ * https://leetcode.com/problems/exclusive-time-of-functions/
+ * 
+ * Time Complexity: O(N)
+ * Space Complexity: O(N)
+ */
+package com.polyg7ot.lc.lvl4.lc0636;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class SolutionApproachStack {
+    public int[] exclusiveTime(int n, List<String> logs) {
+        LinkedList<Integer> stack = new LinkedList<Integer>();
+        int[] ans = new int[n];
+        String[] log = logs.get(0).split(":");
+        stack.add(Integer.parseInt(log[0]));
+        int prev = Integer.parseInt(log[2]);
+        
+        for(int i = 1; i < logs.size(); i++){
+            log = logs.get(i).split(":");
+            if(log[1].equals("start")){
+                if(!stack.isEmpty()){
+                    ans[stack.peekLast()] += Integer.parseInt(log[2]) - prev;
+                }
+                stack.add(Integer.parseInt(log[0]));
+                prev = Integer.parseInt(log[2]);
+            }else{
+                ans[stack.peekLast()] += Integer.parseInt(log[2]) - prev + 1;
+                stack.removeLast();
+                prev = Integer.parseInt(log[2]) + 1;
+            }
+        }
+        
+        return ans;
+    }
+}
--- src/main/java/com/polyg7ot/lc/lvl4/lc0636/SolutionApproachStack1.java
+++ src/main/java/com/polyg7ot/lc/lvl4/lc0636/SolutionApproachStack1.java
@@ -1,12 +1,15 @@
 /**
  * https://leetcode.com/problems/exclusive-time-of-functions/
+ * 
+ * Time Complexity: O(N)
+ * Space Complexity: O(N)
  */
 package com.polyg7ot.lc.lvl4.lc0636;
 
 import java.util.List;
 import java.util.Stack;
 
-public class Solution {
+public class SolutionApproachStack1 {
     public int[] exclusiveTime(int n, List<String> logs) {
         Stack<Integer> stack = new Stack<Integer>();
         int[] res = new int[n];
@@ -17,8 +20,9 @@ public class Solution {
         for(int i = 1; i < logs.size(); i++){
             str = logs.get(i).split(":");
             if(str[1].equals("start")){
-                if(!stack.isEmpty())
+                if(!stack.isEmpty()){
                     res[stack.peek()] += Integer.parseInt(str[2]) - prev;
+                }
                 stack.push(Integer.parseInt(str[0]));
                 prev = Integer.parseInt(str[2]);
             }else{
