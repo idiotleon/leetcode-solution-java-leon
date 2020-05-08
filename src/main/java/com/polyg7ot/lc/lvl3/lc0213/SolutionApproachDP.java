9add7f5a6f6330261d40dc5790f9dfd930476027
Leon <yanglyu.leon.7@gmail.com>
2020-05-07 17:28:16 -0700

    lc0213: House Robber II

--- src/main/java/com/polyg7ot/lc/lvl3/lc0213/SolutionApproachDP.java
+++ src/main/java/com/polyg7ot/lc/lvl3/lc0213/SolutionApproachDP.java
@@ -1,36 +1,39 @@
 /**
  * https://leetcode.com/problems/house-robber-ii/
  * 
- * Time Complexity: O(N)
+ * Time Complexity: O(2 * N) ~ O(N)
  * Space Complexity: O(1)
  */
 package com.polyg7ot.lc.lvl3.lc0213;
 
 public class SolutionApproachDP {
-    public int rob(int[] nums) {
+    public int rob(int[] nums){
         // sanity check
         if(nums == null || nums.length == 0) return 0;
-        
+
         final int L = nums.length;
         if(L == 1) return nums[0];
-        
-        int lastPrev = 0, sum = 0, ans = 0;
-        for(int i = 0; i < L - 1; i++){
-            int prev = sum;
-            sum = Math.max(sum, lastPrev + nums[i]);
-            lastPrev = prev;
+
+        // if the first one is going to be robbed
+        int robFirst = rob(nums, 0, L - 2);
+        // if the second one is going to be robbed
+        int robSecond = rob(nums, 1, L - 1);
+
+        return Math.max(robFirst, robSecond);
+    }
+
+    private int rob(int[] nums, int from, int to){
+        int include = 0, exclude = 0;
+
+        for(int j = from; j <= to; j++){
+            int i = include, e = exclude;
+            // if this one is going to be robbed,
+            // the previous one should be included
+            include = e + nums[j];
+            // if this one is NOT going to be robbed
+            exclude = Math.max(e, i);
         }
-        ans = sum;
-        
-        lastPrev = 0;
-        sum = 0;
-        for(int i = 1; i < L; i++){
-            int prev = sum;
-            sum = Math.max(sum, lastPrev + nums[i]);
-            lastPrev = prev;
-        }
-        ans = Math.max(ans, sum);
-        
-        return ans;
+
+        return Math.max(include, exclude);
     }
 }
