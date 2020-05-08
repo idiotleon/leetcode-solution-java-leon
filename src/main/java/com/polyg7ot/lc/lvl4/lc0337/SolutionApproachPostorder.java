bc87eb0be62f167c6422f61708e65b6efc753774
Leon <yanglyu.leon.7@gmail.com>
2020-05-07 17:48:38 -0700

    lc0337: House Robber III

---
+++ src/main/java/com/polyg7ot/lc/lvl4/lc0337/SolutionApproachPostorder.java
@@ -0,0 +1,25 @@
+/**
+ * https://leetcode.com/problems/house-robber-iii/
+ */
+package com.polyg7ot.lc.lvl4.lc0337;
+
+import com.polyg7ot.data_structure.tree.TreeNode;
+
+public class SolutionApproachPostorder {
+    public int rob(TreeNode root) {
+        int[] ans = postorder(root);
+        return Math.max(ans[0], ans[1]);
+    }
+    
+    private int[] postorder(TreeNode node){
+        if(node == null) return new int[2];
+        int[] left = postorder(node.left);
+        int[] right = postorder(node.right);
+        int[] res = new int[2];
+        // idx being 0 means the max value not having this node robbed
+        // idx being 1 means the max value having this node robbed
+        res[0] = left[1] + right[1] + node.val;
+        res[1] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        return res;
+    }
+}
