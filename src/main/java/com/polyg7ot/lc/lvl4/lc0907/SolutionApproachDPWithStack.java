7f57e127e3867e30d8dd3bd86efa2e237a034faa
Leon <yanglyu.leon.7@gmail.com>
2020-05-08 20:39:59 -0700

    lc0907: Sum of Subarray Minimums

---
+++ src/main/java/com/polyg7ot/lc/lvl4/lc0907/SolutionApproachDPWithStack.java
@@ -0,0 +1,37 @@
+/**
+ * https://leetcode.com/problems/sum-of-subarray-minimums/
+ * 
+ * Time Complexity: O(N)
+ * Space Complexity: O(N)
+ * 
+ * dp[i + 1]: sum of minimum of subarrays which ends with nums[i]
+ * https://leetcode.com/problems/sum-of-subarray-minimums/discuss/170769/Java-O(n)-monotone-idxStack-with-DP
+ */
+package com.polyg7ot.lc.lvl4.lc0907;
+
+import java.util.Stack;
+
+public class SolutionApproachDPWithStack {
+    public int sumSubarrayMins(int[] nums) {
+        final int L = nums.length, MOD = (int)1e9 + 7;
+        int ans = 0;
+        
+        int[] dp = new int[L + 1];
+        
+        Stack<Integer> idxStack = new Stack<Integer>();
+        idxStack.push(-1);
+        
+        for(int i = 0; i < L; i++){
+            while(idxStack.peek() != -1 && nums[i] <= nums[idxStack.peek()]){
+                idxStack.pop();
+            }   
+            
+            dp[i + 1] = (dp[idxStack.peek() + 1] + (i - idxStack.peek()) * nums[i]) % MOD;
+            idxStack.push(i);
+            ans += dp[i + 1];
+            ans %= MOD;
+        }
+        
+        return ans;
+    }
+}
---
+++ src/main/java/com/polyg7ot/lc/lvl4/lc0907/SolutionApproachStack.java
@@ -0,0 +1,35 @@
+/**
+ * https://leetcode.com/problems/sum-of-subarray-minimums/
+ * 
+ * Time Complexity: O(N)
+ * Space Complexity: O(N)
+ * 
+ * https://leetcode.com/problems/sum-of-subarray-minimums/discuss/170750/C%2B%2BJavaPython-Stack-Solution
+ * https://leetcode.com/problems/sum-of-subarray-minimums/discuss/170750/C++JavaPython-Stack-Solution/337512
+ */
+package com.polyg7ot.lc.lvl4.lc0907;
+
+import java.util.Stack;
+
+public class SolutionApproachStack {
+    public int sumSubarrayMins(int[] nums) {
+        final int L = nums.length, MOD = (int)1e9 + 7;
+        int ans = 0, j = 0, k = 0;
+        
+        Stack<Integer> idxStack = new Stack<Integer>();
+        for(int i = 0; i <= L; i++){
+            while(!idxStack.isEmpty() && nums[idxStack.peek()] > (i == L ? 0 : nums[i])){
+                // only to pop out elements when nums[stack.peek()] > nums[i],
+                // nums[k] <= nums[j], nums[j] > nums[i]
+                j = idxStack.pop();
+                // nums[k] is the previous less or equal number
+                // nums[i] is the next less number
+                k = idxStack.isEmpty() ? -1 : idxStack.peek();
+                ans = (ans + nums[j] * (i - j) * (j - k)) % MOD;
+            }
+            idxStack.push(i);
+        }
+        
+        return ans;
+    }
+}
