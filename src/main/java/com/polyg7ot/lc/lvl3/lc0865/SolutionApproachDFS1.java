389998d4c12569ae662deb9d765d3f9df37bc1e3
Leon <yanglyu.leon.7@gmail.com>
2020-05-08 21:10:53 -0700

    lc0856: Score of Parentheses

--- src/main/java/com/polyg7ot/lc/lvl3/lc0865/SolutionApproachDFS1.java
+++ src/main/java/com/polyg7ot/lc/lvl3/lc0865/SolutionApproachDFS1.java
@@ -1,32 +1,32 @@
 /**
  * https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/
  */
-package com.polyg7ot.lc.lvl3.lc0865;
+// package com.polyg7ot.lc.lvl3.lc0865;
 
-import javafx.util.Pair;
+// import javafx.util.Pair;
 
-import com.polyg7ot.data_structure.tree.TreeNode;
+// import com.polyg7ot.data_structure.tree.TreeNode;
 
-public class SolutionApproachDFS1 {
-    public TreeNode subtreeWithAllDeepest(TreeNode root) {
-        return deep(root).getValue();
-    }
+// public class SolutionApproachDFS1 {
+//     public TreeNode subtreeWithAllDeepest(TreeNode root) {
+//         return deep(root).getValue();
+//     }
     
-    private Pair<Integer, TreeNode> deep(TreeNode node){
-        if(node == null) return new Pair(0, null);
+//     private Pair<Integer, TreeNode> deep(TreeNode node){
+//         if(node == null) return new Pair(0, null);
         
-        Pair<Integer, TreeNode> left = deep(node.left);
-        Pair<Integer, TreeNode> right = deep(node.right);
+//         Pair<Integer, TreeNode> left = deep(node.left);
+//         Pair<Integer, TreeNode> right = deep(node.right);
         
-        int leftVal = left.getKey();
-        int rightVal = right.getKey();
+//         int leftVal = left.getKey();
+//         int rightVal = right.getKey();
         
-        if(leftVal == rightVal) {
-            return new Pair(leftVal + 1, node);
-        }else if(leftVal > rightVal){
-            return new Pair(leftVal + 1, left.getValue());
-        }else{
-            return new Pair(rightVal + 1, right.getValue());
-        }
-    }
-}
+//         if(leftVal == rightVal) {
+//             return new Pair(leftVal + 1, node);
+//         }else if(leftVal > rightVal){
+//             return new Pair(leftVal + 1, left.getValue());
+//         }else{
+//             return new Pair(rightVal + 1, right.getValue());
+//         }
+//     }
+// }
