37a4e381706186e25e1bdcd0e863486e056bff28
Leon <yanglyu.leon.7@gmail.com>
2020-05-09 16:26:16 -0700

    lc0316: Remove Duplicate Letters

--- src/main/java/com/polyg7ot/lc/lvl4/lc0316/SolutionApproachStack.java
+++ src/main/java/com/polyg7ot/lc/lvl4/lc0316/SolutionApproachStack.java
@@ -3,6 +3,12 @@
  * 
  * Time Complexity: O(N)
  * Space Complexity: O(N)
+ * 
+ * to main a "strickly increasing" stack
+ *  1. the contents are actual characters
+ *  2. the order of characters is maintained in strictly increasing order, if possible 
+ *      by "if possible", it rather depends on the given String sequence,
+ *      in another words, how many letters left, and what they are
  */
 package com.polyg7ot.lc.lvl4.lc0316;
 
@@ -21,7 +27,7 @@ public class SolutionApproachStack {
         StringBuilder builder = new StringBuilder();
         for(int i = 0; i < L; i++){
             if(used[s.charAt(i) - 'a'] == 0){
-                // to pop out duplicates and bigger characters
+                // to pop out duplicates and bigger characters existing in the builder
                 while(builder.length() > 0
                       && builder.charAt(builder.length() - 1) >= s.charAt(i) 
                       && remaining[builder.charAt(builder.length() - 1) - 'a'] > 0){
