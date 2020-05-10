cd1922d2249bc8a3e36befdb6ed8bf040e4aee26
Leon <yanglyu.leon.7@gmail.com>
2020-05-10 14:40:25 -0700

    lc0946: Validate Stack Sequences

---
+++ src/main/java/com/polyg7ot/lc/lvl3/lc0946/SolutionApproachStack.java
@@ -0,0 +1,26 @@
+/**
+ * https://leetcode.com/problems/validate-stack-sequences/
+ * 
+ * Time Complexity: O(N)
+ * Space Complexity: O(N)
+ */
+package com.polyg7ot.lc.lvl3.lc0946;
+
+import java.util.Stack;
+
+public class SolutionApproachStack {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<Integer>();
+        
+        int idx = 0;
+        for(int num : pushed){
+            stack.push(num);
+            while(!stack.isEmpty() && stack.peek() == popped[idx]){
+                stack.pop();
+                idx++;
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
