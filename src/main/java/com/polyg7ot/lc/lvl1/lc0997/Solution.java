c5fa3e5941d5529ab7cd8c4fa362a92bf46149ed
Leon <yanglyu.leon.7@gmail.com>
2020-05-10 10:14:28 -0700

    lc0997: Find the Town Judge

---
+++ src/main/java/com/polyg7ot/lc/lvl1/lc0997/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * https://leetcode.com/problems/find-the-town-judge/
+ * 
+ * Time Complexity: O(N)
+ * Space Complexity: O(N)
+ */
+package com.polyg7ot.lc.lvl1.lc0997;
+
+public class Solution {
+    public int findJudge(int N, int[][] trust) {
+        int[] count = new int[N + 1];
+        for(int[] t : trust){
+            --count[t[0]];
+            ++count[t[1]];
+        }
+        
+        for(int i = 1; i <= N; i++){
+            if(count[i] == N - 1)
+                return i;
+        }
+        
+        return -1;
+    }
+}
