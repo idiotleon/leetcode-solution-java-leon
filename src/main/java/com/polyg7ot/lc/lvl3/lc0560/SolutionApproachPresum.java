e785695043e3d35a6e9c76ffe887b2b75656a942
Leon <yanglyu.leon.7@gmail.com>
2020-05-11 10:03:45 -0700

    lc0930: Binary Subarrays with Sum

--- src/main/java/com/polyg7ot/lc/lvl3/lc0560/SolutionApproachPresum.java
+++ src/main/java/com/polyg7ot/lc/lvl3/lc0560/SolutionApproachPresum.java
@@ -6,6 +6,10 @@
  * 
  * https://leetcode.com/problems/subarray-sum-equals-k/discuss/102106/Java-Solution-PreSum-%2B-HashMap
  * https://leetcode.com/problems/subarray-sum-equals-k/discuss/301242/General-summary-of-what-kind-of-problem-can-cannot-solved-by-Two-Pointers
+ * 
+ * about preSum.put(0, 1);
+ * https://leetcode.com/problems/subarray-sum-equals-k/discuss/102106/Java-Solution-PreSum-+-HashMap/416171
+ * https://leetcode.com/problems/subarray-sum-equals-k/discuss/102106/Java-Solution-PreSum-+-HashMap/238328
  */
 package com.polyg7ot.lc.lvl3.lc0560;
 
@@ -16,6 +20,13 @@ public class SolutionApproachPreSum {
     public int subarraySum(int[] nums, int k) {
         int sum = 0, ans = 0;
         Map<Integer, Integer> preSum = new HashMap<Integer, Integer>();
+        // this is for situtations where (sum - k) == 0,
+        // which are valid calculations, and should be counted.
+
+        // (Were all elements not zeros)
+        // Equivalently: if(sum == k) count++, in the loop
+
+        // to handle the special case, where the subarray begins at index 0 with (sum == k)
         preSum.put(0, 1);
         
         for(int num : nums){
---
+++ src/main/java/com/polyg7ot/lc/lvl4/lc0930/SolutionApproachPreSum1.java
@@ -0,0 +1,30 @@
+/**
+ * https://leetcode.com/problems/binary-subarrays-with-sum/
+ * 
+ * Time Complexity: O(N)
+ * Space Complexity: O(N)
+ */
+package com.polyg7ot.lc.lvl4.lc0930;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SolutionApproachPreSum1 {
+    public int numSubarraysWithSum(int[] nums, int S) {
+        Map<Integer, Integer> preSum = new HashMap<Integer, Integer>();
+        preSum.put(0, 1);
+        int sum = 0, ans = 0;
+        
+        for(int num : nums){
+            sum += num;
+            
+            if(sum >= S){
+                ans += preSum.getOrDefault(sum - S, 0);
+            }
+            
+            preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+        }
+        
+        return ans;
+    }
+}
