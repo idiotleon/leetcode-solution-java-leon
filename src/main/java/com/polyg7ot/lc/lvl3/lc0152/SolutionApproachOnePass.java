1c4936ccfa2ee53d31416b64851fd3de200911f8
Leon <yanglyu.leon.7@gmail.com>
2020-05-10 21:50:04 -0700

    lc0152: Maximum Product Subarray

---
+++ src/main/java/com/polyg7ot/lc/lvl3/lc0152/SolutionApproachOnePass.java
@@ -0,0 +1,31 @@
+/**
+ * https://leetcode.com/problems/maximum-product-subarray/
+ * 
+ * Time Complexity: O(N)
+ * Space Complexity: O(1)
+ * 
+ * Because that there are possibly negative numbers and 0(s), 
+ * the maximum product must come from either the previous minimum product(containing odd number of negative numbers) or the maximum product times the current value. 
+ * And then to compare the current value to get the current minimum or maximum product.
+ * Please note that do NOT involve any divide operation, because there might be a zero in the array, which costs extra code.
+ */
+package com.polyg7ot.lc.lvl3.lc0152;
+
+public class SolutionApproachOnePass {
+    public int maxProduct(int[] nums) {
+        int ans = Integer.MIN_VALUE;
+        int minProduct = 1, maxProduct = 1;
+        
+        for(int i = 0; i < nums.length; i++){
+            int curMin = Math.min(nums[i], Math.min(nums[i] * minProduct, nums[i] * maxProduct));
+            int curMax = Math.max(nums[i], Math.max(nums[i] * minProduct, nums[i] * maxProduct));
+            
+            minProduct = curMin;
+            maxProduct = curMax;
+            
+            ans = Math.max(ans, maxProduct);
+        }
+        
+        return ans;
+    }
+}
--- src/main/java/com/polyg7ot/lc/lvl3/lc0152/SolutionApproachTwoPtrs.java
+++ src/main/java/com/polyg7ot/lc/lvl3/lc0152/SolutionApproachTwoPtrs.java
@@ -1,19 +1,22 @@
 /**
  * https://leetcode.com/problems/maximum-product-subarray/
+ * 
+ * Time Complexity: O(N)
+ * Space Complexity: O(1)
  */
 package com.polyg7ot.lc.lvl3.lc0152;
 
 public class SolutionApproachTwoPtrs {
     public int maxProduct(int[] nums) {
         final int N = nums.length;
-        int res = nums[0], left = 0, right = 0;
+        int ans = nums[0], left = 0, right = 0;
         
         for(int i = 0; i < N; i++){
             left = (left == 0 ? 1 : left) * nums[i];
             right = (right == 0 ? 1 : right) * nums[N - 1 - i];
-            res = Math.max(res, Math.max(left, right));
+            ans = Math.max(ans, Math.max(left, right));
         }
         
-        return res;
+        return ans;
     }
 }
