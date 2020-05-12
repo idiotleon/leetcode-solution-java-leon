4934f071e158dee7e656f262aff5236b8191d460
Leon <yanglyu.leon.7@gmail.com>
2020-05-12 10:43:05 -0700

    lc0540: Single Element in a Sorted Array

---
+++ src/main/java/com/polyg7ot/lc/lvl4/lc0540/SolutionApproach2OnePass.java
@@ -0,0 +1,17 @@
+/**
+ * https://leetcode.com/problems/single-element-in-a-sorted-array/
+ * 
+ * Time Complexity: O(N)
+ * Space Complexity: O(1)
+ */
+package com.polyg7ot.lc.lvl4.lc0540;
+
+public class SolutionApproach2OnePass {
+    public int singleNonDuplicate(int[] nums) {
+        int ans = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+}
