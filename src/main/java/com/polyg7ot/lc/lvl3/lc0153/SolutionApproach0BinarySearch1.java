78b8d0a5061acb5846134a2033a5b9d2571c2abf
Leon <yanglyu.leon.7@gmail.com>
2020-05-13 20:00:06 -0700

    lc0153: Find Minimum in Rotated Sorted Array

--- src/main/java/com/polyg7ot/lc/lvl3/lc0153/SolutionApproach0BinarySearch1.java
+++ src/main/java/com/polyg7ot/lc/lvl3/lc0153/SolutionApproach0BinarySearch1.java
@@ -1,18 +1,21 @@
 /**
  * https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+ * 
+ * Time Complexity: O(lg(N))
+ * Space Complexity: O(1)
  */
 package com.polyg7ot.lc.lvl3.lc0153;
 
-public class SolutionApproachBinarySearch {
+public class SolutionApproach0BinarySearch1 {
     public int findMin(int[] nums) {
         // sanity check
         if (nums.length == 0)
             return -1;
         int left = 0, right = nums.length - 1;
-        if (nums[right] > nums[left])
-            return nums[0];
+        if (nums[left] <= nums[right])
+            return nums[left];
 
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
 
             if (nums[mid] > nums[mid + 1])
@@ -20,10 +23,11 @@ public class SolutionApproachBinarySearch {
             if (nums[mid - 1] > nums[mid])
                 return nums[mid];
 
-            if (nums[left] < nums[mid])
+            if (nums[left] < nums[mid]){
                 left = mid + 1;
-            else
+            }else{
                 right = mid;
+            }
         }
 
         return nums[left];
