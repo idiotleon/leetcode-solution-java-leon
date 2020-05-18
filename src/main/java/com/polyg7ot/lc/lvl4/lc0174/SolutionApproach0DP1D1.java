68eb01b7bf389866b5b7e8693573726431498da7
Leon <yanglyu.leon.7@gmail.com>
2020-05-18 16:37:21 -0700

    lc0174: Dungeon Game

---
+++ src/main/java/com/polyg7ot/lc/lvl4/lc0174/SolutionApproach0DP1D1.java
@@ -0,0 +1,33 @@
+/**
+ * https://leetcode.com/problems/dungeon-game/
+ * 
+ * Time Complexity: O(NR * NC)
+ * Space Complexity: O(NC)
+ * 
+ * References:
+ *  https://leetcode.com/problems/dungeon-game/discuss/52826/A-very-clean-and-intuitive-solution-(with-explanation)
+ *  https://leetcode.com/problems/dungeon-game/discuss/52897/My-java-solution-with-explanation-in-detail/53923
+ *  https://leetcode.com/problems/dungeon-game/discuss/52826/A-very-clean-and-intuitive-solution-(with-explanation)
+ */
+package com.polyg7ot.lc.lvl4.lc0174;
+
+import java.util.Arrays;
+
+public class SolutionApproach0DP1D1 {
+    public int calculateMinimumHP(int[][] dungeon) {
+        final int NR = dungeon.length, NC = dungeon[0].length;
+        
+        int[] dp = new int[NC + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        
+        dp[NC - 1] = 0;
+        for(int row = NR - 1; row >= 0; row--){
+            for(int col = NC - 1; col >= 0; col--){
+                dp[col] = Math.min(dp[col], dp[col + 1]) - dungeon[row][col];
+                dp[col] = Math.max(dp[col], 0);
+            }
+        }
+        
+        return dp[0] + 1;
+    }
+}
