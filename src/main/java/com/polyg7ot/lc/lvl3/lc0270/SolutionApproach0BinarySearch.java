bf0912ba0f0ab2001c3b6c384d806bc9e3865afb
Leon <yanglyu.leon.7@gmail.com>
2020-05-18 20:35:21 -0700

    lc0270: Closest Binary Search Tree Value
    fix: Correcting the class name

--- src/main/java/com/polyg7ot/lc/lvl3/lc0270/SolutionApproach0BinarySearch.java
+++ src/main/java/com/polyg7ot/lc/lvl3/lc0270/SolutionApproach0BinarySearch.java
@@ -1,11 +1,14 @@
 /**
  * https://leetcode.com/problems/closest-binary-search-tree-value/
+ * 
+ * Time Complexity: O(lg(N))
+ * Space Complexity: O(1)
  */
 package com.polyg7ot.lc.lvl3.lc0270;
 
 import com.polyg7ot.data_structure.tree.TreeNode;
 
-public class Solution {
+public class SolutionApproach0BinarySearch {
     public int closestValue(TreeNode root, double target) {
         int val, closest = root.val;
         
--- src/test/java/com/polyg7ot/lc/lvl2/lc0209/SolutionTestsApproachSlidingWindow.java
+++ src/test/java/com/polyg7ot/lc/lvl2/lc0209/SolutionTestsApproachSlidingWindow.java
@@ -8,11 +8,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.polyg7ot.lc.utils.Constants;
 
 class SolutionTestsApproachSlidingWindow {
-    private final SolutionApproachSlidngWindow approachSlidngWindow = new SolutionApproachSlidngWindow();
+    private final SolutionApproach0SlidngWindow solution = new SolutionApproach0SlidngWindow();
 
     @Test
     @DisplayName("209. Minimum Size Subarray Sum")
     void test() {
-        assertEquals(2, approachSlidngWindow.minSubArrayLen(7, Constants.Array.RANDOM));
+        assertEquals(2, solution.minSubArrayLen(7, Constants.Array.RANDOM));
     }
 }
