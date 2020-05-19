65b052aa4c961e7ef9afc58b66de26053d26287a
Leon <yanglyu.leon.7@gmail.com>
2020-05-18 21:24:10 -0700

    lc0287: Find the Duplicate Number

---
+++ src/main/java/com/polyg7ot/lc/lvl4/lc0287/SolutionApproach1OnePass.java
@@ -0,0 +1,25 @@
+/**
+ * https://leetcode.com/problems/find-the-duplicate-number/
+ * 
+ * Time Complexity: O(N)
+ * Space Complexity: O(N)
+ */
+package com.polyg7ot.lc.lvl4.lc0287;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SolutionApproach1OnePass {
+    public int findDuplicate(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        
+        Set<Integer> visited = new HashSet<Integer>();
+        
+        for(int num : nums){
+            if(visited.contains(num)) return num;
+            visited.add(num);
+        }
+        
+        return -1;
+    }
+}
