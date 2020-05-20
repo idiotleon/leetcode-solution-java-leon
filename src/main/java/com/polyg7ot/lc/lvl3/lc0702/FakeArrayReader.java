584af807abee950c8574d8909cada078abbd48b1
Leon <yanglyu.leon.7@gmail.com>
2020-05-20 15:28:32 -0700

    lc0702: Search in a Sorted Array of Unknown Size

---
+++ src/main/java/com/polyg7ot/lc/lvl3/lc0702/FakeArrayReader.java
@@ -0,0 +1,10 @@
+/**
+ * https://leetcode.com/problems/search-in-a-sorted-array-of-unknown-size/
+ */
+package com.polyg7ot.lc.lvl3.lc0702;
+
+public class FakeArrayReader {
+    public int get(int pos){
+        return -1;
+    }
+}
