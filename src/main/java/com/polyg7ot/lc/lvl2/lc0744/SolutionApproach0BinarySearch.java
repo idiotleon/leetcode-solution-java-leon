61fbb85fea4d3cc52c0c64a4ac86a143cd489e61
Leon <yanglyu.leon.7@gmail.com>
2020-05-21 23:13:52 -0700

    lc0744: Find Smallest Letter Greater Than Target

---
+++ src/main/java/com/polyg7ot/lc/lvl2/lc0744/SolutionApproach0BinarySearch.java
@@ -0,0 +1,26 @@
+/**
+ * https://leetcode.com/problems/find-smallest-letter-greater-than-target/
+ * 
+ * 
+ * References:
+ *  https://leetcode.com/problems/find-smallest-letter-greater-than-target/discuss/110005/Easy-Binary-Search-in-Java-O(log(n))-time
+ */
+package com.polyg7ot.lc.lvl2.lc0744;
+
+public class SolutionApproach0BinarySearch {
+    public char nextGreatestLetter(char[] letters, char target) {
+        if(letters == null || letters.length == 0) return 0;
+        
+        final int N = letters.length;
+        
+        int left = 0, right = N - 1;
+        
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(letters[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+        
+        return letters[left % N];
+    }
+}
