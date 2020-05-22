e0c2d51853b63685efb847b85d7d070503b99e0b
Leon <yanglyu.leon.7@gmail.com>
2020-05-22 01:17:34 -0700

    lc0451: Sort Characters By Frequency

---
+++ src/main/java/com/polyg7ot/lc/lvl2/lc0451/SolutionApproach0Sorting.java
@@ -0,0 +1,29 @@
+/**
+ * https://leetcode.com/problems/sort-characters-by-frequency/
+ * 
+ * Time Complexity: O(N * lg(N))
+ * Space Complexity: O(N)
+ */
+package com.polyg7ot.lc.lvl2.lc0451;
+
+import java.util.Arrays;
+
+public class SolutionApproach0Sorting {
+    public String frequencySort(String s) {
+        Character[] chs = s.chars().mapToObj(c -> (char)c).toArray(Character[]::new);
+        
+        int[] freq = new int[128];
+        for(char ch : chs){
+            ++freq[ch];
+        }
+        
+        Arrays.sort(chs, (a, b) -> freq[b] == freq[a] ? b - a : Integer.compare(freq[b], freq[a]));
+        
+        StringBuilder builder = new StringBuilder();
+        for(Character ch : chs){
+            builder.append(ch);
+        }
+        
+        return builder.toString();
+    }
+}
