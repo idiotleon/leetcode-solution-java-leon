d43123e66d66f4a68600c7452bb53f3594008a80
Leon <yanglyu.leon.7@gmail.com>
2020-05-25 15:53:41 -0700

    lc0938: Range Sum of BST

---
+++ src/main/java/com/polyg7ot/lc/lvl3/lc0938/SolutionApproach0DFSBottomUp.java
@@ -0,0 +1,23 @@
+/**
+ * https://leetcode.com/problems/range-sum-of-bst/
+ * 
+ * Time Complexity: O(N)
+ * 
+ * Space Complexity: O(H)
+ *  O(H), the call stack
+ * 
+ * References:
+ *  https://leetcode.com/problems/range-sum-of-bst/discuss/192019/JavaPython-3-3-similar-recursive-and-1-iterative-methods-w-comment-and-analysis.
+ */
+package com.polyg7ot.lc.lvl3.lc0938;
+
+import com.polyg7ot.data_structure.tree.TreeNode;
+
+public class SolutionApproach0DFSBottomUp {
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        if(root == null) return 0;
+        if(root.val > R) return rangeSumBST(root.left, L, R);   // left branch excluded
+        if(root.val < L) return rangeSumBST(root.right, L, R);  // right branch excluded
+        return root.val + rangeSumBST(root.left, L, R) + rangeSumBST(root.right, L, R); // count in both children
+    }
+}
---
+++ src/main/java/com/polyg7ot/lc/lvl3/lc0938/SolutionApproach0Iteration.java
@@ -0,0 +1,32 @@
+/**
+ * https://leetcode.com/problems/range-sum-of-bst/
+ * 
+ * Time Complexity: O(N)
+ * Space Complexity: O(H)
+ * 
+ * Referneces:
+ *  https://leetcode.com/problems/range-sum-of-bst/discuss/192019/JavaPython-3-3-similar-recursive-and-1-iterative-methods-w-comment-and-analysis.
+ */
+package com.polyg7ot.lc.lvl3.lc0938;
+
+import java.util.Stack;
+
+import com.polyg7ot.data_structure.tree.TreeNode;
+
+public class SolutionApproach0Iteration {
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        
+        int sum = 0;
+        while(!stack.isEmpty()){
+            TreeNode cur = stack.pop();
+            if(cur == null) continue;
+            if(cur.val > L) stack.push(cur.left);
+            if(cur.val < R) stack.push(cur.right);
+            if(cur.val >= L && cur.val <= R) sum += cur.val;
+        }
+        
+        return sum;
+    }
+}
