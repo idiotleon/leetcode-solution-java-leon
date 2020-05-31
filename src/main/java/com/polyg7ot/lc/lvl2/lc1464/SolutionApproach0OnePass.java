048a753eb43365d952992a9d00717eb13cd9dd62
Leon <yanglyu.leon.7@gmail.com>
2020-05-30 22:40:06 -0700

    lc1464: Maximum Product of Two Elements in an Array

---
+++ src/main/java/com/polyg7ot/lc/lvl2/lc1464/SolutionApproach0OnePass.java
@@ -0,0 +1,29 @@
+/**
+ * https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/
+ * 
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(N)
+ * 
+ * to find the max 2 numbers
+ * 
+ * References:
+ *  https://leetcode.com/problems/maximum-product-of-two-elements-in-an-array/discuss/661687/JavaPython-3-Find-the-max-2-numbers.
+ */
+package com.polyg7ot.lc.lvl2.lc1464;
+
+public class SolutionApproach0OnePass {
+    public int maxProduct(int[] nums) {
+        int m = Integer.MIN_VALUE, n = Integer.MIN_VALUE;
+        
+        for(int num : nums){
+            if(num >= m){
+                n = m;
+                m = num;
+            }else if(num > n){
+                n = num;
+            }
+        }
+        
+        return (m - 1) * (n - 1);
+    }
+}
