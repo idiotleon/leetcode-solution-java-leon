6be008cb247ce4ce1ea88b077d95ca3a0962b192
Leon <yanglyu.leon.7@gmail.com>
2020-06-01 18:41:31 -0700

    lc0673: Number of Longest Increasing Subsequence

---
+++ src/main/java/com/polyg7ot/lc/lvl4/lc0673/SolutionApproach0DP.java
@@ -0,0 +1,50 @@
+/**
+ * https://leetcode.com/problems/number-of-longest-increasing-subsequence/
+ * 
+ * Time Complexity:     O(N ^ 2)
+ * Space Complexity:    O(N)
+ * 
+ * References:
+ *  https://leetcode.com/problems/number-of-longest-increasing-subsequence/discuss/107318/C%2B%2B-DP-with-explanation-O(n2)
+ */
+package com.polyg7ot.lc.lvl4.lc0673;
+
+import java.util.Arrays;
+
+public class SolutionApproach0DP {
+    public int findNumberOfLIS(int[] nums) {
+        // sanity check
+        if(nums == null || nums.length == 0) return 0;
+        
+        final int N = nums.length;
+        int[] lens = new int[N], counts = new int[N];
+        Arrays.fill(lens, 1);
+        Arrays.fill(counts, 1);
+        
+        int maxLen = 0, ans = 0;
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < i; j++){
+                if(nums[i] > nums[j]){
+                    if(lens[i] == lens[j] + 1) {
+                        counts[i] += counts[j];
+                    }
+
+                    if(lens[i] < lens[j] + 1){
+                        lens[i] = lens[j] + 1;
+                        counts[i] = counts[j];
+                    }
+                }
+            
+                maxLen = Math.max(maxLen, lens[i]);
+            }
+        }
+        
+        for(int i = 0; i < N; i++){
+            if(lens[i] == maxLen){
+                ans += counts[i];
+            }
+        }
+        
+        return ans;
+    }
+}
