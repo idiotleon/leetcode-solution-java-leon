bb55767c8cf75a3f19194e43736641af6e7b04a6
Leon <yanglyu.leon.7@gmail.com>
2020-06-08 18:00:41 -0700

    lc1368: Minimum Cost to Make at Least One Valid Path in a Grid

---
+++ src/main/java/com/zea7ot/companies/google/graph/how_many_blocks_turned_upside_down/SolutionApproachDijkstrasAlgorithm.java
@@ -0,0 +1,15 @@
+/**
+ * 给一个matrix类似于
+ *  1 0 0 0 1
+ *  0 1 0 1 1
+ *  0 0 0 1 1   
+ *  1 0 1 1 1
+ *  左上角和右下角都是1，1代表path可以直接走，0代表block，不能走，除非把0变成1
+ * 
+ * 问从左上走到右下最少需要把多少个block(0)变成(flip) path(1)
+ */
+package com.zea7ot.companies.google.graph.how_many_blocks_turned_upside_down;
+
+public class SolutionApproachDijkstrasAlgorithm {
+    
+}
---
+++ src/main/java/com/zea7ot/lc/lvl4/lc1368/SolutionApproach1DP2Dimen.java
@@ -0,0 +1,11 @@
+/**
+ * https://leetcode.com/problems/minimum-cost-to-make-at-least-one-valid-path-in-a-grid/
+ * 
+ * References:
+ *  https://leetcode.com/problems/minimum-cost-to-make-at-least-one-valid-path-in-a-grid/discuss/524845/C%2B%2B96ms-Forward-and-Backward-DP-Bounded-by-O((m%2Bn)mn)
+ */
+package com.zea7ot.lc.lvl4.lc1368;
+
+public class SolutionApproach1DP2Dimen {
+    
+}
