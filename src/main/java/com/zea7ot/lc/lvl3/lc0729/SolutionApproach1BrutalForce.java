db32dbe5eabbc64126b3ecdc37fd71bdefb139d5
Leon <yanglyu.leon.7@gmail.com>
2020-06-19 13:14:35 -0700

    lc0731: My Calendar II

--- src/main/java/com/zea7ot/lc/lvl3/lc0729/SolutionApproach1BrutalForce.java
+++ src/main/java/com/zea7ot/lc/lvl3/lc0729/SolutionApproach1BrutalForce.java
@@ -1,7 +1,7 @@
 /**
  * https://leetcode.com/problems/my-calendar-i/
  * 
- * Time Complexity:     O(N)
+ * Time Complexity:     O(N ^ 2)
  * Space Complexity:    O(N)
  */
 package com.zea7ot.lc.lvl3.lc0729;
@@ -10,20 +10,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SolutionApproach1BrutalForce {
-    private List<int[]> booked;
+    private List<int[]> bookings;
 
     public SolutionApproach1BrutalForce() {
-        this.booked = new ArrayList<int[]>();
+        this.bookings = new ArrayList<int[]>();
     }
     
     public boolean book(int start, int end) {
-        for(int[] appointment : booked){
-            if(Math.max(appointment[0], start) < Math.min(appointment[1], end)){
+        for(int[] booking : bookings){
+            if(Math.max(booking[0], start) < Math.min(booking[1], end)){
                 return false;
             }
         }
         
-        booked.add(new int[]{start, end});
+        bookings.add(new int[]{start, end});
         return true;
     }
 }
