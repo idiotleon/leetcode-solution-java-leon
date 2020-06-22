ee9f949296911d8367f27223a069457102b81a66
Leon <yanglyu.leon.7@gmail.com>
2020-06-22 01:51:30 -0700

    lc0137: Single Number II

--- src/main/java/com/zea7ot/lc/lvl3/lc0137/SolutionApproach1HashSet.java
+++ src/main/java/com/zea7ot/lc/lvl3/lc0137/SolutionApproach1HashSet.java
@@ -1,12 +1,15 @@
 /**
  * https://leetcode.com/problems/single-number-ii/
+ * 
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(N)
  */
 package com.zea7ot.lc.lvl3.lc0137;
 
 import java.util.HashSet;
 import java.util.Set;
 
-public class SolutionApproachHashSet {
+public class SolutionApproach1HashSet {
     public int singleNumber(int[] nums) {
         Set<Long> set = new HashSet<Long>();
         long sumSet = 0, sumArray = 0;
