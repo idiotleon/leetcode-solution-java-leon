0ad3bfc8ee1b66508fce4f5bafaebd10662a3b35
Leon <yanglyu.leon.7@gmail.com>
2020-06-24 15:46:23 -0700

    lc1485: Clone Binary Tree with Random Pointer

---
+++ src/main/java/com/zea7ot/lc/lvl3/lc1485/NodeCopy.java
@@ -0,0 +1,20 @@
+package com.zea7ot.lc.lvl3.lc1485;
+
+class NodeCopy {
+    protected int val;
+    protected NodeCopy left;
+    protected NodeCopy right;
+    protected NodeCopy random;
+
+    protected NodeCopy(int val){
+        this.val = val;
+        this.left = this.right = this.random = null;
+    }
+
+    protected NodeCopy(int val, NodeCopy left, NodeCopy right, NodeCopy random){
+        this.val = val;
+        this.left = left;
+        this.right = right;
+        this.random = random;
+    }
+}
