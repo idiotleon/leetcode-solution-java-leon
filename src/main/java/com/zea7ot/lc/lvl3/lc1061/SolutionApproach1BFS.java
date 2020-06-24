a63855c8fb8046cbd258d5ca777babe9d154ab85
Leon <yanglyu.leon.7@gmail.com>
2020-06-24 16:11:02 -0700

    1061: Lexicographically Smallest Equivalent String

---
+++ src/main/java/com/zea7ot/lc/lvl3/lc1061/SolutionApproach1BFS.java
@@ -0,0 +1,11 @@
+/**
+ * https://leetcode.com/problems/lexicographically-smallest-equivalent-string/
+ * 
+ * References:
+ *  https://leetcode.com/problems/lexicographically-smallest-equivalent-string/discuss/412777/python-bfsdfsunion-find-complexity-analysis
+ */
+package com.zea7ot.lc.lvl3.lc1061;
+
+public class SolutionApproach1BFS {
+    
+}
