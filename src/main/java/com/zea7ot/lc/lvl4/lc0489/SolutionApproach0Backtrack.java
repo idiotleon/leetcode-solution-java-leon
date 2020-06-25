91f6bc97014c571d26d1b02273b2d92f0fc5a4d6
Leon <yanglyu.leon.7@gmail.com>
2020-06-24 19:00:34 -0700

    lc0489: Robot Room Cleaner

---
+++ src/main/java/com/zea7ot/lc/lvl4/lc0489/SolutionApproach0Backtrack.java
@@ -0,0 +1,48 @@
+/**
+ * https://leetcode.com/problems/robot-room-cleaner/
+ * 
+ * References:
+ *  https://leetcode.com/problems/robot-room-cleaner/discuss/153530/DFS-Logical-Thinking
+ */
+package com.zea7ot.lc.lvl4.lc0489;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class SolutionApproach0Backtrack {
+    private static final int[][] DIRS = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    
+    public void cleanRoom(FakeRobot robot) {
+        Set<String> visited = new HashSet<String>();
+        backtrack(0, 0, 0, visited, robot);
+    }
+    
+    private void backtrack(int row, int col, int dir, Set<String> visited, FakeRobot robot){
+        robot.clean();
+        visited.add(hash(row, col));
+        
+        for(int d = 0; d < 4; d++){
+            int newDir = (dir + d) % 4;
+            int r = row + DIRS[newDir][0], c = col + DIRS[newDir][1];
+            if(!visited.contains(hash(r, c)) && robot.move()){
+                backtrack(r, c, newDir, visited, robot);
+            }
+
+            // to change the direction
+            robot.turnRight();
+        }
+        
+        // to resume/backtrack to the previous state, including BOTH:
+        //  1. position
+        //  2. direction
+        robot.turnRight();
+        robot.turnRight();
+        robot.move();
+        robot.turnRight();
+        robot.turnRight();
+    }
+    
+    private String hash(int row, int col){
+        return row + "," + col;
+    }
+}
