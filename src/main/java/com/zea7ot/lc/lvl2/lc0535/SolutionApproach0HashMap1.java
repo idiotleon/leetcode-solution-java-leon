a94d770eb3db1cd067caa0632c7b94d37f7e689c
Leon <yanglyu.leon.7@gmail.com>
2020-06-26 13:47:00 -0700

    lc0535: Encode and Decode TinyURL

---
+++ src/main/java/com/zea7ot/lc/lvl2/lc0535/SolutionApproach0HashMap1.java
@@ -0,0 +1,23 @@
+/**
+ * https://leetcode.com/problems/encode-and-decode-tinyurl/
+ */
+package com.zea7ot.lc.lvl2.lc0535;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SolutionApproach0HashMap1 {
+    private Map<Integer, String> map = new HashMap<Integer, String>();
+    private static final String TINY_URL = "http://tinyurl.com/";
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        map.put(longUrl.hashCode(), longUrl);
+        return TINY_URL + longUrl.hashCode();
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        return map.get(Integer.parseInt(shortUrl.replace(TINY_URL, "")));
+    }
+}
