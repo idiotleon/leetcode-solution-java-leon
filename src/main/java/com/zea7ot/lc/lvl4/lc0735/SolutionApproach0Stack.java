5a2a500bd488d90f25c72c992ca4ddf5d65c7a81
Leon <yanglyu.leon.7@gmail.com>
2020-06-29 10:45:35 -0700

    lc0735: Asteroid Collision

---
+++ src/main/java/com/zea7ot/lc/lvl4/lc0735/SolutionApproach0Stack.java
@@ -0,0 +1,34 @@
+/**
+ * https://leetcode.com/problems/asteroid-collision/
+ * 
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(N)
+ */
+package com.zea7ot.lc.lvl4.lc0735;
+
+import java.util.Arrays;
+
+public class SolutionApproach0Stack {
+    public int[] asteroidCollision(int[] asteroids) {
+        // sanity check
+        if(asteroids == null || asteroids.length == 0) return new int[0];
+        final int N = asteroids.length;
+        int[] stack = new int[N];
+        
+        int top = -1;
+        for(int asteriod : asteroids){
+            if(asteriod > 0) stack[++top] = asteriod;
+            else{
+                // to pop out all smaller(absolute values) asteroids
+                while(top > -1 && stack[top] > 0 && stack[top] < -asteriod) top--;
+                
+                // to save it were all left elements going-left asteroids
+                if(top == -1 || stack[top] < 0) stack[++top] = asteriod;
+                // to demolish both, if they are equal(absolute value)
+                else if(stack[top] == -asteriod) top--;
+            }
+        }
+        
+        return Arrays.copyOf(stack, top + 1);
+    }   
+}
--- src/main/java/com/zea7ot/lc/lvl4/lc0735/SolutionApproach0Stack3.java
+++ src/main/java/com/zea7ot/lc/lvl4/lc0735/SolutionApproach0Stack3.java
@@ -1,8 +1,8 @@
 /**
  * https://leetcode.com/problems/asteroid-collision/
  * 
- * Time Complexity: O(N)
- * Space Complexity: O(N)
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(N)
  */
 package com.zea7ot.lc.lvl4.lc0735;
 
@@ -10,30 +10,26 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class SolutionApproachStack2 {
+public class SolutionApproach0Stack3 {
     public int[] asteroidCollision(int[] asteroids) {
+        // sanity check
+        if(asteroids == null || asteroids.length == 0) return new int[0];
+
+        final int N = asteroids.length;
         List<Integer> ans = new ArrayList<Integer>();
         LinkedList<Integer> stack = new LinkedList<Integer>();
         
-        for(int i = 0; i < asteroids.length; i++){
+        for(int i = 0; i < N; i++){
             int cur = asteroids[i];
             if(cur < 0){
-                while(!stack.isEmpty() && stack.peekLast() < Math.abs(cur)){
-                    stack.removeLast();
-                }
+                while(!stack.isEmpty() && stack.peekLast() < Math.abs(cur)) stack.removeLast();
                     
                 if(stack.isEmpty()) ans.add(cur);
-                else if(stack.peekLast() == Math.abs(cur)){
-                    stack.removeLast();
-                }
-            }else{
-                stack.add(cur);
-            }
+                else if(stack.peekLast() == Math.abs(cur)) stack.removeLast();
+            }else stack.add(cur);
         }
         
-        while(!stack.isEmpty()){
-            ans.add(stack.removeFirst());
-        }
+        while(!stack.isEmpty()) ans.add(stack.removeFirst());
         
         return ans.stream().mapToInt(i -> i).toArray();
     }
