f718f28ebae5ab1f984a3432c2f344bbf4845459
Leon <yanglyu.leon.7@gmail.com>
2020-07-01 10:54:49 -0700

    lc0384: Shuffle An Array

---
+++ src/main/java/com/zea7ot/lc/lvl2/lc0384/SolutionApproach1Swap.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ * References:
+ *  https://leetcode.com/problems/shuffle-an-array/discuss/85958/First-Accepted-Solution-Java
+ */
+package com.zea7ot.lc.lvl2.lc0384;
+
+import java.util.Random;
+
+public class SolutionApproach1Swap {
+    private int[] nums;
+    private Random random;
+
+    public SolutionApproach1Swap(int[] nums) {
+        this.nums = nums;
+        random = new Random();
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    public int[] reset() {
+        return nums;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    public int[] shuffle() {
+        if(nums == null) return null;
+        int[] clone = nums.clone();
+        for(int j = 1; j < clone.length; ++j){
+            int i = random.nextInt(j + 1);
+            swap(clone, i, j);
+        }
+        return clone;
+    }
+    
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
