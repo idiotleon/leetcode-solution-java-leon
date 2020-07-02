4bd6157d1f9095bb54285c5a264e6afbed97ee94
Leon <yanglyu.leon.7@gmail.com>
2020-07-02 14:46:36 -0700

    lc0690: Employee Importance

---
+++ src/main/java/com/zea7ot/lc/lvl2/lc0690/FakeEmployee.java
@@ -0,0 +1,9 @@
+package com.zea7ot.lc.lvl2.lc0690;
+
+import java.util.List;
+
+class FakeEmployee {
+    protected int id;
+    protected int importance;
+    protected List<Integer> subordinates;
+}
