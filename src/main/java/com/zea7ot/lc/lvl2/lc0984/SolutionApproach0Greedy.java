aa3b2c85bf2bc05f69c94b8786721a441de0e440
Leon <yanglyu.leon.7@gmail.com>
2020-07-02 11:56:12 -0700

    lc0984: String without AAA or BBB

---
+++ src/main/java/com/zea7ot/lc/lvl2/lc0984/SolutionApproach0Greedy.java
@@ -0,0 +1,34 @@
+/**
+ * https://leetcode.com/problems/string-without-aaa-or-bbb/
+ * 
+ * Time Complexity:     O()
+ * Space Complexity:    O()
+ * 
+ * References:
+ *  https://leetcode.com/problems/string-without-aaa-or-bbb/discuss/226649/JavaC++-(and-Python)-simple-greedy
+ */
+package com.zea7ot.lc.lvl2.lc0984;
+
+public class SolutionApproach0Greedy {
+    public String strWithout3a3b(int A, int B) {
+        return A > B ? strWithout3a3b(A, B, 'a', 'b') : strWithout3a3b(B, A, 'b', 'a');
+    }
+    
+    private String strWithout3a3b(int A, int B, char a, char b){
+        StringBuilder builder = new StringBuilder();
+        int countA = A, countB = B;
+        while(countA-- > 0){
+            builder.append(a);
+            if(countA > countB){
+                builder.append(a);
+                --countA;
+            }
+            
+            if(countB-- > 0){
+                builder.append(b);
+            }
+        }
+        
+        return builder.toString();
+    }
+}
