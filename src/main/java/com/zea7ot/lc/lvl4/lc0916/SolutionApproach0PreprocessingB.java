08c950809e6afd4f1aaf57ae98d9331ada127818
Leon <yanglyu.leon.7@gmail.com>
2020-07-03 18:42:39 -0700

    lc0916: Word Subsets

---
+++ src/main/java/com/zea7ot/lc/lvl4/lc0916/SolutionApproach0PreprocessingB.java
@@ -0,0 +1,46 @@
+/**
+ * https://leetcode.com/problems/word-subsets/
+ * 
+ * Time Complexity:     O(LEN(A) + LEN(B))
+ * Space Complexity:    O(LEN(A) + LEN(B))
+ * 
+ * References:
+ *  https://leetcode.com/problems/word-subsets/discuss/175950/Easy-Java-Solution-Preprocess-Array-B
+ */
+package com.zea7ot.lc.lvl4.lc0916;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SolutionApproach0PreprocessingB {
+    public List<String> wordSubsets(String[] A, String[] B) {
+        int[] freq = new int[26];
+        for(String strB : B){
+            int[] freqB = getFreq(strB);
+            for(int i = 0; i < 26; i++){
+                freq[i] = Math.max(freq[i], freqB[i]);
+            }
+        }
+        
+        List<String> ans = new ArrayList<String>();
+        for(String strA : A){
+            int[] freqA = getFreq(strA);
+            int count = 0;
+            for(int i = 0; i < 26; i++){
+                if(freq[i] <= freqA[i]){
+                    count++;
+                }
+            }
+            
+            if(count == 26) ans.add(strA);
+        }
+        return ans;
+    }
+    
+    private int[] getFreq(String str){
+        int[] freq = new int[26];
+        for(char ch : str.toCharArray())
+            ++freq[ch - 'a'];
+        return freq;
+    }
+}
