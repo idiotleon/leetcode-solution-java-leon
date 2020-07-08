82565763318dbf1edff68fe457fcabe7f8878f9c
Leon <yanglyu.leon.7@gmail.com>
2020-07-08 11:02:34 -0700

    lc0235: Lowest Common Ancestor of a Binary Search Tree

---
+++ src/main/java/com/zea7ot/lc/lvl3/lc0235/SolutionApproach0Recursion.java
@@ -0,0 +1,22 @@
+/**
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ * 
+ * Time Complexity:     O(H)
+ * Space Complexity:    O(H)
+ */
+package com.zea7ot.lc.lvl3.lc0235;
+
+import com.zea7ot.utils.data_structure.tree.TreeNode;
+
+public class SolutionApproach0Recursion {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return null;
+        
+        int val = root.val;
+        if(val > p.val && val > q.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }else if(val < p.val && val < q.val){
+            return lowestCommonAncestor(root.right, p, q);
+        }else return root;
+    }
+}
