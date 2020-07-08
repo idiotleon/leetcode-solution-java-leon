8c7756bae233746d7ed6c17d3a078a7319799444
Leon <yanglyu.leon.7@gmail.com>
2020-07-08 11:15:08 -0700

    lc0236: Lowest Common Ancestor of a Binary Tree

--- src/main/java/com/zea7ot/lc/lvl3/lc0236/SolutionApproach0DFSBottomUp.java
+++ src/main/java/com/zea7ot/lc/lvl3/lc0236/SolutionApproach0DFSBottomUp.java
@@ -1,19 +1,26 @@
 /**
  * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+ * 
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(H)
+ * 
+ * References:
+ *  https://www.youtube.com/watch?v=13m9ZCB8gjw
  */
 package com.zea7ot.lc.lvl3.lc0236;
 
 import com.zea7ot.utils.data_structure.tree.TreeNode;
 
-public class SolutionApproachDFSBottomUp1 {
+public class SolutionApproach0DFSBottomUp {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null || root == p || root == q) return root;
+        if(root == null) return null;
+        if(root == p || root == q) return root;
         
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
         
-        if(left == null) return right;
-        else if(right == null) return left;
-        else return root;
+        if(left != null && right != null) return root;
+        
+        return left == null ? right : left;
     }
 }
