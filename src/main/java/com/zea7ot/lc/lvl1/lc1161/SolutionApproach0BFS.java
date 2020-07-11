bb6b00aa3f7881e84c1fe67639dfab735cec8aa8
Leon <yanglyu.leon.7@gmail.com>
2020-07-10 18:37:27 -0700

    lc1161: Maximum Level Sum of a Binary Tree

---
+++ src/main/java/com/zea7ot/lc/lvl1/lc1161/SolutionApproach0BFS.java
@@ -0,0 +1,43 @@
+/**
+ * https://leetcode.com/problems/maximum-level-sum-of-a-binary-tree/
+ * 
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(W)
+ */
+package com.zea7ot.lc.lvl1.lc1161;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import com.zea7ot.utils.data_structure.tree.TreeNode;
+
+public class SolutionApproach0BFS {
+    public int maxLevelSum(TreeNode root) {
+        // sanity check
+        if(root == null) return 0;
+
+        int maxSum = Integer.MIN_VALUE, maxLevel = 1;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        
+        int level = 0;
+        while(!queue.isEmpty()){
+            final int SIZE = queue.size();
+            int sum = 0;
+            ++level;
+            for(int i = 0; i < SIZE; i++){
+                TreeNode top = queue.poll();
+                sum += top.val;
+                if(top.left != null) queue.offer(top.left);
+                if(top.right != null) queue.offer(top.right);
+            }
+            
+            if(maxSum < sum){
+                maxSum = sum;
+                maxLevel = level;
+            }
+        }
+        
+        return maxLevel;
+    }
+}
