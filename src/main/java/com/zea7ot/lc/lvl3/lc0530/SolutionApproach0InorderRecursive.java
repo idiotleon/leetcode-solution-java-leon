9fc4735be251d524ea2f325985ab698bdcd1231f
Leon <yanglyu.leon.7@gmail.com>
2020-07-17 09:58:52 -0700

    lc0530 & lc 0783

---
+++ src/main/java/com/zea7ot/lc/lvl3/lc0530/SolutionApproach0InorderRecursive.java
@@ -0,0 +1,37 @@
+/**
+ * https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+ * 
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(H)
+ * 
+ * References:
+ *  https://leetcode.com/problems/minimum-absolute-difference-in-bst/discuss/99905/Two-Solutions-in-order-traversal-and-a-more-general-way-using-TreeSet
+ */
+package com.zea7ot.lc.lvl3.lc0530;
+
+import com.zea7ot.utils.data_structure.tree.TreeNode;
+
+public class SolutionApproach0InorderRecursive {
+    private int min;
+    private TreeNode prev;
+    
+    public int getMinimumDifference(TreeNode root) {
+        // sanity check
+        if(root == null) return min;
+        this.min = Integer.MAX_VALUE;
+        this.prev = null;
+        
+        inorder(root);
+        return min;
+    }
+    
+    private void inorder(TreeNode node){
+        if(node == null) return;
+        inorder(node.left);
+        if(prev != null)
+            min = Math.min(min, node.val - prev.val);
+        
+        prev = node;
+        inorder(node.right);
+    }
+}
