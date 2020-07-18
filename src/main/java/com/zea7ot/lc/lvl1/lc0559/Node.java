972ecbb0dfd0da21e46f5533368b08f0d32ed5ab
Leon <yanglyu.leon.7@gmail.com>
2020-07-18 10:38:29 -0700

    lc0559: Maximum Depth of N-ary Tree

---
+++ src/main/java/com/zea7ot/lc/lvl1/lc0559/Node.java
@@ -0,0 +1,17 @@
+package com.zea7ot.lc.lvl1.lc0559;
+
+import java.util.List;
+
+class Node {
+    protected int val;
+    protected List<Node> children;
+
+    protected Node(int val){
+        this.val = val;
+    }
+
+    protected Node(int val, List<Node> children){
+        this.val = val;
+        this.children = children;
+    }
+}
