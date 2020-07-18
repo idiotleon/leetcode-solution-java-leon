5029387b997a8b979894f0da13ec3428800a8ca1
Leon <yanglyu.leon.7@gmail.com>
2020-07-17 20:40:48 -0700

    lc0637: Average of Levels in Binary Tree

---
+++ src/main/java/com/zea7ot/lc/lvl2/lc0637/SolutionApproach0BFS.java
@@ -0,0 +1,42 @@
+/**
+ * https://leetcode.com/problems/average-of-levels-in-binary-tree/
+ * 
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(W)
+ * 
+ * References:
+ *  https://leetcode.com/problems/average-of-levels-in-binary-tree/discuss/105107/Java-BFS-Solution
+ */
+package com.zea7ot.lc.lvl2.lc0637;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+import com.zea7ot.utils.data_structure.tree.TreeNode;
+
+public class SolutionApproach0BFS {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> ans = new ArrayList<Double>();
+        // sanity check
+        if(root == null) return ans;
+        
+        Deque<TreeNode> queue = new ArrayDeque<TreeNode>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            final int SIZE = queue.size();
+            double sum = 0.0;
+            for(int i = 0; i < SIZE; i++){
+                TreeNode node = queue.poll();
+                sum += node.val;
+                if(node.left != null) queue.offer(node.left);
+                if(node.right != null) queue.offer(node.right);
+            }
+            
+            ans.add(sum / SIZE);
+        }
+        
+        return ans;
+    }
+}
