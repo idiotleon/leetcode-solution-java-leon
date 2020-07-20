739e1d58335398000f4b560aae0b55531c731d62
Leon <yanglyu.leon.7@gmail.com>
2020-07-20 15:37:51 -0700

    lc0376: Wiggle Subsequence

--- src/main/java/com/zea7ot/lc/lvl2/lc0203/Solution.java
+++ src/main/java/com/zea7ot/lc/lvl2/lc0203/Solution.java
@@ -1,5 +1,8 @@
 /**
  * https://leetcode.com/problems/remove-linked-list-elements/
+ * 
+ * Time Complexity:     O(L)
+ * Space Complexity:    O(1)
  */
 package com.zea7ot.lc.lvl2.lc0203;
 
@@ -7,6 +10,7 @@ import com.zea7ot.utils.data_structure.linkedlist.ListNode;
 
 public class Solution {
     public ListNode removeElements(ListNode head, int val) {
+        // sanity check
         if(head == null) return head;
         
         ListNode dummy = new ListNode(-1), prev = dummy;
