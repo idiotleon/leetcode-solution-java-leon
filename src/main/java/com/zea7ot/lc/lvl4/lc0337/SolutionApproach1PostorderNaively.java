a7984d380a6bc211d8bab89646d694f4a3188ed4
Leon <yanglyu.leon.7@gmail.com>
2020-07-22 16:05:29 -0700

    lc0337: House Robber III

--- src/main/java/com/zea7ot/lc/lvl4/lc0337/SolutionApproach1PostorderNaively.java
+++ src/main/java/com/zea7ot/lc/lvl4/lc0337/SolutionApproach1PostorderNaively.java
@@ -1,11 +1,17 @@
 /**
  * https://leetcode.com/problems/house-robber-iii/
+ * 
+ * Time Complexity:     ?O(N)
+ * Space Complexity:    O(H)
+ * 
+ * References:
+ *  https://leetcode.com/problems/house-robber-iii/discuss/79330/Step-by-step-tackling-of-the-problem
  */
 package com.zea7ot.lc.lvl4.lc0337;
 
 import com.zea7ot.utils.data_structure.tree.TreeNode;
 
-public class SolutionApproachDFS {
+public class SolutionApproach1PostorderNaively {
     public int rob(TreeNode root) {
         if(root == null) return 0;
         
