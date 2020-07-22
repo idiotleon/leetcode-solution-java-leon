75c2fe7a0c0b9b06f3276a73bb684eb830b247de
Leon <yanglyu.leon.7@gmail.com>
2020-07-21 17:11:28 -0700

    lc0646: Maximum Length of Pair Chain

--- src/main/java/com/zea7ot/lc/lvl4/lc0646/SolutionApproach1DP1.java
+++ src/main/java/com/zea7ot/lc/lvl4/lc0646/SolutionApproach1DP1.java
@@ -16,21 +16,20 @@ public class SolutionApproach1DP1 {
         // sanity check
         if(pairs == null || pairs.length < 2) return 0;
         
-        final int L = pairs.length;
-        Arrays.sort(pairs, (a, b) -> a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+        final int N = pairs.length;
+        Arrays.sort(pairs, (a, b) -> a[0] == b[0] ? Integer.compare(a[1], b[1]) : Integer.compare(a[0], b[0]));
         
-        int[] dp = new int[L];
+        int[] dp = new int[N];
         Arrays.fill(dp, 1);
         
-        for(int i = 0; i < L; i++){
-            for(int j = i - 1; j >= 0; j--){
+        for(int i = 0; i < N; ++i){
+            for(int j = i - 1; j >= 0; --j){
                 if(pairs[i][0] > pairs[j][1]){
                     dp[i] = dp[j] + 1;
-                    break;
                 }
             }
         }
         
-        return dp[L - 1];
+        return dp[N - 1];
     }
 }
