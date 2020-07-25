3e37c29746366c95f198586fd8037725be25781d
Leon <yanglyu.leon.7@gmail.com>
2020-07-25 14:40:57 -0700

    lc0134, 0135, 0654

--- src/main/java/com/zea7ot/lc/lvl3/lc0654/SolutionApproach0Deque.java
+++ src/main/java/com/zea7ot/lc/lvl3/lc0654/SolutionApproach0Deque.java
@@ -2,7 +2,7 @@
  * https://leetcode.com/problems/maximum-binary-tree/
  * 
  * Time Complexity:     O(N)
- * Space Complexity:    O()
+ * Space Complexity:    O(N)
  * 
  * References:
  *  https://leetcode.com/problems/maximum-binary-tree/discuss/106156/Java-worst-case-O(N)-solution/143674
@@ -18,20 +18,22 @@ import com.zea7ot.utils.data_structure.tree.TreeNode;
 public class SolutionApproach0Deque {
     public TreeNode constructMaximumBinaryTree(int[] nums) {
         // sanity check
-        if(nums == null || nums.length == 0) return null;
+        if (nums == null || nums.length == 0)
+            return null;
         final int N = nums.length;
-        
+
         Deque<TreeNode> stack = new ArrayDeque<TreeNode>();
-        for(int i = 0; i < N; i++){
+        for (int i = 0; i < N; i++) {
             TreeNode root = new TreeNode(nums[i]);
-            while(!stack.isEmpty() && stack.peek().val < nums[i])
+            while (!stack.isEmpty() && stack.peek().val < nums[i])
                 root.left = stack.pop();
-            
-            if(!stack.isEmpty()) stack.peek().right = root;
-            
+
+            if (!stack.isEmpty())
+                stack.peek().right = root;
+
             stack.push(root);
         }
-        
+
         return stack.isEmpty() ? null : stack.removeLast();
     }
 }
