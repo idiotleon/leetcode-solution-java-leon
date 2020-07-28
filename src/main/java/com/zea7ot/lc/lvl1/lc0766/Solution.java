57dcc813e2215113e8e791c8cca80e9e942c421b
Leon <yanglyu.leon.7@gmail.com>
2020-07-28 09:30:39 -0700

    lc0766: Toeplitz Matrix

---
+++ src/main/java/com/zea7ot/lc/lvl1/lc0766/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * https://leetcode.com/problems/toeplitz-matrix/
+ * 
+ * Time Complexity:     O(NR * NC)
+ * Space Complexity:    O(1)
+ * 
+ * References:
+ *  https://leetcode.com/problems/toeplitz-matrix/discuss/113417/Java-solution-4-liner.
+ */
+package com.zea7ot.lc.lvl1.lc0766;
+
+public class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        final int NR = matrix.length, NC = matrix[0].length;
+        for(int row = 0; row < NR - 1; ++row){
+            for(int col = 0; col < NC - 1; ++col){
+                if(matrix[row][col] != matrix[row + 1][col + 1])
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+}
