abdd4efea458b5ec4199d023c2de13c8396a7bae
Leon <yanglyu.leon.7@gmail.com>
2020-07-28 09:30:39 -0700

    lc0766: Toeplitz Matrix

---
+++ src/main/java/com/zea7ot/lc/lvl1/lc0766/followup/followup2/Solution.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ * References:
+ *  https://leetcode.com/problems/toeplitz-matrix/discuss/179882/Follow-up-questions
+ */
+package com.zea7ot.lc.lvl1.lc0766.followup.followup2;
+
+public class Solution {
+    
+}
