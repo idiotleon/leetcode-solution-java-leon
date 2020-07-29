1d16bf07fa0ea48160d968de92838ea330668c37
Leon <yanglyu.leon.7@gmail.com>
2020-07-29 10:42:53 -0700

    lc0280: Wiggle Sort

---
+++ src/main/java/com/zea7ot/lc/lvl2/lc0280/SolutionApproach0Sorting.java
@@ -0,0 +1,30 @@
+/**
+ * https://leetcode.com/problems/wiggle-sort/
+ * 
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(1)
+ * 
+ * References:
+ *  https://leetcode.com/problems/wiggle-sort/discuss/71692/Java-O(N)-solution/122857
+ *  https://leetcode.com/problems/wiggle-sort/discuss/71693/My-explanations-of-the-best-voted-Algo
+ */
+package com.zea7ot.lc.lvl2.lc0280;
+
+public class SolutionApproach0Sorting {
+    public void wiggleSort(int[] nums) {
+        // sanity check
+        if(nums == null || nums.length == 0) return;
+        
+        final int N = nums.length;
+        int toggle = 1;
+        for(int i = 0; i < N - 1; ++i){
+            if((nums[i + 1] - nums[i]) * toggle < 0){
+                int temp = nums[i];
+                nums[i] = nums[i + 1];
+                nums[i + 1] = temp;
+            }
+            
+            toggle *= -1;
+        }
+    }
+}
