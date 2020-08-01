9b809b586cb14b84fac1e82a5617bbdeca922ee0
Leon <yanglyu.leon.7@gmail.com>
2020-08-01 11:43:23 -0700

    review

--- src/main/java/com/zea7ot/lc/lvl3/lc0086/Solution.java
+++ src/main/java/com/zea7ot/lc/lvl3/lc0086/Solution.java
@@ -1,5 +1,8 @@
 /**
  * https://leetcode.com/problems/partition-list/
+ * 
+ * Time Complexity:     O(L)
+ * Space Complexity:    O(1)
  */
 package com.zea7ot.lc.lvl3.lc0086;
 
@@ -9,22 +12,22 @@ public class Solution {
     public ListNode partition(ListNode head, int x) {
         ListNode dummy1 = new ListNode(-1), dummy2 = new ListNode(-1);
         ListNode cur1 = dummy1, cur2 = dummy2;
-        
-        while(head != null){
-            if(head.val < x){
+
+        while (head != null) {
+            if (head.val < x) {
                 cur1.next = head;
                 cur1 = head;
-            }else{
+            } else {
                 cur2.next = head;
                 cur2 = head;
             }
-            
+
             head = head.next;
         }
-        
+
         cur2.next = null;
         cur1.next = dummy2.next;
-        
+
         return dummy1.next;
     }
 }
