7dacddbc21db2ae13ad704be2574ebfbe9dd61d7
Leon <yanglyu.leon.7@gmail.com>
2020-08-01 00:07:48 -0700

    review

--- src/main/java/com/zea7ot/lc/lvl3/lc0061/Solution.java
+++ src/main/java/com/zea7ot/lc/lvl3/lc0061/Solution.java
@@ -1,34 +1,34 @@
 /**
  * https://leetcode.com/problems/rotate-list/
+ * 
+ * to link the tail of the list with the head, making it a circle
  */
 package com.zea7ot.lc.lvl3.lc0061;
 
 import com.zea7ot.utils.data_structure.linkedlist.ListNode;
 
 public class Solution {
-    /**
-     * to link the tail of the list with the thead, making it a cycle
-     */
     public ListNode rotateRight(ListNode head, int k) {
-        if(head == null) return head;
-        
+        // sanity check
+        if (head == null)
+            return head;
+
         ListNode copyHead = head;
-        
+
         int len = 1;
-        while(copyHead.next != null){
+        while (copyHead.next != null) {
             copyHead = copyHead.next;
-            len++;
+            ++len;
         }
-        
+
         copyHead.next = head;
-        
-        for(int i = len - k % len; i > 1; i--){
+
+        for (int i = len - k % len; i > 1; --i)
             head = head.next;
-        }
-        
+
         copyHead = head.next;
         head.next = null;
-        
+
         return copyHead;
     }
 }
--- src/main/java/com/zea7ot/lc/lvl3/lc0062/SolutionApproach0DP2Dimen.java
+++ src/main/java/com/zea7ot/lc/lvl3/lc0062/SolutionApproach0DP2Dimen.java
@@ -9,16 +9,19 @@ package com.zea7ot.lc.lvl3.lc0062;
 public class SolutionApproach0DP2Dimen {
     public int uniquePaths(int m, int n) {
         // sanity check
-        if(m <= 0 || n <= 0) return 0;
+        if (m <= 0 || n <= 0)
+            return 0;
 
         int[][] dp = new int[m][n];
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                if(i == 0 || j == 0) dp[i][j] = 1;
-                else dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+        for (int row = 0; row < m; ++row) {
+            for (int col = 0; col < n; ++col) {
+                if (row == 0 || col == 0)
+                    dp[row][col] = 1;
+                else
+                    dp[row][col] = dp[row][col - 1] + dp[row - 1][col];
             }
         }
-        
+
         return dp[m - 1][n - 1];
     }
 }
