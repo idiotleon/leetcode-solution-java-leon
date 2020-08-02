b01a2b7ae67221011e4113679aee528bf3ed0277
Leon <yanglyu.leon.7@gmail.com>
2020-08-02 16:58:16 -0700

    lc1379: find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree

---
+++ src/main/java/com/zea7ot/lc/lvl2/lc1379/SolutionApproach1BFS.java
@@ -0,0 +1,40 @@
+/**
+ * https://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/
+ * 
+ * Time Complexity:     O(2 * N) ~ O(N)
+ * Space Complexity:    O(2 * W) ~ O(W)
+ */
+package com.zea7ot.lc.lvl2.lc1379;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import com.zea7ot.utils.data_structure.tree.TreeNode;
+
+public class SolutionApproach1BFS {
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        Deque<TreeNode> originalQueue = new ArrayDeque<TreeNode>();
+        originalQueue.offer(original);
+        Deque<TreeNode> clonedQueue = new ArrayDeque<TreeNode>();
+        clonedQueue.offer(cloned);
+
+        while (!originalQueue.isEmpty()) {
+            TreeNode originalTop = originalQueue.poll();
+            TreeNode clonedTop = clonedQueue.poll();
+            if (originalTop == target)
+                return clonedTop;
+
+            if (originalTop.left != null) {
+                originalQueue.offer(originalTop.left);
+                clonedQueue.offer(clonedTop.left);
+            }
+
+            if (originalTop.right != null) {
+                originalQueue.offer(originalTop.right);
+                clonedQueue.offer(clonedTop.right);
+            }
+        }
+
+        return null;
+    }
+}
