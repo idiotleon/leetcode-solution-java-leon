5bc7aaaa773b25d8ce088bdf7562066135106356
Leon <yanglyu.leon.7@gmail.com>
2020-08-01 21:13:14 -0700

    lc1535: Find the Winner of an Array Game

---
+++ src/main/java/com/zea7ot/lc/lvl3/lc1535/SolutionApproach0Greedy.java
@@ -0,0 +1,28 @@
+/**
+ * https://leetcode.com/problems/find-the-winner-of-an-array-game/
+ * 
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(1)
+ * 
+ * References:
+ *  https://leetcode.com/problems/find-the-winner-of-an-array-game/discuss/768007/JavaC%2B%2BPython-One-Pass-O(1)-Space
+ */
+package com.zea7ot.lc.lvl3.lc1535;
+
+public class SolutionApproach0Greedy {
+    public int getWinner(int[] arr, int k) {
+        final int N = arr.length;
+        int winner = arr[0], win = 0;
+        
+        for(int i = 1; i < N; ++i){
+            if(arr[i] > winner){
+                winner = arr[i];
+                win = 0;
+            }
+            
+            if(++win == k) break;
+        }
+        
+        return winner;
+    }
+}
