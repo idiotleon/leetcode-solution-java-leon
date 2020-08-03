bc080c1c83510e178411e237eb52586a1eec8cb0
Leon <yanglyu.leon.7@gmail.com>
2020-08-03 00:37:08 -0700

    lc0543: Diameter of Binary Tree

--- src/main/java/com/zea7ot/lc/lvl3/lc0543/SolutionApproach0PostorderRecursive1.java
+++ src/main/java/com/zea7ot/lc/lvl3/lc0543/SolutionApproach0PostorderRecursive1.java
@@ -1,30 +1,33 @@
 /**
  * https://leetcode.com/problems/diameter-of-binary-tree/
  * 
- * Time Complexity: O(N)
- * Space Complexity: O(H). Recursion consumes stack space.
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(H)
+ * 
+ * a bottom up approach
  */
 package com.zea7ot.lc.lvl3.lc0543;
 
 import com.zea7ot.utils.data_structure.tree.TreeNode;
 
-public class SolutionApproachDFSBottomUp {
+public class SolutionApproach0PostorderRecursive1 {
     private int max;
-    
+
     public int diameterOfBinaryTree(TreeNode root) {
         this.max = 0;
-        dfs(root);
+        postorder(root);
         return max;
     }
-    
-    private int dfs(TreeNode node){
-        if(node == null) return 0;
-        
-        int left = dfs(node.left);
-        int right = dfs(node.right);
-        
+
+    private int postorder(TreeNode node) {
+        if (node == null)
+            return 0;
+
+        int left = postorder(node.left);
+        int right = postorder(node.right);
+
         max = Math.max(max, left + right);
-        
+
         return Math.max(left, right) + 1;
     }
 }
