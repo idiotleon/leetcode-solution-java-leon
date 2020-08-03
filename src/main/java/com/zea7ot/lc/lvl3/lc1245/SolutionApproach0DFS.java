d36f22554241d2cd55e8be9019e8834196ff0169
Leon <yanglyu.leon.7@gmail.com>
2020-08-03 10:39:50 -0700

    lc1245: Tree Diameter

--- src/main/java/com/zea7ot/lc/lvl3/lc1245/SolutionApproach0DFS.java
+++ src/main/java/com/zea7ot/lc/lvl3/lc1245/SolutionApproach0DFS.java
@@ -1,60 +1,67 @@
 /**
  * https://leetcode.com/problems/tree-diameter/
  * 
- * Time Complexity:     O()
- * Space Complexity:    O()
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(H)
  * 
  * References:
- *  https://leetcode.com/problems/tree-diameter/discuss/418982/Java-Depth-of-the-Tree-solution-Time-O(N)-Easy-to-understand
+ *  https://leetcode.com/problems/tree-diameter/discuss/418820/(Java)-Using-2-BFS
+ *  https://leetcode.com/problems/tree-diameter/discuss/418820/(Java)-Using-2-BFS/402688
+ *  https://stackoverflow.com/a/29219346/6061609
  */
 package com.zea7ot.lc.lvl3.lc1245;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class SolutionApproach0DFS {
     public int treeDiameter(int[][] edges) {
         // sanity check
-        if(edges == null || edges.length < 1) return 0;
-        Map<Integer, List<Integer>> graph = buildGraph(edges);
-        
-        int[] diameter = {0};
-        dfs(graph, 0, -1, diameter);
-        return diameter[0];
+        if (edges == null || edges.length < 1)
+            return 0;
+
+        // best[0]: the length
+        // best[1]: the starting position/node/vetice
+        int[] best = { -1, -1 };
+        List<List<Integer>> graph = buildGraph(edges);
+
+        dfs(0, -1, 0, graph, best);
+        dfs(best[1], -1, 0, graph, best);
+
+        return best[0];
     }
-    
-    private int dfs(Map<Integer, List<Integer>> graph, 
-                      int cur, 
-                      int parent, 
-                      int[] diameter){
-        int deepest = 0, secDeepest = 0;
-        for(int child : graph.get(cur)){
-            if(child == parent) continue;
-            int depth = dfs(graph, child, cur, diameter);
-            if(depth > deepest){
-                secDeepest = deepest;
-                deepest = depth;
-            }else if(depth > secDeepest){
-                secDeepest = depth;
-            }
+
+    private void dfs(int cur, int parent, int len, List<List<Integer>> graph, int[] best) {
+        if (len > best[0]) {
+            best[0] = len;
+            best[1] = cur;
         }
-        
-        diameter[0] = Math.max(diameter[0], deepest + secDeepest);
-        return deepest + 1;
+
+        // not working,
+        // as `best` is not a class member variable
+        // if(len > best[0])
+        // best = new int[]{len, cur};
+
+        for (int next : graph.get(cur)) {
+            if (next == parent)
+                continue;
+            dfs(next, cur, len + 1, graph, best);
+        }
     }
-    
-    private Map<Integer, List<Integer>> buildGraph(int[][] edges){
-        Map<Integer, List<Integer>> graph = new HashMap<Integer, List<Integer>>();
-        for(int[] edge : edges){
+
+    private List<List<Integer>> buildGraph(int[][] edges) {
+        final int N = edges.length;
+        List<List<Integer>> graph = new ArrayList<List<Integer>>();
+        // since it is a "tree", all nodes are connected
+        for (int i = 0; i < N + 1; ++i)
+            graph.add(new ArrayList<Integer>());
+
+        for (int[] edge : edges) {
             int u = edge[0], v = edge[1];
-            graph.putIfAbsent(u, new ArrayList<Integer>());
-            graph.putIfAbsent(v, new ArrayList<Integer>());
             graph.get(u).add(v);
             graph.get(v).add(u);
         }
-        
+
         return graph;
     }
 }
