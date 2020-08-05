5088316e5357b1ecf07cad05a2d75d34f4e239dd
Leon <yanglyu.leon.7@gmail.com>
2020-08-05 10:09:05 -0700

    lc0211: Add and Search Word - Data Structure Design

--- src/main/java/com/zea7ot/lc/lvl4/lc0211/SolutionApproach0TrieWithDFS.java
+++ src/main/java/com/zea7ot/lc/lvl4/lc0211/SolutionApproach0TrieWithDFS.java
@@ -1,61 +1,78 @@
 /**
  * https://leetcode.com/problems/add-and-search-word-data-structure-design/
+ * 
+ * Time Compleixties:
+ *  `addWord`:  O(N * L)
+ *  `search`:   O(N * L)
+ *      N, number of calls of each funtion
+ *  
+ * Space Complexity:    O(N * L)
  */
 package com.zea7ot.lc.lvl4.lc0211;
 
 public class SolutionApproach0TrieWithDFS {
-    private TrieNode root;
+    private static final char PLACE_HOLDER = '.';
+    private final TrieNode ROOT;
 
     /** Initialize your data structure here. */
     public SolutionApproach0TrieWithDFS() {
-        root = new TrieNode();
+        this.ROOT = new TrieNode();
     }
-    
+
     /** Adds a word into the data structure. */
     public void addWord(String word) {
-        TrieNode node = root;
-        for(int i = 0; i < word.length(); i++){
-            char c = word.charAt(i);
-            if(node.children[c - 'a'] == null){
-                node.children[c - 'a'] = new TrieNode();
-            }
-            node = node.children[c - 'a'];
+        TrieNode node = ROOT;
+        for (char ch : word.toCharArray()) {
+            int idx = ch - 'a';
+            if (node.children[idx] == null)
+                node.children[idx] = new TrieNode();
+
+            node = node.children[idx];
         }
-        
-        node.isEndOfWord = true;
+
+        node.word = word;
     }
-    
-    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+
+    /**
+     * Returns if the word is in the data structure. A word could contain the dot
+     * character '.' to represent any one letter.
+     */
     public boolean search(String word) {
-        return dfs(word, 0, root);
+        TrieNode node = ROOT;
+        return dfs(word, 0, node);
     }
-    
-    private boolean dfs(String word, int start, TrieNode node){
-        if(start == word.length()) return node.isEndOfWord;
-        char c = word.charAt(start);
-        if(c != '.'){
-            TrieNode next = node.children[c - 'a'];
-            if(next == null) return false;
-            return dfs(word, start + 1, next);
-        }else{
-            for(int i = 0; i < 26; i++){
-                if(node.children[i] != null){
-                    if(dfs(word, start + 1, node.children[i])){
-                        return true;
-                    };
+
+    private boolean dfs(String word, int idx, TrieNode node) {
+        final int L = word.length();
+        if (idx == L)
+            return node.word != null;
+
+        char ch = word.charAt(idx);
+        if (ch != PLACE_HOLDER) {
+            TrieNode child = node.children[ch - 'a'];
+            if (child == null)
+                return false;
+            return dfs(word, idx + 1, child);
+        } else {
+            for (TrieNode child : node.children) {
+                if (child == null)
+                    continue;
+                if (dfs(word, idx + 1, child)) {
+                    return true;
                 }
             }
         }
-        
+
         return false;
     }
 
-    private class TrieNode{
-        protected boolean isEndOfWord;
-        protected final TrieNode[] children;
-        protected TrieNode(){
-            children = new TrieNode[26];
+    private class TrieNode {
+        protected TrieNode[] children;
+        protected String word;
+
+        protected TrieNode() {
+            this.children = new TrieNode[26];
+            this.word = null;
         }
     }
 }
-
