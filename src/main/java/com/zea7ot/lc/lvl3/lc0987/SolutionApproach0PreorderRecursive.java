f9f7b67507e6b15a8ecf3110351f756ae3d26afc
Leon <yanglyu.leon.7@gmail.com>
2020-08-07 10:05:22 -0700

    lc0987: Vertical Order Traversal of a Binary Tree

---
+++ src/main/java/com/zea7ot/lc/lvl3/lc0987/SolutionApproach0PreorderRecursive.java
@@ -0,0 +1,58 @@
+/**
+ * https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
+ * 
+ * Time Complexity:     O(W * lg(W) * H * lg(H)) + O(W * lg(W)) ~ O(H * W * lg(H * W))
+ *  W, the maximum width the binary tree
+ * 
+ * Space Complexity:    O(N) + O(H) ~ O(N)
+ * 
+ * References:
+ *  https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/discuss/231148/Java-TreeMap-Solution
+ */
+package com.zea7ot.lc.lvl3.lc0987;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
+
+import com.zea7ot.utils.data_structure.tree.TreeNode;
+
+public class SolutionApproach0PreorderRecursive {
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        // sanity check
+        if (root == null)
+            return ans;
+
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        preorder(0, 0, root, map);
+
+        for (Map.Entry<Integer, TreeMap<Integer, PriorityQueue<Integer>>> entryX : map.entrySet()) {
+            List<Integer> level = new ArrayList<Integer>();
+            Map<Integer, PriorityQueue<Integer>> mapY = entryX.getValue();
+            for (Map.Entry<Integer, PriorityQueue<Integer>> entryY : mapY.entrySet()) {
+                PriorityQueue<Integer> minHeap = entryY.getValue();
+                while (!minHeap.isEmpty()) {
+                    level.add(minHeap.poll());
+                }
+            }
+
+            ans.add(level);
+        }
+
+        return ans;
+    }
+
+    private void preorder(int x, int y, TreeNode node, TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map) {
+        if (node == null)
+            return;
+
+        map.putIfAbsent(x, new TreeMap<Integer, PriorityQueue<Integer>>());
+        map.get(x).putIfAbsent(y, new PriorityQueue<Integer>());
+        map.get(x).get(y).add(node.val);
+        preorder(x - 1, y + 1, node.left, map);
+        preorder(x + 1, y + 1, node.right, map);
+    }
+}
---
+++ src/main/java/com/zea7ot/lc/lvl3/lc0987/SolutionApproach0PreorderRecursive1.java
@@ -0,0 +1,57 @@
+/**
+ * https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
+ * 
+ * Time Complexity:     O(W * lg(W) * H * lg(H)) + O(W * lg(W)) ~ O(H * W * lg(H * W))
+ *  W, the maximum width the binary tree
+ * 
+ * Space Complexity:    O(N) + O(H) ~ O(N)
+ * 
+ * References:
+ *  https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/discuss/231148/Java-TreeMap-Solution
+ */
+package com.zea7ot.lc.lvl3.lc0987;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+import com.zea7ot.utils.data_structure.tree.TreeNode;
+
+public class SolutionApproach0PreorderRecursive1 {
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        // sanity check
+        if (root == null)
+            return ans;
+
+        TreeMap<Integer, TreeMap<Integer, List<Integer>>> map = new TreeMap<>();
+        preorder(0, 0, root, map);
+
+        for (Map.Entry<Integer, TreeMap<Integer, List<Integer>>> entryX : map.entrySet()) {
+            List<Integer> level = new ArrayList<Integer>();
+            Map<Integer, List<Integer>> mapY = entryX.getValue();
+            for (Map.Entry<Integer, List<Integer>> entryY : mapY.entrySet()) {
+                List<Integer> list = entryY.getValue();
+                Collections.sort(list);
+                level.addAll(list);
+            }
+
+            ans.add(level);
+        }
+
+        return ans;
+    }
+
+    private void preorder(int x, int y, TreeNode node, TreeMap<Integer, TreeMap<Integer, List<Integer>>> map) {
+        if (node == null)
+            return;
+
+        map.putIfAbsent(x, new TreeMap<Integer, List<Integer>>());
+        map.get(x).putIfAbsent(y, new ArrayList<Integer>());
+        map.get(x).get(y).add(node.val);
+        preorder(x - 1, y + 1, node.left, map);
+        preorder(x + 1, y + 1, node.right, map);
+    }
+}
