561af927125680cd16c83a661ef1e5334faa1154
Leon <yanglyu.leon.7@gmail.com>
2020-08-08 08:57:49 -0700

    lc0270: Closest Binary Search Tree Value

--- src/main/java/com/zea7ot/lc/lvl3/lc0270/SolutionApproach0BinarySearch.java
+++ src/main/java/com/zea7ot/lc/lvl3/lc0270/SolutionApproach0BinarySearch.java
@@ -1,8 +1,8 @@
 /**
  * https://leetcode.com/problems/closest-binary-search-tree-value/
  * 
- * Time Complexity: O(lg(N))
- * Space Complexity: O(1)
+ * Time Complexity:     O(H)
+ * Space Complexity:    O(1)
  */
 package com.zea7ot.lc.lvl3.lc0270;
 
@@ -11,13 +11,13 @@ import com.zea7ot.utils.data_structure.tree.TreeNode;
 public class SolutionApproach0BinarySearch {
     public int closestValue(TreeNode root, double target) {
         int val, closest = root.val;
-        
-        while(root != null){
+
+        while (root != null) {
             val = root.val;
-            closest = Math.abs(val - target) < Math.abs(closest - target) ? val : closest; 
+            closest = Math.abs(val - target) < Math.abs(closest - target) ? val : closest;
             root = target < root.val ? root.left : root.right;
         }
-        
+
         return closest;
     }
 }
