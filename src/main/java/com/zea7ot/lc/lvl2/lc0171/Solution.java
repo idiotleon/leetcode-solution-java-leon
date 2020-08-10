7b27d870a16cd9538b51f9c4e46c69db6c9422a1
Leon <yanglyu.leon.7@gmail.com>
2020-08-10 00:25:35 -0700

    lc0030: Substring with Concatenation of all Words
    review: lc0168&lc0171

--- src/main/java/com/zea7ot/lc/lvl2/lc0171/Solution.java
+++ src/main/java/com/zea7ot/lc/lvl2/lc0171/Solution.java
@@ -1,15 +1,25 @@
 /**
  * https://leetcode.com/problems/excel-sheet-column-number/
+ * 
+ * Time Complexity:     O(L)
+ * Space Complexity:    O(1) / O(L)
  */
 package com.zea7ot.lc.lvl2.lc0171;
 
 public class Solution {
     public int titleToNumber(String s) {
-        int result = 0;
-        for(int i = 0; i < s.length(); i++){
-            result = result * 26 + (s.charAt(i) - 'A' + 1);
+        // sanity check
+        if (s == null || s.isEmpty())
+            return 0;
+
+        final int L = s.length();
+        char[] chs = s.toCharArray();
+
+        int ans = 0;
+        for (int i = 0; i < L; ++i) {
+            ans = ans * 26 + (chs[i] - 'A' + 1);
         }
-        
-        return result;
+
+        return ans;
     }
 }
