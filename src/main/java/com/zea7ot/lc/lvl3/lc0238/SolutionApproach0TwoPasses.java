dedf85232e5fc6866b19970ab9b7bc1c33b6d35d
Leon <yanglyu.leon.7@gmail.com>
2020-08-11 16:36:22 -0700

    lc0238: Product of Array except Self

--- src/main/java/com/zea7ot/lc/lvl3/lc0238/SolutionApproach0TwoPasses.java
+++ src/main/java/com/zea7ot/lc/lvl3/lc0238/SolutionApproach0TwoPasses.java
@@ -9,21 +9,23 @@ package com.zea7ot.lc.lvl3.lc0238;
 public class SolutionApproach0TwoPasses {
     public int[] productExceptSelf(int[] nums) {
         // sanity cehck
-        if(nums == null || nums.length == 0) return new int[0];
-        
+        if (nums == null || nums.length == 0)
+            return new int[0];
+
         final int N = nums.length;
         int[] ans = new int[N];
         ans[0] = 1;
-        for(int i = 1; i < N; i++){
+        // from the left to the right
+        for (int i = 1; i < N; ++i)
             ans[i] = nums[i - 1] * ans[i - 1];
+
+        // from the right to the left
+        int res = 1;
+        for (int i = N - 1; i >= 0; --i) {
+            ans[i] *= res;
+            res *= nums[i];
         }
-        
-        int R = 1;
-        for(int i = N - 1; i >= 0; i--){
-            ans[i] *= R;
-            R *= nums[i];
-        }
-        
+
         return ans;
     }
 }
