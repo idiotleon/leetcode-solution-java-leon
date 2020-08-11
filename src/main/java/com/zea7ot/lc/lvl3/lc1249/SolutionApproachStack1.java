7f3dd9e792e40db62853d0d71121789854424588
Leon <yanglyu.leon.7@gmail.com>
2020-08-11 10:20:48 -0700

    1249: Minimum Remove to Make Valid Parentheses

--- src/main/java/com/zea7ot/lc/lvl3/lc1249/SolutionApproachStack1.java
+++ src/main/java/com/zea7ot/lc/lvl3/lc1249/SolutionApproachStack1.java
@@ -12,31 +12,30 @@ package com.zea7ot.lc.lvl3.lc1249;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-public class SolutionApproachStack {
+public class SolutionApproachStack1 {
     private static final char PLACE_HOLDER = '*';
 
     public String minRemoveToMakeValid(String s) {
         final int L = s.length();
-        char[] chs = s.toCharArray();
         StringBuilder builder = new StringBuilder(s);
-        Deque<Integer> idxStack = new ArrayDeque<Integer>();
-
-        for (int i = 0; i < L; ++i) {
-            char ch = chs[i];
-            if (ch == '(')
-                idxStack.offer(i);
+        Deque<Integer> stack = new ArrayDeque<Integer>();
+        for (int i = 0; i < L; i++) {
+            char ch = builder.charAt(i);
+            if (ch == '(') {
+                stack.add(i);
+            }
 
             if (ch == ')') {
-                if (idxStack.isEmpty()) {
-                    builder.setCharAt(i, PLACE_HOLDER);
+                if (!stack.isEmpty()) {
+                    stack.pop();
                 } else {
-                    idxStack.pop();
+                    builder.setCharAt(i, PLACE_HOLDER);
                 }
             }
         }
 
-        while (!idxStack.isEmpty()) {
-            builder.setCharAt(idxStack.pop(), PLACE_HOLDER);
+        while (!stack.isEmpty()) {
+            builder.setCharAt(stack.pop(), PLACE_HOLDER);
         }
 
         return builder.toString().replaceAll("\\*", "");
