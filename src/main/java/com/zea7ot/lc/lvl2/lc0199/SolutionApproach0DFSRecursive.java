455fd47dcc12b1f19a24d70d9558033894a44919
Leon <yanglyu.leon.7@gmail.com>
2020-08-14 11:23:13 -0700

    reviews

---
+++ src/main/java/com/zea7ot/lc/lvl2/lc0199/SolutionApproach0DFSRecursive.java
@@ -0,0 +1,39 @@
+/**
+ * https://leetcode.com/problems/binary-tree-right-side-view/
+ * 
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(H)
+ * 
+ * this is not a preorder traversal
+ */
+package com.zea7ot.lc.lvl2.lc0199;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.zea7ot.utils.data_structure.tree.TreeNode;
+
+public class SolutionApproach0DFSRecursive {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<Integer>();
+        // sanity check
+        if (root == null)
+            return ans;
+
+        dfs(root, 0, ans);
+
+        return ans;
+    }
+
+    private void dfs(TreeNode node, int curDepth, List<Integer> res) {
+        if (node == null)
+            return;
+
+        if (curDepth == res.size()) {
+            res.add(node.val);
+        }
+
+        dfs(node.right, curDepth + 1, res);
+        dfs(node.left, curDepth + 1, res);
+    }
+}
---
+++ src/main/java/com/zea7ot/lc/lvl3/lc0523/SolutionApproach1PrefixSums1.java
@@ -0,0 +1,36 @@
+/**
+ * https://leetcode.com/problems/continuous-subarray-sum/
+ * 
+ * Time Complexity:     O(N ^ 2)
+ * Space Complexity:    O(1)
+ * 
+ * References:
+ *  https://leetcode.com/problems/continuous-subarray-sum/discuss/99518/Not-smart-solution-but-easy-to-understand
+ */
+package com.zea7ot.lc.lvl3.lc0523;
+
+public class SolutionApproach1PrefixSums1 {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        // sanity check
+        if (nums == null || nums.length < 2)
+            return false;
+
+        final int N = nums.length;
+        int[] prefixSums = new int[N + 1];
+
+        for (int i = 1; i <= N; ++i)
+            prefixSums[i] = prefixSums[i - 1] + nums[i - 1];
+
+        for (int i = 0; i < N; ++i) {
+            for (int j = i + 2; j <= N; ++j) {
+                if (k == 0) {
+                    if (prefixSums[j] - prefixSums[i] == 0)
+                        return true;
+                } else if ((prefixSums[j] - prefixSums[i]) % k == 0)
+                    return true;
+            }
+        }
+
+        return false;
+    }
+}
