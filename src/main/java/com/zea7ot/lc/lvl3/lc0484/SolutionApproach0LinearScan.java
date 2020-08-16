a99bccc3bdc94647ed06c21ea3b609a7a0b7750d
Leon <yanglyu.leon.7@gmail.com>
2020-08-15 21:11:20 -0700

    lc0484: Find Permutation

---
+++ src/main/java/com/zea7ot/lc/lvl3/lc0484/SolutionApproach0LinearScan.java
@@ -0,0 +1,48 @@
+/**
+ * https://leetcode.com/problems/find-permutation/
+ * 
+ * Time Complexity:     O(L)
+ * Space Complexity:    O(1) / O(L)
+ *  O(L), consumed by the answer array
+ * 
+ * References:
+ *  https://leetcode.com/problems/find-permutation/discuss/96613/Java-O(n)-clean-solution-easy-to-understand
+ */
+package com.zea7ot.lc.lvl3.lc0484;
+
+public class SolutionApproach0LinearScan {
+    private static final char DECREASING = 'D';
+
+    public int[] findPermutation(String s) {
+        // sanity check
+        if (s == null || s.isEmpty())
+            return new int[0];
+
+        final int L = s.length();
+        int[] ans = new int[L + 1];
+        for (int i = 0; i <= L; ++i)
+            ans[i] = i + 1;
+
+        char[] chs = s.toCharArray();
+        for (int hi = 0; hi < L; ++hi) {
+            if (chs[hi] == DECREASING) {
+                int lo = hi;
+                while (hi < L && chs[hi] == DECREASING)
+                    ++hi;
+                reverse(lo, hi, ans);
+            }
+        }
+
+        return ans;
+    }
+
+    private void reverse(int lo, int hi, int[] nums) {
+        while (lo < hi) {
+            int temp = nums[lo];
+            nums[lo] = nums[hi];
+            nums[hi] = temp;
+            ++lo;
+            --hi;
+        }
+    }
+}
