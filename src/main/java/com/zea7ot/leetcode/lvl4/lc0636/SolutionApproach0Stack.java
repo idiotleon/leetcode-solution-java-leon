8561a1cded7dc805d999e2aa218a70e03f2e6bd7
Leon <yanglyu.leon.7@gmail.com>
2020-08-19 18:23:10 -0700

    review

--- src/main/java/com/zea7ot/leetcode/lvl4/lc0636/SolutionApproach0Stack.java
+++ src/main/java/com/zea7ot/leetcode/lvl4/lc0636/SolutionApproach0Stack.java
@@ -1,8 +1,8 @@
 /**
  * https://leetcode.com/problems/exclusive-time-of-functions/
  * 
- * Time Complexity:     O(N)
- * Space Complexity:    O(N)
+ * Time Complexity:     O(`n`)
+ * Space Complexity:    O(`n`)
  * 
  * `stack` is used to save the IDs, which should be pused in if it is a start, of functions
  * 
@@ -17,19 +17,19 @@ import java.util.Deque;
 import java.util.List;
 
 public class SolutionApproach0Stack {
+    private static final String START = "start";
+
     public int[] exclusiveTime(int n, List<String> logs) {
         int[] ans = new int[n];
-
-        final String START = "start";
 
         Deque<Integer> stack = new ArrayDeque<Integer>();
         int prevTimestamp = 0;
 
-        for (String cur : logs) {
-            String[] log = cur.split(":");
-            int id = Integer.parseInt(log[0]);
-            String state = log[1];
-            int timestamp = Integer.parseInt(log[2]);
+        for (String log : logs) {
+            String[] res = log.split(":");
+            int id = Integer.parseInt(res[0]);
+            String state = res[1];
+            int timestamp = Integer.parseInt(res[2]);
 
             if (!stack.isEmpty())
                 ans[stack.peek()] += timestamp - prevTimestamp;
