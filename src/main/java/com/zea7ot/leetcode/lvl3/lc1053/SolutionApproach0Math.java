484832c8c553732ae0800931169dade562974382
Leon <yanglyu.leon.7@gmail.com>
2020-08-20 19:50:25 -0700

    review

--- src/main/java/com/zea7ot/leetcode/lvl3/lc1053/SolutionApproach0Math.java
+++ src/main/java/com/zea7ot/leetcode/lvl3/lc1053/SolutionApproach0Math.java
@@ -3,38 +3,36 @@
  * 
  * Time Complexity:     O(N)
  * Space Complexity:    O(1)
- * 
- * References: 
- *  https://leetcode.com/problems/previous-permutation-with-one-swap/discuss/299244/Similar-to-find-previous-permutation-written-in-Java
  */
 package com.zea7ot.leetcode.lvl3.lc1053;
 
 public class SolutionApproach0Math {
     public int[] prevPermOpt1(int[] nums) {
         // sanity check
-        if (nums == null || nums.length < 2)
-            return nums;
+        if (nums == null || nums.length == 0)
+            return new int[0];
 
         final int N = nums.length;
-        int idx = -1;
-        for (int i = N - 1; i >= 1; --i)
-            if (nums[i] < nums[i - 1]) {
-                idx = i - 1;
-                break;
-            }
 
-        if (idx == -1)
+        int i = N - 2;
+        while (i >= 0 && nums[i] <= nums[i + 1])
+            --i;
+
+        if (i < 0)
             return nums;
 
-        for (int i = N - 1; i > idx; --i) {
-            if (nums[i] < nums[idx] && nums[i] != nums[i - 1]) {
-                int temp = nums[i];
-                nums[i] = nums[idx];
-                nums[idx] = temp;
-                break;
-            }
-        }
+        int j = N - 1;
+        while (j >= 0 && (nums[j] >= nums[i] || nums[j] == nums[j - 1]))
+            --j;
+
+        swap(i, j, nums);
 
         return nums;
+    }
+
+    private void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
