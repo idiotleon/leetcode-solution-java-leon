5abafa0c313e48ef3de221fb34f14d3273e0ed22
Leon <yanglyu.leon.7@gmail.com>
2020-08-22 23:50:25 -0700

    reviews

--- src/main/java/com/zea7ot/leetcode/lvl4/lc0505/SolutionApproach0DijkstrasAlgorithm1.java
+++ src/main/java/com/zea7ot/leetcode/lvl4/lc0505/SolutionApproach0DijkstrasAlgorithm1.java
@@ -12,68 +12,56 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 
 public class SolutionApproach0DijkstrasAlgorithm1 {
-    private static final int[] DIRS = {0, -1, 0, 1, 0};
-    private int NR, NC;
-    
+    private static final int[] DIRS = { 0, -1, 0, 1, 0 };
+
     public int shortestDistance(int[][] maze, int[] start, int[] destination) {
         // sanity check
-        if(maze == null || maze.length == 0 || maze[0].length == 0) return -1;
-        
+        if (maze == null || maze.length == 0 || maze[0].length == 0)
+            return -1;
+
         // boundaries/sizes of the maze
-        this.NR = maze.length;
-        this.NC = maze[0].length;
-        
+        final int NR = maze.length, NC = maze[0].length;
+
         int[][] distances = new int[NR][NC];
-        for(int[] row : distances){
+        for (int[] row : distances)
             Arrays.fill(row, Integer.MAX_VALUE);
-        }
         distances[start[0]][start[1]] = 0;
-        
+
         // min heap, by distance, the 1st element
-        PriorityQueue<int[]> minHeap = new PriorityQueue<int[]>((a, b) -> a[0] - b[0]);
-        minHeap.add(new int[]{0, start[0], start[1]});
-        
-        while(!minHeap.isEmpty()){
-            int[] top = minHeap.poll();
-            start[0] = top[1];
-            start[1] = top[2];
+        PriorityQueue<int[]> minHeap = new PriorityQueue<int[]>((a, b) -> Integer.compare(a[0], b[0]));
+        minHeap.add(new int[] { 0, start[0], start[1] });
+
+        while (!minHeap.isEmpty()) {
+            int[] cur = minHeap.poll();
+            int row = cur[1], col = cur[2];
             // to compare by value
-            if(Arrays.equals(start, destination)) break;
-            shortestDistance(maze, distances, start, minHeap);
+            if (destination[0] == row && destination[1] == col)
+                break;
+
+            int distance = distances[row][col];
+            for (int d = 0; d < 4; d++) {
+                int steps = 0;
+                int r = row, c = col;
+                while (isValid(r + DIRS[d], c + DIRS[d + 1], maze)) {
+                    ++steps;
+                    r += DIRS[d];
+                    c += DIRS[d + 1];
+                }
+
+                // to update with the shortest distance
+                if (distances[r][c] > distance + steps) {
+                    distances[r][c] = distance + steps;
+                    minHeap.add(new int[] { distances[r][c], r, c });
+                }
+            }
         }
-        
+
         int shortestDistance = distances[destination[0]][destination[1]];
         return shortestDistance == Integer.MAX_VALUE ? -1 : shortestDistance;
     }
-    
-    private void shortestDistance(int[][] maze, 
-                                  int[][] distances, 
-                                  int[] start, 
-                                  PriorityQueue<int[]> minHeap){
-        int distance = distances[start[0]][start[1]];
-        for(int d = 0; d < 4; d++){
-            int steps = 0;
-            int row = start[0], col = start[1];
-            while(true){
-                row += DIRS[d];
-                col += DIRS[d + 1];
-                if(row >= 0 && row < NR && col >= 0 && col < NC && maze[row][col] == 0){
-                    steps++;
-                }else{
-                    row -= DIRS[d];
-                    col -= DIRS[d + 1];
-                    break;
-                }
-            }
-            // to skip if it remains at the same/start position
-            // to compare by value
-            if(row == start[0] && col == start[1]) continue;
-            
-            // to update with the shortest distance
-            if(distances[row][col] > distance + steps){
-                distances[row][col] = distance + steps;
-                minHeap.add(new int[]{distances[row][col], row, col});
-            }
-        }
+
+    private boolean isValid(int row, int col, int[][] maze) {
+        final int NR = maze.length, NC = maze[0].length;
+        return row >= 0 && row < NR && col >= 0 && col < NC && maze[row][col] == 0;
     }
 }
--- src/main/java/com/zea7ot/leetcode/lvl5/lc0499/SolutionApproach0DFS.java
+++ src/main/java/com/zea7ot/leetcode/lvl5/lc0499/SolutionApproach0DFS.java
@@ -10,12 +10,12 @@
 package com.zea7ot.leetcode.lvl5.lc0499;
 
 public class SolutionApproach0DFS {
-    private static int[][] DIRS = {{1, 0}, {0, 1}, {0, -1}, {-1, 0}};
+    private static int[][] DIRS = { { 1, 0 }, { 0, 1 }, { 0, -1 }, { -1, 0 } };
     // to strictly align with DIRS(int[][])
-    private static char[] MOVES = {'d', 'r', 'l', 'u'};
+    private static char[] MOVES = { 'd', 'r', 'l', 'u' };
     private int minSteps, M, N;
     private String ans;
-    
+
     public String findShortestWay(int[][] maze, int[] ball, int[] hole) {
         this.M = maze.length;
         this.N = maze[0].length;
@@ -23,43 +23,39 @@ public class SolutionApproach0DFS {
         this.ans = null;
         boolean[][] visited = new boolean[M][N];
         visited[ball[0]][ball[1]] = true;
-        
+
         backtrack(maze, hole, visited, ball[0], ball[1], "", 0);
 
         return ans == null ? "impossible" : ans;
     }
-    
-    private void backtrack(int[][] maze, 
-                           int[] hole, 
-                           boolean[][] visited, 
-                           int row, 
-                           int col, 
-                           String route, 
-                           int steps){
-        if(steps > minSteps) return;
-        if(row == hole[0] && col == hole[1]){
-            if(steps == minSteps && route.compareTo(ans) < 0){
+
+    private void backtrack(int[][] maze, int[] hole, boolean[][] visited, int row, int col, String route, int steps) {
+        if (steps > minSteps)
+            return;
+        if (row == hole[0] && col == hole[1]) {
+            if (steps == minSteps && route.compareTo(ans) < 0) {
                 ans = route;
-            }else if(steps < minSteps){
+            } else if (steps < minSteps) {
                 minSteps = steps;
                 ans = route;
             }
-            
+
             visited[row][col] = false;
             return;
         }
-        
-        for(int d = 0; d < 4; d++){
-            int r = row , c = col;
-            while(r + DIRS[d][0] >= 0 && r + DIRS[d][0] < M 
-                  && c + DIRS[d][1] >= 0 && c + DIRS[d][1] < N 
-                  && maze[r + DIRS[d][0]][c + DIRS[d][1]] != 1){
+
+        for (int d = 0; d < 4; d++) {
+            int r = row, c = col;
+            while (r + DIRS[d][0] >= 0 && r + DIRS[d][0] < M && c + DIRS[d][1] >= 0 && c + DIRS[d][1] < N
+                    && maze[r + DIRS[d][0]][c + DIRS[d][1]] != 1) {
                 r += DIRS[d][0];
                 c += DIRS[d][1];
-                if(r == hole[0] && c == hole[1] || visited[r][c]) break;
+                if (r == hole[0] && c == hole[1] || visited[r][c])
+                    break;
             }
-            
-            if(visited[r][c] || maze[r][c] == 1) continue;
+
+            if (visited[r][c] || maze[r][c] == 1)
+                continue;
             visited[r][c] = true;
             backtrack(maze, hole, visited, r, c, route + MOVES[d], steps + Math.abs(r - row) + Math.abs(c - col));
             visited[r][c] = false;
