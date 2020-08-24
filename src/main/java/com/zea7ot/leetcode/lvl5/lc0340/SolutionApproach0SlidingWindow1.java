56fbf3368f3aaa7999e5aa79c67d2deca95c86b2
Leon <yanglyu.leon.7@gmail.com>
2020-08-24 10:31:07 -0700

    review

--- src/main/java/com/zea7ot/leetcode/lvl5/lc0340/SolutionApproach0SlidingWindow1.java
+++ src/main/java/com/zea7ot/leetcode/lvl5/lc0340/SolutionApproach0SlidingWindow1.java
@@ -6,30 +6,21 @@
  */
 package com.zea7ot.leetcode.lvl5.lc0340;
 
-public class SolutionApproach0SlidingWindow {
+public class SolutionApproach0SlidingWindow1 {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
         final int L = s.length();
         char[] chs = s.toCharArray();
         int[] freq = new int[256];
-
-        int distinct = 0;
-        int longest = 0;
+        int distinct = 0, longest = 0;
 
         int lo = 0, hi = 0;
         while (hi < L) {
-            if (freq[chs[hi]]++ == 0)
+            if (freq[chs[hi++]]++ == 0)
                 ++distinct;
-
-            while (distinct > k) {
-                if (--freq[chs[lo]] == 0)
+            while (distinct > k)
+                if (--freq[chs[lo++]] == 0)
                     --distinct;
-                ++lo;
-            }
-
-            // please pay attention to the relative sequence of the below two lines,
-            // which defines the running `length`.
-            longest = Math.max(longest, hi - lo + 1);
-            ++hi;
+            longest = Math.max(longest, hi - lo);
         }
 
         return longest;
