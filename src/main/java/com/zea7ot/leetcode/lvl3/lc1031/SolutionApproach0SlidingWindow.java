4b8a270bcbe10e6c0672e0f78c7e6a4df7f84480
Leon <yanglyu.leon.7@gmail.com>
2020-08-24 13:23:37 -0700

    lc1234: Replace the Substring for Balanced String

--- src/main/java/com/zea7ot/leetcode/lvl3/lc1031/SolutionApproach0SlidingWindow.java
+++ src/main/java/com/zea7ot/leetcode/lvl3/lc1031/SolutionApproach0SlidingWindow.java
@@ -14,23 +14,27 @@ public class SolutionApproach0SlidingWindow {
     public int maxSumTwoNoOverlap(int[] nums, int L, int M) {
         return Math.max(maxSum(nums, L, M), maxSum(nums, M, L));
     }
-    
-    private int maxSum(int[] nums, int L, int M){
+
+    private int maxSum(int[] nums, int L, int M) {
+        final int N = nums.length;
+
         int sumL = 0, sumM = 0;
-        // to compute the initial values of subarrays of length L & M 
-        for(int i = 0; i < L + M; i++){
-            if(i < L) sumL += nums[i];
-            else sumM += nums[i];
+        // to compute the initial values of subarrays of length L & M
+        for (int i = 0; i < L + M; ++i) {
+            if (i < L)
+                sumL += nums[i];
+            else
+                sumM += nums[i];
         }
-        
+
         int ans = sumM + sumL; // sum of `sumL` and `sumM`
-        for(int i = L + M, maxL = sumL; i < nums.length; i++){
+        for (int i = L + M, maxL = sumL; i < N; ++i) {
             sumM += nums[i] - nums[i - M]; // to update `sumM`
             sumL += nums[i - M] - nums[i - L - M]; // to update `sumL`
             maxL = Math.max(maxL, sumL); // to update the max value of subarray of length L
             ans = Math.max(ans, maxL + sumM); // to update the max value of the sum of subarrays of length L & M
         }
-        
+
         return ans;
     }
 }
---
+++ src/main/java/com/zea7ot/leetcode/lvl4/lc1234/SolutionApproach0SlidingWindow.java
@@ -0,0 +1,72 @@
+/**
+ * https://leetcode.com/problems/replace-the-substring-for-balanced-string/
+ * 
+ * Time Complexity:     O(L)
+ * Space Complexity:    O(4) / O(L) ~ O(1) / O(L)
+ * 
+ * `freq[]` keeps acount of the frequencies OUTSIDE the sliding window
+ * 
+ * References:
+ *  https://leetcode.com/problems/replace-the-substring-for-balanced-string/discuss/408978/JavaC%2B%2BPython-Sliding-Window
+ */
+package com.zea7ot.leetcode.lvl4.lc1234;
+
+public class SolutionApproach0SlidingWindow {
+    private static final char[] LETTERS = new char[] { 'Q', 'W', 'E', 'R' };
+
+    public int balancedString(String s) {
+        // sanity check
+        if (s == null || s.isEmpty())
+            return 0;
+        final int L = s.length();
+        char[] chs = s.toCharArray();
+        final int K = L / 4;
+
+        int[] freq = new int[4];
+        for (char ch : chs)
+            ++freq[hash(ch)];
+
+        if (isValid(freq, K))
+            return 0;
+
+        int lo = 0, hi = 0;
+        int shortest = L;
+
+        while (hi < L) {
+            --freq[hash(chs[hi])];
+
+            while (isValid(freq, K)) {
+                shortest = Math.min(shortest, hi - lo + 1);
+                ++freq[hash(chs[lo])];
+                ++lo;
+            }
+
+            ++hi;
+        }
+
+        return shortest;
+    }
+
+    private boolean isValid(int[] freq, final int K) {
+        int max = 0;
+        for (char ch : LETTERS)
+            max = Math.max(max, freq[hash(ch)]);
+
+        return max <= K;
+    }
+
+    private int hash(char ch) {
+        switch (ch) {
+            case 'Q':
+                return 0;
+            case 'W':
+                return 1;
+            case 'E':
+                return 2;
+            case 'R':
+                return 3;
+        }
+
+        return -1;
+    }
+}
