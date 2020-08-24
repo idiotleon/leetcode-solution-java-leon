cf692ef5f77d70adb476d1f1ea413af87589131b
Leon <yanglyu.leon.7@gmail.com>
2020-08-23 19:08:31 -0700

    reviews

---
+++ src/main/java/com/zea7ot/leetcode/lvl2/lc0209/followups/SolutionApproach0BinarySearchWithPrefixSums.java
@@ -0,0 +1,19 @@
+/**
+ * https://leetcode.com/problems/minimum-size-subarray-sum/
+ * 
+ * If you have figured out the O(n) solution, 
+ *  how about another solution of which the time complexity is O(n log n). 
+ * 
+ * Time Complexity:     O(N * lg(N)) + O(N) ~ O(N * lg(N))
+ * Space Complexity:    O(N)
+ * 
+ * prefix sums + binary search
+ * 
+ * References:
+ *  
+ */
+package com.zea7ot.leetcode.lvl2.lc0209.followups;
+
+public class SolutionApproach0BinarySearchWithPrefixSums {
+
+}
