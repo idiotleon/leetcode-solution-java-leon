e51439c2dd728fb28535dd8f2edf9f52262a800a
Leon <yanglyu.leon.7@gmail.com>
2020-08-26 11:41:32 -0700

    reviews

---
+++ src/main/java/com/zea7ot/leetcode/lvl4/lc0158/FakeReader4.java
@@ -0,0 +1,7 @@
+package com.zea7ot.leetcode.lvl4.lc0158;
+
+class FakeReader4 {
+    protected int read4(char[] buf4) {
+        return 4;
+    }
+}
--- src/main/java/com/zea7ot/utils/Reader4.java
+++
@@ -1,7 +0,0 @@
-package com.zea7ot.utils;
-
-public class Reader4 {
-    public int read4(char[] buf){
-        return 4;
-    }
-}
