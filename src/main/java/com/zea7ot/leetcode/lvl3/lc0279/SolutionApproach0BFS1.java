dd6cb23acaad10e8a6e8bdc66466b60bd9cbfde8
Leon <yanglyu.leon.7@gmail.com>
2020-08-26 23:19:51 -0700

    reviews

--- src/main/java/com/zea7ot/leetcode/lvl3/lc0279/SolutionApproach0BFS1.java
+++ src/main/java/com/zea7ot/leetcode/lvl3/lc0279/SolutionApproach0BFS1.java
@@ -11,31 +11,37 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Set;
 
-public class SolutionApproach1BFS1 {
+public class SolutionApproach0BFS1 {
     public int numSquares(int n) {
         // sanity check
-        if(n == 0 || n == 1) return n;
-        
+        if (n == 0 || n == 1)
+            return n;
+
         Queue<Integer> queue = new LinkedList<Integer>();
         queue.offer(0);
-        int level = 0;
+
         Set<Integer> visited = new HashSet<Integer>();
         visited.add(0);
-        while(!queue.isEmpty()){
+
+        int steps = 1;
+
+        while (!queue.isEmpty()) {
             final int SIZE = queue.size();
-            for(int i = 0; i < SIZE; i++){
+
+            for (int i = 0; i < SIZE; ++i) {
                 int cur = queue.poll();
-                for(int j = 1; cur + j * j <= n; j++){
+                for (int j = 1; cur + j * j <= n; j++) {
                     int temp = cur + j * j;
-                    if(temp == n) return level + 1;
-                    if(!visited.contains(temp)){
-                        queue.offer(temp);
-                        visited.add(temp);
-                    }
+                    if (temp == n)
+                        return steps;
+                    if (!visited.add(temp))
+                        continue;
+                    queue.offer(temp);
                 }
             }
-            level++;
+            ++steps;
         }
-        return 0;
+
+        return -1;
     }
 }
--- src/main/java/com/zea7ot/leetcode/lvl3/lc0279/SolutionApproach0DP1.java
+++ src/main/java/com/zea7ot/leetcode/lvl3/lc0279/SolutionApproach0DP1.java
@@ -14,19 +14,20 @@ import java.util.List;
 
 public class SolutionApproach0DP1 {
     private static List<Integer> ans = new ArrayList<Integer>();
-    
+
     public int numSquares(int n) {
-        if(ans.isEmpty()) ans.add(0);
-        
-        while(ans.size() <= n){
+        if (ans.isEmpty())
+            ans.add(0);
+
+        while (ans.size() <= n) {
             final int SIZE = ans.size();
             int tempMin = Integer.MAX_VALUE;
-            for(int j = 1; j * j <= SIZE; j++){
-                tempMin = Math.min(tempMin, ans.get(SIZE - j * j)  + 1);
+            for (int j = 1; j * j <= SIZE; j++) {
+                tempMin = Math.min(tempMin, ans.get(SIZE - j * j) + 1);
             }
             ans.add(tempMin);
         }
-        
+
         return ans.get(n);
     }
 }
