e9c04095e87b69a1f3bd2785d2572c18800c2e6e
Leon <yanglyu.leon.7@gmail.com>
2020-08-29 14:52:43 -0700

    reviews

--- src/main/java/com/zea7ot/leetcode/lvl3/lc0969/SolutionApproach0Sorting.java
+++ src/main/java/com/zea7ot/leetcode/lvl3/lc0969/SolutionApproach0Sorting.java
@@ -12,44 +12,46 @@ package com.zea7ot.leetcode.lvl3.lc0969;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SolutionApproach0BrutalForce {
+public class SolutionApproach0Sorting {
     public List<Integer> pancakeSort(int[] nums) {
         List<Integer> ans = new ArrayList<Integer>();
         // sanity check
-        if(nums == null || nums.length == 0) return ans;
-        
+        if (nums == null || nums.length == 0)
+            return ans;
+
         final int N = nums.length;
         int len = N;
-        for(int i = 0; i < N; i++){
-            int index = find(nums, len);
-            pancakeFlip(nums, index);
-            pancakeFlip(nums, len - 1);
+        for (int i = 0; i < N; ++i) {
+            int index = find(len, nums);
+            pancakeFlip(index, nums);
+            pancakeFlip(len - 1, nums);
             ans.add(index + 1);
             ans.add(len);
-            len--;
+            --len;
         }
-        
+
         return ans;
     }
-    
-    private int find(int[] nums, int target){
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == target) {
+
+    private int find(int target, int[] nums) {
+        final int N = nums.length;
+        for (int i = 0; i < N; ++i) {
+            if (nums[i] == target)
                 return i;
-            }
         }
-        
+
         return -1;
     }
-    
-    private void pancakeFlip(int[] nums, int index){
+
+    private void pancakeFlip(int index, int[] nums) {
         int lo = 0, hi = index;
-        while(lo < hi){
-            int temp = nums[lo];
-            nums[lo] = nums[hi];
-            nums[hi] = temp;
-            lo++;
-            hi--;
-        }
+        while (lo < hi)
+            swap(lo++, hi--, nums);
+    }
+
+    private void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
