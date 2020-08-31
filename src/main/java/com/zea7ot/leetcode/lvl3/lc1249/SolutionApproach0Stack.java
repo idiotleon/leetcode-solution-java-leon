d284a10da6a31bb4a36314942b761ad2c804a074
Leon <yanglyu.leon.7@gmail.com>
2020-08-30 21:57:24 -0700

    reviews

--- src/main/java/com/zea7ot/leetcode/lvl3/lc1249/SolutionApproach0Stack.java
+++ src/main/java/com/zea7ot/leetcode/lvl3/lc1249/SolutionApproach0Stack.java
@@ -12,32 +12,35 @@ package com.zea7ot.leetcode.lvl3.lc1249;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-public class SolutionApproachStack1 {
-    private static final char PLACE_HOLDER = '*';
+public class SolutionApproach0Stack {
+    private static final char PLACE_HOLDER = '.';
 
     public String minRemoveToMakeValid(String s) {
+        // santiy check
+        if (s == null || s.isEmpty())
+            return "";
+
         final int L = s.length();
-        StringBuilder builder = new StringBuilder(s);
+        final char[] CHS = s.toCharArray();
         Deque<Integer> stack = new ArrayDeque<Integer>();
-        for (int i = 0; i < L; i++) {
-            char ch = builder.charAt(i);
-            if (ch == '(') {
-                stack.add(i);
-            }
+
+        for (int i = 0; i < L; ++i) {
+            char ch = CHS[i];
+            if (ch == '(')
+                stack.offer(i);
 
             if (ch == ')') {
-                if (!stack.isEmpty()) {
-                    stack.pop();
+                if (stack.isEmpty()) {
+                    CHS[i] = PLACE_HOLDER;
                 } else {
-                    builder.setCharAt(i, PLACE_HOLDER);
+                    stack.pop();
                 }
             }
         }
 
-        while (!stack.isEmpty()) {
-            builder.setCharAt(stack.pop(), PLACE_HOLDER);
-        }
+        while (!stack.isEmpty())
+            CHS[stack.pop()] = PLACE_HOLDER;
 
-        return builder.toString().replaceAll("\\*", "");
+        return new String(CHS).replaceAll(String.valueOf(PLACE_HOLDER), "");
     }
 }
