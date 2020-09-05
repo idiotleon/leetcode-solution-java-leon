80b5993e610e9d7fbdff826bb77931ccc07ca607
Leon <yanglyu.leon.7@gmail.com>
2020-09-05 11:59:14 -0700

    lc0872: Leaf-Similar Trees

--- src/main/java/com/zea7ot/leetcode/lvl1/lc0872/SolutionApproach0PostorderIterative.java
+++ src/main/java/com/zea7ot/leetcode/lvl1/lc0872/SolutionApproach0PostorderIterative.java
@@ -14,26 +14,31 @@ import java.util.Deque;
 
 import com.zea7ot.utils.data_structure.tree.TreeNode;
 
-public class SolutionApproach0DFS {
+public class SolutionApproach0PostorderIterative {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
         Deque<TreeNode> stack1 = new ArrayDeque<TreeNode>();
-        Deque<TreeNode> stack2 = new ArrayDeque<TreeNode>();
         stack1.push(root1);
+        Deque<TreeNode> stack2 = new ArrayDeque<TreeNode>();
         stack2.push(root2);
-        while(!stack1.isEmpty() && !stack2.isEmpty()){
-            if(dfs(stack1) != dfs(stack2))
+        while (!stack1.isEmpty() && !stack2.isEmpty()) {
+            if (dfs(stack1) != dfs(stack2))
                 return false;
         }
-        
+
         return stack1.isEmpty() && stack2.isEmpty();
     }
-    
-    private int dfs(Deque<TreeNode> stack){
-        while(true){
+
+    private int dfs(Deque<TreeNode> stack) {
+        while (true) {
             TreeNode node = stack.pop();
-            if(node.right != null) stack.push(node.right);
-            if(node.left != null) stack.push(node.left);
-            if(node.left == null && node.right == null)
+
+            if (node.left != null)
+                stack.push(node.left);
+
+            if (node.right != null)
+                stack.push(node.right);
+
+            if (node.left == null && node.right == null)
                 return node.val;
         }
     }
