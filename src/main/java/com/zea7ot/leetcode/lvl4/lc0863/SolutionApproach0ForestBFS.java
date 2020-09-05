178eff897b8a3be14ed9605c06c6e38593c2c42c
Leon <yanglyu.leon.7@gmail.com>
2020-09-05 16:37:06 -0700

    reviews

--- src/main/java/com/zea7ot/leetcode/lvl4/lc0863/SolutionApproach0ForestBFS.java
+++ src/main/java/com/zea7ot/leetcode/lvl4/lc0863/SolutionApproach0ForestBFS.java
@@ -8,13 +8,13 @@
  */
 package com.zea7ot.leetcode.lvl4.lc0863;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 import java.util.Set;
 
 import com.zea7ot.utils.data_structure.tree.TreeNode;
@@ -22,55 +22,61 @@ import com.zea7ot.utils.data_structure.tree.TreeNode;
 public class SolutionApproach0ForestBFS {
     public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
         List<Integer> ans = new ArrayList<Integer>();
-        if(root == null) return ans;
-        
-        Map<TreeNode, List<TreeNode>> graph = new HashMap<TreeNode, List<TreeNode>>();
+        // sanity check
+        if (root == null)
+            return ans;
+
+        Map<TreeNode, List<TreeNode>> graph = new HashMap<>();
         buildForest(root, graph);
-        
-        Set<TreeNode> visited = new HashSet<TreeNode>();
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+
+        Deque<TreeNode> queue = new ArrayDeque<>();
         queue.add(target);
-        visited.add(target);
-        
-        while(!queue.isEmpty() && K > 0){
+
+        Set<TreeNode> seen = new HashSet<>();
+        seen.add(target);
+
+        int steps = 0;
+        while (!queue.isEmpty()) {
             final int SIZE = queue.size();
-            K--;
-            
-            for(int i = 0; i < SIZE; i++){
+            if (steps++ == K)
+                break;
+
+            for (int i = 0; i < SIZE; i++) {
                 TreeNode cur = queue.poll();
-                
-                if(graph.containsKey(cur)){
-                    for(TreeNode neigbhor : graph.get(cur)){
-                        if(visited.contains(neigbhor)) continue;
-                        visited.add(neigbhor);
-                        queue.add(neigbhor);
-                    }
+
+                if (!graph.containsKey(cur))
+                    continue;
+                for (TreeNode neigbhor : graph.get(cur)) {
+                    if (!seen.add(neigbhor))
+                        continue;
+                    queue.add(neigbhor);
                 }
             }
         }
-        
-        while(!queue.isEmpty()) ans.add(queue.poll().val);
-        
+
+        while (!queue.isEmpty())
+            ans.add(queue.poll().val);
+
         return ans;
     }
-    
-    private void buildForest(TreeNode node, Map<TreeNode, List<TreeNode>> graph){
-        
-        if(node.left != null){
-            graph.putIfAbsent(node.left, new ArrayList<TreeNode>());
+
+    private void buildForest(TreeNode node, Map<TreeNode, List<TreeNode>> graph) {
+
+        if (node.left != null) {
+            graph.putIfAbsent(node.left, new ArrayList<>());
             graph.get(node.left).add(node);
-            graph.putIfAbsent(node, new ArrayList<TreeNode>());
+            graph.putIfAbsent(node, new ArrayList<>());
             graph.get(node).add(node.left);
-            
+
             buildForest(node.left, graph);
         }
-        
-        if(node.right != null){
-            graph.putIfAbsent(node.right, new ArrayList<TreeNode>());
+
+        if (node.right != null) {
+            graph.putIfAbsent(node.right, new ArrayList<>());
             graph.get(node.right).add(node);
-            graph.putIfAbsent(node, new ArrayList<TreeNode>());
+            graph.putIfAbsent(node, new ArrayList<>());
             graph.get(node).add(node.right);
-            
+
             buildForest(node.right, graph);
         }
     }
