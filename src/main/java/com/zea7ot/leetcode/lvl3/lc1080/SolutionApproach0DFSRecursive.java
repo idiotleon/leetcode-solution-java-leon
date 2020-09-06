0bbe7da0b12040344cda448791c20a550e7e4c94
Leon <yanglyu.leon.7@gmail.com>
2020-09-06 14:08:20 -0700

    reviews

--- src/main/java/com/zea7ot/leetcode/lvl3/lc1080/SolutionApproach0DFSRecursive.java
+++ src/main/java/com/zea7ot/leetcode/lvl3/lc1080/SolutionApproach0DFSRecursive.java
@@ -11,26 +11,30 @@ package com.zea7ot.leetcode.lvl3.lc1080;
 
 import com.zea7ot.utils.data_structure.tree.TreeNode;
 
-public class SolutionApproach0DFS {
+public class SolutionApproach0DFSRecursive {
     public TreeNode sufficientSubset(TreeNode root, int limit) {
-        return dfs(root, limit, 0);
+        return dfs(root, 0, limit);
     }
-    
-    private TreeNode dfs(TreeNode node, int limit, int sum){
-        if(node == null) return null;
-        
+
+    private TreeNode dfs(TreeNode node, int sum, final int LIMIT) {
+        if (node == null)
+            return null;
+
         sum += node.val;
-        
-        if(isLeaf(node)) return sum >= limit ? node : null;
-        
-        node.left = dfs(node.left, limit, sum);
-        node.right = dfs(node.right, limit, sum);
-        
+
+        if (isLeaf(node))
+            return sum >= LIMIT ? node : null;
+
+        node.left = dfs(node.left, sum, LIMIT);
+        node.right = dfs(node.right, sum, LIMIT);
+
         return isLeaf(node) ? null : node;
     }
-    
-    private boolean isLeaf(TreeNode node){
-        if(node == null) return false;
+
+    private boolean isLeaf(TreeNode node) {
+        if (node == null)
+            return false;
+
         return node.left == null && node.right == null;
     }
 }
