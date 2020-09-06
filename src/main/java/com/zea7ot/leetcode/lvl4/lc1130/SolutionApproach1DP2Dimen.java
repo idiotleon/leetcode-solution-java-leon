df12d1589a421cc47a8040ab7cd72f10fb99035c
Leon <yanglyu.leon.7@gmail.com>
2020-09-06 13:43:26 -0700

    reviews

--- src/main/java/com/zea7ot/leetcode/lvl4/lc1130/SolutionApproach1DP2Dimen.java
+++ src/main/java/com/zea7ot/leetcode/lvl4/lc1130/SolutionApproach1DP2Dimen.java
@@ -1,8 +1,8 @@
 /**
  * https://leetcode.com/problems/minimum-cost-tree-from-leaf-values/
  * 
- * Time Complexity: O(N ^ 3)
- * Space Complexity: O(N ^ 2)
+ * Time Complexity:     O(N ^ 3)
+ * Space Complexity:    O(N ^ 2)
  * 
  * dp[i, j] = dp[i, k] + dp[k + 1, j] + max(nums[i, k]) * max(nums[k + 1, j])
  * 
@@ -12,35 +12,34 @@
  */
 package com.zea7ot.leetcode.lvl4.lc1130;
 
-public class SolutionApproach1DP2D {
+public class SolutionApproach1DP2Dimen {
     public int mctFromLeafValues(int[] nums) {
         final int N = nums.length;
         int[][] dp = new int[N][N];
-        
-        for(int level = 2; level <= N; level++){
-            for(int i = 0; i <= N - level; i++){
+
+        for (int level = 2; level <= N; level++) {
+            for (int i = 0; i <= N - level; i++) {
                 int j = i + level - 1;
-                for(int k = i; k < j; k++){
-                    if(dp[i][j] != 0){
-                        dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k + 1][j] 
-                                            + (getMax(nums, i, k) * getMax(nums, k + 1, j)));
-                    }else{
-                        dp[i][j] = dp[i][k] + dp[k + 1][j] 
-                            + (getMax(nums, i, k) * getMax(nums, k + 1, j));
+                for (int k = i; k < j; k++) {
+                    if (dp[i][j] != 0) {
+                        dp[i][j] = Math.min(dp[i][j],
+                                dp[i][k] + dp[k + 1][j] + (getMax(nums, i, k) * getMax(nums, k + 1, j)));
+                    } else {
+                        dp[i][j] = dp[i][k] + dp[k + 1][j] + (getMax(nums, i, k) * getMax(nums, k + 1, j));
                     }
                 }
             }
         }
-        
+
         return dp[0][N - 1];
     }
-    
-    private int getMax(int[] nums, int i, int j){
+
+    private int getMax(int[] nums, int i, int j) {
         int max = Integer.MIN_VALUE;
-        for(int k = i; k <= j; k++){
+        for (int k = i; k <= j; ++k) {
             max = Math.max(max, nums[k]);
         }
-        
+
         return max;
     }
 }
