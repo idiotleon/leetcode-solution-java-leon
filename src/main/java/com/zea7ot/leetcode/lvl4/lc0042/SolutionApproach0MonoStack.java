509f905be090bdd2842756184dea6229dcac48e5
Leon <yanglyu.leon.7@gmail.com>
2020-09-09 18:31:36 -0700

    reviews

--- src/main/java/com/zea7ot/leetcode/lvl4/lc0042/SolutionApproach0MonoStack.java
+++ src/main/java/com/zea7ot/leetcode/lvl4/lc0042/SolutionApproach0MonoStack.java
@@ -5,39 +5,41 @@
  * Space Complexity:    O(N)
  * 
  * to maintain a "non-increasing" stack:
- * 1. to contain indexes, instead of heights/values
- * 2. to maintain a non-increasing order of heights in the stack
+ *  1. to contain indexes, instead of heights/values
+ *  2. to maintain a non-increasing order of heights in the stack
  * 
  * in another word, the stack always keeps 
- * the (index of) left taller (compare to cur) column
+ *  the (index of) left taller (compare to cur) column
  * 
  * in another word, the stack.peek() always has (the index of) 
- * the most immediate left taller column 
+ *  the most immediate left taller column 
  * 
  * to keep checking out once an increase of height has been discovered,
- * till the leftmost taller column (of the entire height array).
+ *  till the leftmost taller column (of the entire height array).
  */
 package com.zea7ot.leetcode.lvl4.lc0042;
 
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class SolutionApproach0MonoStack {
     public int trap(int[] height) { // [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]
         // sanity check
-        if(height == null || height.length < 2) return 0;
+        if (height == null || height.length < 2)
+            return 0;
 
         final int N = height.length;
-        Stack<Integer> stack = new Stack<Integer>();
+        Deque<Integer> stack = new ArrayDeque<>();
         int water = 0, idx = 0;
 
-        while(idx < N){
+        while (idx < N) {
             // as long as the incoming height is non-increasing
-            if(stack.isEmpty() || height[idx] <= height[stack.peek()]){
+            if (stack.isEmpty() || height[idx] <= height[stack.peek()]) {
                 // to push it to the stack as candidates as left tallers columns
                 stack.push(idx++);
-            }else{  // once a concave has been found (meaning height starts to increase)
-                int idxConcave = stack.pop();  // the bottom (of the concave)
-                if(!stack.isEmpty()){
+            } else { // once a concave has been found (meaning height starts to increase)
+                int idxConcave = stack.pop(); // the bottom (of the concave)
+                if (!stack.isEmpty()) {
                     // to find the smaller height of the left and right sides
                     int minHeight = Math.min(height[stack.peek()], height[idx]);
                     // to calculate the area
@@ -45,7 +47,7 @@ public class SolutionApproach0MonoStack {
                 }
             }
         }
-        
+
         return water;
     }
 }
--- src/main/java/com/zea7ot/leetcode/lvl4/lc0042/SolutionApproach0TwoPointers1.java
+++ src/main/java/com/zea7ot/leetcode/lvl4/lc0042/SolutionApproach0TwoPointers1.java
@@ -6,28 +6,31 @@
  */
 package com.zea7ot.leetcode.lvl4.lc0042;
 
-public class SolutionApproach0TwoPtrs1 {
+public class SolutionApproach0TwoPointers1 {
     public int trap(int[] height) {
         // sanity check
-        if(height == null || height.length == 0) return 0;
+        if (height == null || height.length < 2)
+            return 0;
 
         int left = 0, right = height.length - 1;
         int ans = 0;
         int leftMax = 0, rightMax = 0;
-        
-        while(left < right)
-            if(height[left] < height[right]){
-                if(height[left] >= leftMax) 
+
+        while (left < right)
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax)
                     leftMax = height[left];
-                else ans += (leftMax - height[left]);
+                else
+                    ans += (leftMax - height[left]);
                 ++left;
-            }else{
-                if(height[right] >= rightMax)
+            } else {
+                if (height[right] >= rightMax)
                     rightMax = height[right];
-                else ans += (rightMax - height[right]);
+                else
+                    ans += (rightMax - height[right]);
                 --right;
             }
-        
+
         return ans;
     }
 }
