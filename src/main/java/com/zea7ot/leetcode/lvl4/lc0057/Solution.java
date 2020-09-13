5071cdc1d26d40bc6e0ba1a3f0492536d234d972
Leon <yanglyu.leon.7@gmail.com>
2020-09-13 10:21:57 -0700

    reviews

--- src/main/java/com/zea7ot/leetcode/lvl4/lc0057/Solution.java
+++ src/main/java/com/zea7ot/leetcode/lvl4/lc0057/Solution.java
@@ -1,28 +1,37 @@
 /**
  * https://leetcode.com/problems/insert-interval/
+ * 
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(N)
  */
 package com.zea7ot.leetcode.lvl4.lc0057;
 
 import java.util.ArrayList;
 import java.util.List;
 
-class Solution{
+class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> res = new ArrayList<>();
-        for(int[] interval : intervals)
-            if(newInterval == null || interval[1] < newInterval[0])
-                res.add(interval);
-            else if(interval[0] > newInterval[1]){
-                res.add(newInterval);
-                res.add(interval);
+        // sanity check
+        if (intervals == null || intervals.length == 0)
+            return new int[0][2];
+
+        List<int[]> ans = new ArrayList<>();
+        for (int[] interval : intervals) {
+            if (newInterval == null || interval[1] < newInterval[0])
+                ans.add(interval);
+            else if (interval[0] > newInterval[1]) {
+                ans.add(newInterval);
+                ans.add(interval);
                 newInterval = null;
-            }else{
+            } else {
                 newInterval[0] = Math.min(newInterval[0], interval[0]);
                 newInterval[1] = Math.max(newInterval[1], interval[1]);
             }
-        
-        if(newInterval != null) res.add(newInterval);
-        
-        return res.toArray(new int[res.size()][]);
+        }
+
+        if (newInterval != null)
+            ans.add(newInterval);
+
+        return ans.toArray(new int[ans.size()][]);
     }
 }
