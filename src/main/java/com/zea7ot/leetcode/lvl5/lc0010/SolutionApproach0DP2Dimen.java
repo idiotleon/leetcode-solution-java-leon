3ff1bf82e7b084fc26ab0019586ac15dd2433759
Leon <yanglyu.leon.7@gmail.com>
2020-09-19 13:18:10 -0700

    reviews

--- src/main/java/com/zea7ot/leetcode/lvl5/lc0010/SolutionApproach0DP2Dimen.java
+++ src/main/java/com/zea7ot/leetcode/lvl5/lc0010/SolutionApproach0DP2Dimen.java
@@ -4,8 +4,18 @@
  * Time Complexity:     O(LEN_S * LEN_P)
  * Space Complexity:    O(LEN_S * LEN_P)
  * 
+ * 
+ * case 1: if CHS_P[idxP] == CHS_S[idxS] :  dp[idxS][idxP] = dp[idxS - 1][idxP - 1];
+ * case 2: if CHS_P[idxP] == '.' : dp[idxS][idxP] = dp[idxS - 1][idxP - 1];
+ * case 3: if CHS_P[idxP] == '*': 
+ *  3.1: if CHS_P[idxP - 1] != CHS_S[idxS] : dp[idxS][idxP] = dp[idxS][idxP - 2]  // in this case, a* only counts as empty
+ *  3.2: if CHS_P[idxP - 1] == CHS_S[idxS] or CHS_P[idxP - 1] == '.':
+ *             dp[idxS][idxP] = dp[idxS - 1][idxP]   // in this case, a* counts as multiple a 
+ *          or dp[idxS][idxP] = dp[idxS][idxP - 1]   // in this case, a* counts as single a
+ *          or dp[idxS][idxP] = dp[idxS][idxP - 2]   // in this case, a* counts as empty
+ * 
  * References:
- *  https://www.youtube.com/watch?v=l3hda49XcDE&t=389s
+ *  https://leetcode.com/problems/regular-expression-matching/discuss/5651/Easy-DP-Java-Solution-with-detailed-Explanation
  */
 package com.zea7ot.leetcode.lvl5.lc0010;
 
@@ -20,26 +30,29 @@ public class SolutionApproach0DP2Dimen {
         boolean[][] dp = new boolean[LEN_S + 1][LEN_P + 1];
         dp[0][0] = true;
 
-        for (int col = 1; col <= LEN_P; ++col)
-            if (CHS_P[col - 1] == PLACE_HOLDER_MULTIPLE)
-                dp[0][col] = dp[0][col - 2];
+        for (int idxP = 0; idxP < LEN_P; ++idxP) {
+            if (CHS_P[idxP] == PLACE_HOLDER_MULTIPLE && dp[0][idxP - 1]) {
+                dp[0][idxP + 1] = true;
+            }
+        }
 
-        for (int idxS = 1; idxS <= LEN_S; ++idxS) {
-            for (int idxP = 1; idxP <= LEN_P; ++idxP) {
-                if (CHS_P[idxP - 1] == PLACE_HOLDER_SINGLE || CHS_P[idxP - 1] == CHS_S[idxS - 1]) {
-                    dp[idxS][idxP] = dp[idxS - 1][idxP - 1];
-                } else if (CHS_P[idxP - 1] == PLACE_HOLDER_MULTIPLE) {
-                    // 0 occurrence: dp[i][j - 2]
-                    dp[idxS][idxP] = dp[idxS][idxP - 2];
-                    // 1 or more occurrences:
-                    // dp[idxS - 1][idxP] if CHS[idxS] == CHS_P[idxP - 1] || CHS_P[idxP - 1] ==
-                    // PLACE_HOLDER_SINGLE
-                    // (str[idxS] == pattern[idxP - 1] || pattern[idxP - 1] == PLACE_HOLDER_SINGLE)
+        for (int idxS = 0; idxS < LEN_S; ++idxS) {
+            for (int idxP = 0; idxP < LEN_P; ++idxP) {
+                if (CHS_P[idxP] == PLACE_HOLDER_SINGLE) {
+                    dp[idxS + 1][idxP + 1] = dp[idxS][idxP];
+                }
 
-                    if (CHS_P[idxP - 2] == PLACE_HOLDER_SINGLE || CHS_P[idxP - 2] == CHS_S[idxS - 1])
-                        dp[idxS][idxP] = dp[idxS][idxP] || dp[idxS - 1][idxP];
-                } else
-                    dp[idxS][idxP] = false;
+                if (CHS_P[idxP] == CHS_S[idxS]) {
+                    dp[idxS + 1][idxP + 1] = dp[idxS][idxP];
+                }
+
+                if (CHS_P[idxP] == PLACE_HOLDER_MULTIPLE) {
+                    if (CHS_P[idxP - 1] != CHS_S[idxS] && CHS_P[idxP - 1] != PLACE_HOLDER_SINGLE) {
+                        dp[idxS + 1][idxP + 1] = dp[idxS + 1][idxP - 1];
+                    } else {
+                        dp[idxS + 1][idxP + 1] = (dp[idxS + 1][idxP] || dp[idxS][idxP + 1] || dp[idxS + 1][idxP - 1]);
+                    }
+                }
             }
         }
 
