eab019b27194b325ff40215a96a7a86bc480b912
Leon <yanglyu.leon.7@gmail.com>
2020-09-19 22:34:05 -0700

    reviews

---
+++ src/main/java/com/zea7ot/leetcode/lvl3/lc0117/SolutionApproach0BFS1.java
@@ -0,0 +1,43 @@
+/**
+ * https://leetcode.com/problems/populating-next-right-pointers-in-each-node-ii/
+ * 
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(W)
+ */
+package com.zea7ot.leetcode.lvl3.lc0117;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class SolutionApproach0BFS1 {
+    public Node connect(Node root) {
+        // sanity check
+        if (root == null)
+            return null;
+
+        Deque<Node> queue = new ArrayDeque<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            final int SIZE = queue.size();
+
+            for (int i = 0; i < SIZE; ++i) {
+                Node cur = queue.poll();
+
+                if (i < SIZE - 1) {
+                    cur.next = queue.peekFirst();
+                }
+
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+        }
+
+        return root;
+    }
+}
