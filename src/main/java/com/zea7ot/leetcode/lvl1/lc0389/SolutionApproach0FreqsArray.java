c3da3865ba681aa26bfdc8234987eda4d47a2956
Leon <yanglyu.leon.7@gmail.com>
2020-09-24 10:25:28 -0700

    lc0389: Find the Difference

---
+++ src/main/java/com/zea7ot/leetcode/lvl1/lc0389/SolutionApproach0FreqsArray.java
@@ -0,0 +1,24 @@
+/**
+ * @author: Leon
+ * https://leetcode.com/problems/find-the-difference/
+ * 
+ * Time Complexity:     O(LEN_S) + O(LEN_T) ~ O(LEN_T)
+ * Space Complexity:    O(1) / O(L)
+ */
+package com.zea7ot.leetcode.lvl1.lc0389;
+
+public class SolutionApproach0FreqsArray {
+    public char findTheDifference(String s, String t) {
+        final int[] FREQS = new int[26];
+        for (final char CH : s.toCharArray()) {
+            ++FREQS[CH - 'a'];
+        }
+
+        for (final char CH : t.toCharArray()) {
+            if (--FREQS[CH - 'a'] < 0)
+                return CH;
+        }
+
+        return '#';
+    }
+}
