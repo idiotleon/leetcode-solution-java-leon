300b4374b4203cb4263dd36ce43c401d61eeb404
Leon <yanglyu.leon.7@gmail.com>
2020-10-01 21:35:20 -0700

    lc0933: Number of Recent Calls

---
+++ src/main/java/com/zea7ot/leetcode/ood/lvl2/lc0933/SolutionApproach1Queue.java
@@ -0,0 +1,32 @@
+/**
+ * https://leetcode.com/problems/number-of-recent-calls/
+ * 
+ * Time Complexity:     O(min(N, 3000))
+ *  N, number of pings
+ * 
+ * Space Complexity:    O(min(N, 3000))
+ * 
+ * References:
+ *  https://leetcode.com/problems/number-of-recent-calls/discuss/189239/JavaPython-3-Five-solutions%3A-TreeMap-TreeSet-ArrayList-Queue-Circular-List.
+ */
+package com.zea7ot.leetcode.ood.lvl2.lc0933;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class SolutionApproach1Queue {
+    private Deque<Integer> queue;
+
+    public SolutionApproach1Queue() {
+        this.queue = new ArrayDeque<>();
+    }
+
+    public int ping(int t) {
+        queue.offer(t);
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+
+        return queue.size();
+    }
+}
