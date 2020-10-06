677e3ede0858aea94443dbbfac7afba4595d9f9f
Leon <yanglyu.leon.7@gmail.com>
2020-10-06 16:23:49 -0700

    review

--- src/main/java/com/zea7ot/leetcode/lvl3/lc0503/SolutionApproach0MonoStack.java
+++ src/main/java/com/zea7ot/leetcode/lvl3/lc0503/SolutionApproach0MonoStack.java
@@ -5,36 +5,37 @@
  * Space Complexity:    O(N)
  * 
  * to maintain a "non-increasing" stack
- * 1. the contents are indexes, instead of values
- * 2. the values/elements, accessed by those indexes, are non-increasing
+ *  1. the contents are indexes, instead of values
+ *  2. the values/elements, accessed by those indexes, are non-increasing
  */
 package com.zea7ot.leetcode.lvl3.lc0503;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.Deque;
-import java.util.LinkedList;
 
 public class SolutionApproach0MonoStack {
     public int[] nextGreaterElements(int[] nums) {
         // sanity check
-        if(nums == null || nums.length == 0) return new int[0];
-        
+        if (nums == null || nums.length == 0)
+            return new int[0];
+
         final int N = nums.length;
         int[] ans = new int[N];
         Arrays.fill(ans, -1);
-        
-        Deque<Integer> idxStack = new LinkedList<Integer>();
-        
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
         int idx = 0;
-        while(idx < 2 * N){
-            while(!idxStack.isEmpty() && nums[idxStack.peek()] < nums[idx % N]){
-                ans[idxStack.pop()] = nums[idx % N];
+        while (idx < 2 * N) {
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[idx % N]) {
+                ans[stack.pop()] = nums[idx % N];
             }
-            
-            idxStack.push(idx % N);
+
+            stack.push(idx % N);
             idx++;
         }
-        
+
         return ans;
     }
 }
