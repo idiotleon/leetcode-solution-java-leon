2b49cef47cb563114fc902086b7486211d7b821a
Leon <yanglyu.leon.7@gmail.com>
2020-10-13 09:53:31 -0700

    lc1602: Find Nearest Right Node in Binary Tree

---
+++ src/main/java/com/zea7ot/leetcode/lvl2/lc1602/SolutionApproach0BFS.java
@@ -0,0 +1,48 @@
+/**
+ * @author: Leon
+ * https://leetcode.com/problems/find-nearest-right-node-in-binary-tree/
+ * 
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(W)
+ */
+package com.zea7ot.leetcode.lvl2.lc1602;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+import com.zea7ot.utils.data_structure.tree.TreeNode;
+
+public class SolutionApproach0BFS {
+    public TreeNode findNearestRightNode(TreeNode root, TreeNode u) {
+        if (root == u)
+            return null;
+
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            final int SIZE = queue.size();
+
+            for (int i = 0; i < SIZE; ++i) {
+                TreeNode cur = queue.poll();
+
+                if (cur == u) {
+                    if (i < SIZE - 1)
+                        return queue.peek();
+                    else
+                        return null;
+                }
+
+                if (cur.left != null) {
+                    queue.offer(cur.left);
+                }
+
+                if (cur.right != null) {
+                    queue.offer(cur.right);
+                }
+            }
+        }
+
+        return null;
+    }
+}
---
+++ src/main/java/com/zea7ot/leetcode/lvl2/lc1602/SolutionApproach0MorrisPreorder.java
@@ -0,0 +1,62 @@
+/**
+ * https://leetcode.com/problems/find-nearest-right-node-in-binary-tree/
+ * 
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(1)
+ * 
+ * References:
+ *  https://leetcode.com/problems/find-nearest-right-node-in-binary-tree/discuss/874398/Java-Morris-Preorder-Time-O(n)-Space-O(1)
+ */
+package com.zea7ot.leetcode.lvl2.lc1602;
+
+import com.zea7ot.utils.data_structure.tree.TreeNode;
+
+public class SolutionApproach0MorrisPreorder {
+    public TreeNode findNearestRightNode(TreeNode root, TreeNode u) {
+        if (root == u)
+            return null;
+
+        int curDepth = 0;
+        int targetDepth = -1;
+
+        TreeNode cur = root;
+        while (cur != null) {
+            if (cur.left == null) {
+                if (cur == u)
+                    targetDepth = curDepth;
+                else if (targetDepth == curDepth)
+                    return cur;
+
+                cur = cur.right;
+                ++curDepth;
+            } else {
+                int delta = 1;
+                TreeNode predecessor = cur.left;
+                while (predecessor.right != cur && predecessor.right != null) {
+                    predecessor = predecessor.right;
+                    ++delta;
+                }
+
+                if (predecessor.right == null) {
+                    predecessor.right = cur;
+
+                    if (cur == u)
+                        targetDepth = curDepth;
+                    else if (targetDepth == curDepth)
+                        return cur;
+
+                    cur = cur.left;
+                    ++curDepth;
+
+                } else {
+                    predecessor.right = null;
+                    cur = cur.right;
+                    curDepth -= delta;
+                }
+            }
+        }
+
+        return null;
+    }
+
+}
