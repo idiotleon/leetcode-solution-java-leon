413a6a83e6a04efdc319f2d80621a4fd6a6f5b93
Leon <yanglyu.leon.7@gmail.com>
2020-10-18 16:14:04 -0700

    reviews

--- src/main/java/com/zea7ot/leetcode/lvl4/lc0329/SolutionApproach1DP2Dimen.java
+++ src/main/java/com/zea7ot/leetcode/lvl4/lc0329/SolutionApproach1DP2Dimen.java
@@ -6,7 +6,6 @@
  * 
  * a bottom-up approach
  * 
- * 
  * References:
  *  https://www.youtube.com/watch?v=yKr4iyQnBpY
  */
@@ -29,30 +28,34 @@ public class SolutionApproach1DP2Dimen {
         final int NR = matrix.length, NC = matrix[0].length;
 
         int[][] dp = new int[NR][NC];
-        for (int[] row : dp)
+        for (int[] row : dp) {
             Arrays.fill(row, 1);
+        }
 
-        List<int[]> cells = new ArrayList<int[]>();
-        for (int row = 0; row < NR; ++row)
-            for (int col = 0; col < NC; ++col)
+        List<int[]> cells = new ArrayList<>();
+        for (int row = 0; row < NR; ++row) {
+            for (int col = 0; col < NC; ++col) {
                 cells.add(new int[] { matrix[row][col], row, col });
+            }
+        }
 
         Collections.sort(cells, (a, b) -> Integer.compare(b[0], a[0]));
 
-        int ans = 0;
+        int longest = 0;
         for (int[] cell : cells) {
             int row = cell[1], col = cell[2];
 
             for (int d = 0; d < 4; ++d) {
-                int r = row + DIRS[d], c = col + DIRS[d + 1];
-                if (r < 0 || r >= NR || c < 0 || c >= NC || matrix[r][c] <= matrix[row][col])
+                int nextRow = row + DIRS[d], nextCol = col + DIRS[d + 1];
+                if (nextRow < 0 || nextRow >= NR || nextCol < 0 || nextCol >= NC
+                        || matrix[nextRow][nextCol] <= matrix[row][col])
                     continue;
-                dp[row][col] = Math.max(dp[row][col], 1 + dp[r][c]);
+                dp[row][col] = Math.max(dp[row][col], 1 + dp[nextRow][nextCol]);
             }
 
-            ans = Math.max(ans, dp[row][col]);
+            longest = Math.max(longest, dp[row][col]);
         }
 
-        return ans;
+        return longest;
     }
 }
