a339b07d3463d6eb6b028c1d3fa17e91a4f6073e
Leon <yanglyu.leon.7@gmail.com>
2020-10-19 21:58:04 -0700

    lc1612: Check if two Expression Trees are Equivalent

---
+++ src/main/java/com/zea7ot/leetcode/lvl3/lc1612/Node.java
@@ -0,0 +1,21 @@
+package com.zea7ot.leetcode.lvl3.lc1612;
+
+class Node {
+   protected char val;
+   protected  Node left;
+   protected  Node right;
+
+   protected   Node() {
+        this.val = ' ';
+    }
+
+    protected   Node(char val) {
+        this.val = val;
+    }
+
+    protected  Node(char val, Node left, Node right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
