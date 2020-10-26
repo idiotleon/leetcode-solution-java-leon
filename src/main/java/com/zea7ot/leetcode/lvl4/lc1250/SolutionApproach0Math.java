fb15be139894db264e41bed884aa732e053a9296
Leon <yanglyu.leon.7@gmail.com>
2020-10-26 13:47:39 -0700

    lc1250: Check if it is a Good Array

---
+++ src/main/java/com/zea7ot/leetcode/lvl4/lc1250/SolutionApproach0Math.java
@@ -0,0 +1,14 @@
+/**
+ * https://leetcode.com/problems/check-if-it-is-a-good-array/
+ *
+ * Time Complexity:     O(`N`)
+ * Space Complexity:    O(`N`)
+ *
+ * References:
+ *  https://leetcode.com/problems/check-if-it-is-a-good-array/discuss/419368/JavaC%2B%2BPython-Chinese-Remainder-Theorem
+ */
+package com.zea7ot.leetcode.lvl4.lc1250;
+
+public class SolutionApproach0Math {
+
+}
