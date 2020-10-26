845b7ead02dec60f7bd15220ffa4407c8d5dcf0b
Leon <yanglyu.leon.7@gmail.com>
2020-10-25 20:19:36 -0700

    reviews

--- src/main/java/com/zea7ot/leetcode/lvl4/lc0947/SolutionApproach0UnionFind.java
+++ src/main/java/com/zea7ot/leetcode/lvl4/lc0947/SolutionApproach0UnionFind.java
@@ -1,8 +1,8 @@
 /**
  * https://leetcode.com/problems/most-stones-removed-with-same-row-or-column/
  * 
- * Time Complexity: O((N ^ 2) * lg(N))
- * Space Complexity: O(N)
+ * Time Complexity:     O((N ^ 2) * lg(N))
+ * Space Complexity:    O(N)
  * 
  * Really like the use of indexes, in stones(int[][]), identifying actual stones/elements
  * 
@@ -14,36 +14,68 @@ package com.zea7ot.leetcode.lvl4.lc0947;
 public class SolutionApproach0UnionFind {
     public int removeStones(int[][] stones) {
         // sanity check
-        if(stones == null || stones.length == 0) return 0;
+        if (stones == null || stones.length == 0)
+            return 0;
 
         final int N = stones.length;
-        int[] roots = new int[N];
-        for(int i = 0; i < N; i++){
-            roots[i] = i;
-        }
-        
-        for(int i = 0; i < N - 1; i++){
-            for(int j = i + 1; j < N; j++){
+        UnionFind uf = new UnionFind(N);
+
+        for (int i = 0; i < N - 1; ++i) {
+            for (int j = i + 1; j < N; ++j) {
                 int[] stone1 = stones[i], stone2 = stones[j];
-                
-                if(stone1[0] == stone2[0] 
-                   || stone1[1] == stone2[1]){
-                    int root1 = find(roots, i), root2 = find(roots, j);
-                    roots[root2] = root1;
+
+                if (stone1[0] == stone2[0] || stone1[1] == stone2[1]) {
+                    uf.union(i, j);
                 }
             }
         }
-        
-        int count = 0;
-        for(int i = 0; i < N; i++){
-            if(roots[i] == i) count++;
-        }
-        
-        return N - count;
+
+        return N - uf.getCount();
     }
-    
-    private int find (int[] roots, int x) {
-        if(x == roots[x]) return x;
-        return roots[x] = find(roots, roots[x]);
+
+    private class UnionFind {
+        private int[] roots;
+        private int[] ranks;
+        private int N;
+
+        private UnionFind(final int N) {
+            this.N = N;
+
+            this.roots = new int[N];
+            this.ranks = new int[N];
+            for (int i = 0; i < N; ++i) {
+                roots[i] = i;
+                ranks[i] = 1;
+            }
+        }
+
+        private void union(int x, int y) {
+            int rootX = find(x), rootY = find(y);
+
+            if (ranks[rootX] > ranks[rootY]) {
+                roots[rootY] = rootX;
+                ++ranks[rootX];
+            } else {
+                roots[rootX] = rootY;
+                ++ranks[rootY];
+            }
+        }
+
+        private int find(int x) {
+            if (x != roots[x]) {
+                roots[x] = find(roots[x]);
+            }
+
+            return roots[x];
+        }
+
+        private int getCount() {
+            int count = 0;
+            for (int i = 0; i < N; ++i) {
+                if (roots[i] == i)
+                    ++count;
+            }
+            return count;
+        }
     }
 }
