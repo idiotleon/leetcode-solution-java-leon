92ee6c33f2adc3efaf5224a42a95599bb9911c03
Leon <yanglyu.leon.7@gmail.com>
2020-10-26 08:45:07 -0700

    reviews

---
+++ src/main/java/com/zea7ot/leetcode/lvl2/lc0844/SolutionApproach0TwoPointers.java
@@ -0,0 +1,32 @@
+/**
+ * @author: Leon
+ * https://leetcode.com/problems/backspace-string-compare/
+ *
+ * Time Complexity:     O(max(`LEN_S`, `LEN_T`))
+ * Space Complexity:    O(max(`LEN_S`, `LEN_T`))
+ */
+package com.zea7ot.leetcode.lvl2.lc0844;
+
+public class SolutionApproach0TwoPointers {
+    private static final char ESCAPE = '#';
+
+    public boolean backspaceCompare(String S, String T) {
+        // not used
+        // final int LEN_S = S.length(), LEN_T = T.length();
+        return parse(S).equals(parse(T));
+    }
+
+    private String parse(String str) {
+        StringBuilder builder = new StringBuilder();
+        for (final char CH : str.toCharArray()) {
+            if (CH == ESCAPE) {
+                if (builder.length() > 0) {
+                    builder.deleteCharAt(builder.length() - 1);
+                }
+            } else
+                builder.append(CH);
+        }
+
+        return builder.toString();
+    }
+}
