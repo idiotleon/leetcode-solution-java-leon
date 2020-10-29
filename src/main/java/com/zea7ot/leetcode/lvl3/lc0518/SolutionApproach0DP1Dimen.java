3d1f9013b2efa568c397110d380d09292097c9df
Leon <yanglyu.leon.7@gmail.com>
2020-10-28 21:52:39 -0700

    reviews

--- src/main/java/com/zea7ot/leetcode/lvl3/lc0518/SolutionApproach0DP1Dimen.java
+++ src/main/java/com/zea7ot/leetcode/lvl3/lc0518/SolutionApproach0DP1Dimen.java
@@ -19,13 +19,13 @@ public class SolutionApproach0DP1Dimen {
     public int change(int amount, int[] coins) {
         int[] dp = new int[amount + 1];
         dp[0] = 1;
-        
-        for(int coin : coins){
-            for(int i = coin; i <= amount; i++){
+
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
                 dp[i] += dp[i - coin];
             }
         }
-        
+
         return dp[amount];
     }
 }
