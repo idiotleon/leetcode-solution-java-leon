def69cad4554a20f673b81f61cfafbf0ab7a5ec7
Leon <yanglyu.leon.7@gmail.com>
2020-11-11 19:32:26 -0800

    reviews

--- src/main/java/com/zea7ot/leetcode/lvl3/lc0785/SolutionApproach0DFS.java
+++ src/main/java/com/zea7ot/leetcode/lvl3/lc0785/SolutionApproach0DFS.java
@@ -1,4 +1,5 @@
 /**
+ * @author: Leon
  * https://leetcode.com/problems/is-graph-bipartite/
  * 
  * Time Complexity:     O(V + E) + O(N) ~ O(2 * N) + O(N) ~ O(N)
@@ -22,7 +23,7 @@ public class SolutionApproach0DFS {
             // to check each node that is not visited, this graph might be disconnected
             if (colors[i] != NOT_YET_COLORED)
                 continue;
-                
+
             if (!isValidColor(i, COLORED, colors, graph))
                 return false;
         }
