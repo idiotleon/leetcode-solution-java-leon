d3da6989716c56dca1e0b5a8db44f90c448adf1c
Leon <yanglyu.leon.7@gmail.com>
2020-11-13 22:57:25 -0800

    reviews

--- src/main/java/com/zea7ot/leetcode/lvl3/lc1109/SolutionApproach0SegmentTree.java
+++ src/main/java/com/zea7ot/leetcode/lvl3/lc1109/SolutionApproach0SegmentTree.java
@@ -1,13 +1,14 @@
 /**
  * https://leetcode.com/problems/corporate-flight-bookings/
  * 
+ * Time Complexity:     O()
+ * Space Complexity:    O()
  * 
  * References:
- *  https://leetcode.com/problems/corporate-flight-bookings/discuss/482356/Java-or-100-speed-100-memory-or-Lazy-propagation
  *  https://leetcode.com/problems/corporate-flight-bookings/discuss/328866/Python-Segment-Tree-with-explanation-(not-optimal-solution)
  */
 package com.zea7ot.leetcode.lvl3.lc1109;
 
-public class SolutionApproach0BinaryIndexedTree {
-    
+public class SolutionApproach0SegmentTree {
+
 }
