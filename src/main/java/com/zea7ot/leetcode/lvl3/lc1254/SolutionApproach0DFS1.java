9200ba0a61ba6b5960a387d79615974ce8a62d20
Leon <yanglyu.leon.7@gmail.com>
2020-11-16 00:32:29 -0800

    reviews

--- src/main/java/com/zea7ot/leetcode/lvl3/lc1254/SolutionApproach0DFS1.java
+++ src/main/java/com/zea7ot/leetcode/lvl3/lc1254/SolutionApproach0DFS1.java
@@ -10,38 +10,40 @@
 package com.zea7ot.leetcode.lvl3.lc1254;
 
 public class SolutionApproach0DFS1 {
-    private static final int[] DIRS = {0, -1, 0, 1, 0};
-    
+    private static final int[] DIRS = { 0, -1, 0, 1, 0 };
+
     public int closedIsland(int[][] grid) {
-        if(grid == null || grid.length == 0 || grid[0].length == 0) return 0;
-        
+        if (grid == null || grid.length == 0 || grid[0].length == 0)
+            return 0;
+
         final int NR = grid.length, NC = grid[0].length;
-        for(int row = 0; row < NR; row++){
-            for(int col = 0; col < NC; col++){
-                if(row == 0 || col == 0 || row == NR - 1 || col == NC - 1){
+        for (int row = 0; row < NR; ++row) {
+            for (int col = 0; col < NC; ++col) {
+                if (row == 0 || col == 0 || row == NR - 1 || col == NC - 1) {
                     fill(row, col, grid);
                 }
             }
         }
-        
+
         int count = 0;
-        for(int row = 0; row < NR; row++){
-            for(int col = 0; col < NC; col++){
-                if(grid[row][col] == 0){
+        for (int row = 0; row < NR; row++) {
+            for (int col = 0; col < NC; col++) {
+                if (grid[row][col] == 0) {
                     count++;
                     fill(row, col, grid);
                 }
             }
         }
-        
+
         return count;
     }
-    
-    private void fill(int row, int col, int[][] grid){
+
+    private void fill(int row, int col, int[][] grid) {
         final int NR = grid.length, NC = grid[0].length;
-        if(row < 0 || row >= NR || col < 0 || col >= NC || grid[row][col] == 1) return;
+        if (row < 0 || row >= NR || col < 0 || col >= NC || grid[row][col] == 1)
+            return;
         grid[row][col] = 1;
-        for(int d = 0; d < 4; d++){
+        for (int d = 0; d < 4; d++) {
             int r = row + DIRS[d], c = col + DIRS[d + 1];
             fill(r, c, grid);
         }
