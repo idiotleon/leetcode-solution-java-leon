7600316f2e4b7ba9c1349a93c4ba620ee7961420
Leon <yanglyu.leon.7@gmail.com>
2020-11-18 23:44:22 -0800

    reviews

--- src/main/java/com/zea7ot/leetcode/lvl4/lc0051/SolutionApproach0Backtrack.java
+++ src/main/java/com/zea7ot/leetcode/lvl4/lc0051/SolutionApproach0Backtrack.java
@@ -5,6 +5,7 @@
  * 
  * References:
  *  
+ *  https://mp.weixin.qq.com/s?__biz=MzAxODQxMDM0Mw==&mid=2247484709&idx=1&sn=1c24a5c41a5a255000532e83f38f2ce4&chksm=9bd7fb2daca0723be888b30345e2c5e64649fc31a00b05c27a0843f349e2dd9363338d0dac61&scene=178&cur_album_id=1318883740306948097#rd
  */
 package com.zea7ot.leetcode.lvl4.lc0051;
 
--- src/main/java/com/zea7ot/leetcode/lvl5/lc0037/SolutionApproach0Backtrack.java
+++ src/main/java/com/zea7ot/leetcode/lvl5/lc0037/SolutionApproach0Backtrack.java
@@ -7,6 +7,7 @@
  * 
  * References:
  *  https://leetcode.com/problems/sudoku-solver/discuss/15911/Less-than-30-line-clean-java-solution-using-DFS
+ *  https://mp.weixin.qq.com/s?__biz=MzAxODQxMDM0Mw==&mid=2247485097&idx=1&sn=a5e82da8646cd8985de6b2b0950de4e2&chksm=9bd7f8a1aca071b7b72e23013bc2a7c528ee913fded9278e2058bc98d7c746e439737d7abb5b&scene=178&cur_album_id=1318883740306948097#rd
  */
 package com.zea7ot.leetcode.lvl5.lc0037;
 
