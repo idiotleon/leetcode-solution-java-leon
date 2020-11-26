55fd94a2660a268c5e3ed02e70bdc41d69a52ce8
Leon <yanglyu.leon.7@gmail.com>
2020-11-25 23:03:43 -0800

    reviews

--- src/main/java/com/zea7ot/leetcode/lvl4/lc0621/SolutionApproach0Greedy.java
+++ src/main/java/com/zea7ot/leetcode/lvl4/lc0621/SolutionApproach0Greedy.java
@@ -13,17 +13,19 @@ public class SolutionApproach0Greedy {
     public int leastInterval(char[] tasks, int n) {
         final int N = tasks.length;
 
-        int[] freq = new int[26];
-        for (char task : tasks)
-            ++freq[task - 'A'];
+        int[] freqs = new int[26];
+        for (char task : tasks) {
+            ++freqs[task - 'A'];
+        }
 
         int maxFreq = 0;
-        for (int count : freq)
-            maxFreq = Math.max(maxFreq, count);
+        for (int freq : freqs) {
+            maxFreq = Math.max(maxFreq, freq);
+        }
 
         int ans = (maxFreq - 1) * (n + 1);
-        for (int count : freq) {
-            if (count == maxFreq) {
+        for (int freq : freqs) {
+            if (freq == maxFreq) {
                 ++ans;
             }
         }
