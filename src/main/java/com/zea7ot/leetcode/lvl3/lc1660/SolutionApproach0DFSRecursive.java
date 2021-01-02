70be1984a057e147f8aae3bca33f2978592abe1d
Leon <yanglyu.leon.7@gmail.com>
2021-01-02 13:32:51 -0800

    reviews

---
+++ src/main/java/com/zea7ot/leetcode/lvl3/lc1660/SolutionApproach0DFSRecursive.java
@@ -0,0 +1,32 @@
+/**
+ * https://leetcode.com/problems/correct-a-binary-tree/
+ * 
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(N)
+ * 
+ * References:
+ *  https://leetcode.com/problems/correct-a-binary-tree/discuss/940769/Java-HashSet.-One-Pass.-Short.
+ */
+package com.zea7ot.leetcode.lvl3.lc1660;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.zea7ot.utils.data_structure.tree.TreeNode;
+
+public class SolutionApproach0DFSRecursive {
+    public TreeNode correctBinaryTree(TreeNode root) {
+        Set<Integer> seen = new HashSet<>();
+        
+        return dfs(root, seen);
+    }
+    
+    private TreeNode dfs(TreeNode node, Set<Integer> seen){
+        if(node == null) return null;
+        if(node.right != null && seen.contains(node.right.val)) return null;
+        seen.add(node.val);
+        node.right = dfs(node.right, seen);
+        node.left = dfs(node.left, seen);
+        return node;
+    }
+}
