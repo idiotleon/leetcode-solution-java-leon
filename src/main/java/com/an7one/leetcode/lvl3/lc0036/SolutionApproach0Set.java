1c0d7faed17c454bcf703a71c0d1fc54f9d9d28b
Leon <yanglyu.leon.7@gmail.com>
2021-08-23 19:57:23 -0700

    refactor

--- src/main/java/com/an7one/leetcode/lvl3/lc0036/SolutionApproach0Set.java
+++ src/main/java/com/an7one/leetcode/lvl3/lc0036/SolutionApproach0Set.java
@@ -1,14 +1,17 @@
-/**
- * https://leetcode.com/problems/valid-sudoku/
- * 
- * Time Complexity:     O(9 * 9) ~ O(1)
- * Space Complexity:    O(9 * 9) ~ O(1)
- */
 package com.an7one.leetcode.lvl3.lc0036;
+
+import com.an7one.util.Constant;
 
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * https://leetcode.com/problems/valid-sudoku/
+ * <p>
+ * Time Complexity:     O(9 * 9) ~ O(1)
+ * Space Complexity:    O(9 * 9) ~ O(1)
+ */
+@SuppressWarnings(Constant.WARNING.UNUSED)
 public class SolutionApproach0Set {
     public boolean isValidSudoku(char[][] board) {
         // sanity check
@@ -18,7 +21,7 @@ public class SolutionApproach0Set {
         final int N = 9;
         final char EMPTY = '.';
 
-        Set<String> seen = new HashSet<String>();
+        Set<String> seen = new HashSet<>();
         for (int row = 0; row < N; ++row) {
             for (int col = 0; col < N; ++col) {
                 if (board[row][col] == EMPTY)
--- src/main/java/com/an7one/leetcode/lvl3/lc0036/SolutionApproach0Set2.java
+++ src/main/java/com/an7one/leetcode/lvl3/lc0036/SolutionApproach0Set2.java
@@ -1,14 +1,17 @@
-/**
- * https://leetcode.com/problems/valid-sudoku/
- * 
- * Time Complexity:     O(9 * 9) ~ O(1)
- * Space Complexity:    O(9 * 9) ~ O(1)
- */
 package com.an7one.leetcode.lvl3.lc0036;
+
+import com.an7one.util.Constant;
 
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * https://leetcode.com/problems/valid-sudoku/
+ * <p>
+ * Time Complexity:     O(9 * 9) ~ O(1)
+ * Space Complexity:    O(9 * 9) ~ O(1)
+ */
+@SuppressWarnings(Constant.WARNING.UNUSED)
 public class SolutionApproach0Set2 {
     public boolean isValidSudoku(char[][] board) {
         // sanity check
@@ -19,9 +22,9 @@ public class SolutionApproach0Set2 {
         final char EMPTY = '.';
 
         for (int i = 0; i < N; i++) {
-            Set<Character> rows = new HashSet<Character>();
-            Set<Character> columns = new HashSet<Character>();
-            Set<Character> cube = new HashSet<Character>();
+            Set<Character> rows = new HashSet<>();
+            Set<Character> columns = new HashSet<>();
+            Set<Character> cube = new HashSet<>();
 
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] != EMPTY && !rows.add(board[i][j]))
