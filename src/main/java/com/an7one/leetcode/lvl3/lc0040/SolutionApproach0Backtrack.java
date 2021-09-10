ddfbff2751d501df13fe78800b38287ab4f49c1d
Leon <yanglyu.leon.7@gmail.com>
2021-09-09 23:31:22 -0700

    refactor

--- src/main/java/com/an7one/leetcode/lvl3/lc0040/SolutionApproach0Backtrack.java
+++ src/main/java/com/an7one/leetcode/lvl3/lc0040/SolutionApproach0Backtrack.java
@@ -1,22 +1,26 @@
-/**
- * https://leetcode.com/problems/combination-sum-ii/
- * 
- * Time Complexity:     O(N ^ target) in the worst case
- * Space Complexity:    O(target) in the worst case
- * 
- * References:
- *  https://leetcode.com/problems/combination-sum/discuss/16502/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partitioning)/435867
- *  https://medium.com/@vasanths294/permutation-combination-subset-time-complexity-eca924e00071
- */
 package com.an7one.leetcode.lvl3.lc0040;
+
+import com.an7one.util.Constant;
+import com.sun.org.apache.bcel.internal.Const;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * https://leetcode.com/problems/combination-sum-ii/
+ * <p>
+ * Time Complexity:     O(N ^ target) in the worst case
+ * Space Complexity:    O(target) in the worst case
+ * <p>
+ * References:
+ * https://leetcode.com/problems/combination-sum/discuss/16502/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partitioning)/435867
+ * https://medium.com/@vasanths294/permutation-combination-subset-time-complexity-eca924e00071
+ */
+@SuppressWarnings(Constant.WARNING.UNUSED)
 public class SolutionApproach0Backtrack {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<List<Integer>> ans = new ArrayList<>();
         // sanity check
         if (candidates == null || candidates.length == 0 || target < 1)
             return ans;
@@ -25,12 +29,12 @@ public class SolutionApproach0Backtrack {
         final int N = candidates.length;
         boolean[] used = new boolean[N];
 
-        backtrack(0, target, new ArrayList<Integer>(), used, candidates, ans);
+        backtrack(0, target, new ArrayList<>(), used, candidates, ans);
         return ans;
     }
 
     private void backtrack(int startIdx, int target, List<Integer> path, boolean[] used, int[] candidates,
-            List<List<Integer>> paths) {
+                           List<List<Integer>> paths) {
         if (target < 0)
             return;
 
