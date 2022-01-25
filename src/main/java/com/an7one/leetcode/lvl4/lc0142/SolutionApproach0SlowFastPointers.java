8b43e70af24511ed321f9842ad0e900210932818
Leon <yanglyu.leon.7@gmail.com>
2022-01-24 20:10:45 -0800

    review

--- src/main/java/com/an7one/leetcode/lvl4/lc0142/SolutionApproach0SlowFastPointers.java
+++ src/main/java/com/an7one/leetcode/lvl4/lc0142/SolutionApproach0SlowFastPointers.java
@@ -1,27 +1,32 @@
 /**
  * https://leetcode.com/problems/linked-list-cycle-ii/
+ * <p>
+ * Time Complexity:     O(L)
+ * Space Complexity:    O(1)
  */
 package com.an7one.leetcode.lvl4.lc0142;
 
+import com.an7one.util.Constant;
 import com.an7one.util.data_structure.linkedlist.ListNode;
 
-public class SolutionApproachSlowFastPointers {
+@SuppressWarnings(Constant.WARNING.UNUSED)
+public class SolutionApproach0SlowFastPointers {
     public ListNode detectCycle(ListNode head) {
         ListNode slow = head, fast = head;
-        while(fast != null && fast.next != null){
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
-            
-            if(slow == fast){
-                while(head != slow){
+
+            if (slow == fast) {
+                while (head != slow) {
                     head = head.next;
                     slow = slow.next;
                 }
-                
+
                 return slow;
             }
         }
-        
+
         return null;
     }
 }
--- src/main/java/com/an7one/leetcode/lvl4/lc0142/SolutionApproach1HashMap.java
+++ src/main/java/com/an7one/leetcode/lvl4/lc0142/SolutionApproach1HashMap.java
@@ -6,20 +6,21 @@ package com.an7one.leetcode.lvl4.lc0142;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.an7one.util.Constant;
 import com.an7one.util.data_structure.linkedlist.ListNode;
 
-public class SolutionApproachHashMap {
+@SuppressWarnings(Constant.WARNING.UNUSED)
+public class SolutionApproach1HashMap {
     public ListNode detectCycle(ListNode head) {
-        Set<ListNode> visited = new HashSet<ListNode>();
-        
+        Set<ListNode> visited = new HashSet<>();
+
         ListNode node = head;
-        while(node != null){
-            if(visited.contains(node))
-                return node;
+        while (node != null) {
+            if (visited.contains(node)) return node;
             visited.add(node);
             node = node.next;
         }
-        
+
         return null;
     }
 }
