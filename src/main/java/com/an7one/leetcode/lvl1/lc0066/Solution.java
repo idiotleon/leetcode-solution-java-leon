70c3b294dca97b86485f7c0dfa916d80998861dc
Leon <yanglyu.leon.7@gmail.com>
2022-04-17 23:46:28 -0700

    review

--- src/main/java/com/an7one/leetcode/lvl1/lc0066/Solution.java
+++ src/main/java/com/an7one/leetcode/lvl1/lc0066/Solution.java
@@ -1,15 +1,22 @@
-/**
- * https://leetcode.com/problems/plus-one/
- */
 package com.an7one.leetcode.lvl1.lc0066;
 
+import com.an7one.util.Constant;
+
+/**
+ * @author: Leon
+ * <a href="https://leetcode.com/problems/plus-one/">source</a>
+ *
+ * Time Complexity:     O(`N`)
+ * Space Complexity:    O(1)
+ */
+@SuppressWarnings(Constant.WARNING.UNUSED)
 public class Solution {
     public int[] plusOne(int[] digits) {
         final int N = digits.length;
         
         for(int i = N - 1; i >= 0; i--){
             if(digits[i] < 9){
-                digits[i]++;
+                ++digits[i];
                 return digits;
             }
             
