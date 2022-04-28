43df97664341fe2df9847a6524eeaf70a2ab8018
Leon <yanglyu.leon.7@gmail.com>
2022-04-27 23:53:11 -0700

    refactor

--- src/main/java/com/an7one/leetcode/lvl1/lc0172/Solution.java
+++ src/main/java/com/an7one/leetcode/lvl1/lc0172/Solution.java
@@ -1,12 +1,15 @@
-/**
- * https://leetcode.com/problems/factorial-trailing-zeroes/
- * 
- * Time Complexity: O(logn)
- * 
- * Space Complexity: O(1)
- */
 package com.an7one.leetcode.lvl1.lc0172;
 
+import com.an7one.util.Constant;
+
+/**
+ * @author: Leon
+ * https://leetcode.com/problems/factorial-trailing-zeroes/
+ *
+ * Time Complexity:     O(log`n`)
+ * Space Complexity:    O(1)
+ */
+@SuppressWarnings(Constant.WARNING.UNUSED)
 public class Solution {
     public int trailingZeroes(int n) {
         int zeroCount = 0;
