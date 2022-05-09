a6587c7ede6b3babf3700559790d8a9a68d5455a
Leon <yanglyu.leon.7@gmail.com>
2022-05-08 21:54:34 -0700

    refactor

--- src/main/java/com/an7one/leetcode/lvl4/lc1163/SolutionApproach0SlidingWindow1.java
+++ src/main/java/com/an7one/leetcode/lvl4/lc1163/SolutionApproach0SlidingWindow1.java
@@ -1,14 +1,18 @@
-/**
- * https://leetcode.com/problems/last-substring-in-lexicographical-order/
- * 
- * Time Complexity:     O(L)
- * Space Complexity:    O(1)
- * 
- * References:
- *  https://leetcode.com/problems/last-substring-in-lexicographical-order/discuss/363662/Short-python-code-O(n)-time-and-O(1)-space-with-proof-and-visualization/456308
- */
 package com.an7one.leetcode.lvl4.lc1163;
 
+import com.an7one.util.Constant;
+
+/**
+ * @author: Leon
+ * <a href="https://leetcode.com/problems/last-substring-in-lexicographical-order/">Description</a>
+ *
+ * Time Complexity:     O(`L`)
+ * Space Complexity:    O(1)
+ *
+ * References:
+ *  <a href="https://leetcode.com/problems/last-substring-in-lexicographical-order/discuss/363662/Short-python-code-O(n)-time-and-O(1)-space-with-proof-and-visualization/456308">Short Python Code</a>
+ */
+@SuppressWarnings(Constant.WARNING.UNUSED)
 public class SolutionApproach0SlidingWindow1 {
     public String lastSubstring(String s) {
         // sanity check
@@ -16,22 +20,20 @@ public class SolutionApproach0SlidingWindow1 {
             return "";
 
         final int L = s.length();
-        final char[] CHS = s.toCharArray();
+        final char[] chs = s.toCharArray();
         int lo = 0, hi = 1, k = 0;
 
         while (hi + k < L) {
-            if (CHS[lo + k] == CHS[hi + k]) {
+            if (chs[lo + k] == chs[hi + k]) {
                 ++k;
                 continue;
             }
 
-            if (CHS[lo + k] > CHS[hi + k]) {
-                ++hi;
-            } else {
+            if (chs[lo + k] <= chs[hi + k]) {
                 lo = hi;
-                hi = lo + 1;
             }
 
+            ++hi;
             k = 0;
         }
 
