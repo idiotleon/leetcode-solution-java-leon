ca5b00144c0be38aed2086afd3f7197fc1efb099
Leon <yanglyu.leon.7@gmail.com>
2022-07-05 22:17:33 -0700

    review

--- src/main/java/com/an7one/leetcode/lvl1/lc0509/SolutionApproach0DFSMemo.java
+++ src/main/java/com/an7one/leetcode/lvl1/lc0509/SolutionApproach0DFSMemo.java
@@ -1,27 +1,31 @@
-/**
- * https://leetcode.com/problems/fibonacci-number/
- * 
- * Time Complexity:     O(N)
- * Space Complexity:    O(N)
- * 
- * References:
- *  https://leetcode.com/problems/fibonacci-number/discuss/215992/Java-Solutions
- */
 package com.an7one.leetcode.lvl1.lc0509;
 
+import com.an7one.util.Constant;
+
+/**
+ * @author: Leon
+ * <a href="https://leetcode.com/problems/fibonacci-number/">LC0509</a>
+ * <p>
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(N)
+ * <p>
+ * References:
+ * <a href="https://leetcode.com/problems/fibonacci-number/discuss/215992/Java-Solutions">Leetcode Discussion</a>
+ */
+@SuppressWarnings(Constant.WARNING.UNUSED)
 public class SolutionApproach0DFSMemo {
     public int fib(int N) {
         int[] memo = new int[31];
         return dfs(N, memo);
     }
 
-    private int dfs(int N, int[] memo) {
-        if (N <= 1)
-            return N;
+    private int dfs(int num, int[] memo) {
+        if (num <= 1)
+            return num;
 
-        if (memo[N] != 0)
-            return memo[N];
+        if (memo[num] != 0)
+            return memo[num];
 
-        return dfs(N - 1, memo) + dfs(N - 2, memo);
+        return dfs(num - 1, memo) + dfs(num - 2, memo);
     }
 }
--- src/main/java/com/an7one/leetcode/lvl1/lc0509/SolutionApproach0DP1D.java
+++ src/main/java/com/an7one/leetcode/lvl1/lc0509/SolutionApproach0DP1D.java
@@ -6,22 +6,26 @@ import com.an7one.util.Constant;
  * @author: Leon
  * <a href="https://leetcode.com/problems/fibonacci-number/">LC0509</a>
  * <p>
- * Time Complexity:     O(N ^ 2)
+ * Time Complexity:     O(N)
  * Space Complexity:    O(N)
  * <p>
  * References:
  * <a href="https://leetcode.com/problems/fibonacci-number/discuss/215992/Java-Solutions">LC Discussion</a>
  */
 @SuppressWarnings(Constant.WARNING.UNUSED)
-public class SolutionApproach1DFSNaive {
+public class SolutionApproach0DP1D {
     public int fib(int N) {
-        return dfs(N);
-    }
-
-    private int dfs(int N) {
         if (N <= 1)
             return N;
 
-        return dfs(N - 1) + dfs(N - 2);
+        int[] dp = new int[N + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for (int i = 2; i <= N; ++i) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[N];
     }
 }
