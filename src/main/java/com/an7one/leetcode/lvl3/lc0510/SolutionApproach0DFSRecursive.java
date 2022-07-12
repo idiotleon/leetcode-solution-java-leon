145b7809a61709ade63e6e68aa8f11e534fcaa23
Leon <yanglyu.leon.7@gmail.com>
2022-07-11 21:59:17 -0700

    review

--- src/main/java/com/an7one/leetcode/lvl3/lc0510/SolutionApproach0DFSRecursive.java
+++ src/main/java/com/an7one/leetcode/lvl3/lc0510/SolutionApproach0DFSRecursive.java
@@ -1,14 +1,17 @@
-/**
- * https://leetcode.com/problems/inorder-successor-in-bst-ii/
- * 
- * Time Complexity:     O(H)
- * Space Complexity:    O(1)
- * 
- * References:  
- *  https://leetcode.com/problems/inorder-successor-in-bst-ii/discuss/586196/Java-3-one-liner-functions
- */
 package com.an7one.leetcode.lvl3.lc0510;
 
+import com.an7one.util.Constant;
+
+/**
+ * <a href="https://leetcode.com/problems/inorder-successor-in-bst-ii/">LC0510</a>
+ * <p>
+ * Time Complexity:     O(H)
+ * Space Complexity:    O(1)
+ * <p>
+ * References:
+ * <a href="https://leetcode.com/problems/inorder-successor-in-bst-ii/discuss/586196/Java-3-one-liner-functions">LC Discussion</a>
+ */
+@SuppressWarnings(Constant.WARNING.UNUSED)
 public class SolutionApproach0DFSRecursive {
     public Node inorderSuccessor(Node node) {
         return node.right == null ? traverseUp(node.parent, node.val) : traverseDown(node.right);
--- src/main/java/com/an7one/leetcode/lvl3/lc0510/SolutionApproach0Iteration.java
+++ src/main/java/com/an7one/leetcode/lvl3/lc0510/SolutionApproach0Iteration.java
@@ -1,37 +1,40 @@
-/**
- * https://leetcode.com/problems/inorder-successor-in-bst-ii/
- * 
- * Time Complexity:     O(H)
- * Space Complexity:    O(1)
- * 
- * There are only two cases: 
- *  Case 1 the node has no right node, so successor might be in the parent.
- *      Just to loop through the parent and find the first one bigger than `node`
- *  Case 2 is where the `node` has a right child. 
- *      It doesn't matter if the parent node exist of not. Just to find the successor.
- * 
- * 
- * References:
- *  https://leetcode.com/problems/inorder-successor-in-bst-ii/discuss/231587/Java-find-in-parents-or-find-in-descendents
- *  https://leetcode.com/problems/inorder-successor-in-bst-ii/discuss/231587/Java-find-in-parents-or-find-in-descendents/279202
- *  https://leetcode.com/problems/delete-node-in-a-bst/solution/
- */
 package com.an7one.leetcode.lvl3.lc0510;
 
+import com.an7one.util.Constant;
+
+/**
+ * <a href="https://leetcode.com/problems/inorder-successor-in-bst-ii/">LC0510</a>
+ * <p>
+ * Time Complexity:     O(H)
+ * Space Complexity:    O(1)
+ * <p>
+ * There are only two cases:
+ * Case 1 the node has no right node, so successor might be in the parent.
+ * Just to loop through the parent and find the first one bigger than `node`
+ * Case 2 is where the `node` has a right child.
+ * It doesn't matter if the parent node exist of not. Just to find the successor.
+ * <p>
+ * <p>
+ * References:
+ * <a href="https://leetcode.com/problems/inorder-successor-in-bst-ii/discuss/231587/Java-find-in-parents-or-find-in-descendents">LC Discussion</a>
+ * <a href="https://leetcode.com/problems/inorder-successor-in-bst-ii/discuss/231587/Java-find-in-parents-or-find-in-descendents/279202">LC Discussion</a>
+ * <a href="https://leetcode.com/problems/delete-node-in-a-bst/solution/">LC Solution</a>
+ */
+@SuppressWarnings(Constant.WARNING.UNUSED)
 public class SolutionApproach0Iteration {
     public Node inorderSuccessor(Node node) {
+        Node cur;
         if (node.right == null) {
-            Node cur = node.parent;
+            cur = node.parent;
             while (cur != null && cur.val < node.val) {
                 cur = cur.parent;
             }
-            return cur;
         } else {
-            Node cur = node.right;
+            cur = node.right;
             while (cur.left != null) {
                 cur = cur.left;
             }
-            return cur;
         }
+        return cur;
     }
 }
