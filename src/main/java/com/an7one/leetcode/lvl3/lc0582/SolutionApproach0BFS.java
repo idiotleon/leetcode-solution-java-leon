a050bbbd4b9d55b60fcad48d10860d1ddd77e1a9
Leon <yanglyu.leon.7@gmail.com>
2022-07-18 00:31:31 -0700

    review

--- src/main/java/com/an7one/leetcode/lvl3/lc0582/SolutionApproach0BFS.java
+++ src/main/java/com/an7one/leetcode/lvl3/lc0582/SolutionApproach0BFS.java
@@ -1,10 +1,6 @@
-/**
- * https://leetcode.com/problems/kill-process/
- * 
- * Time Complexity:     O(N)
- * Space Complexity:    O(N)
- */
 package com.an7one.leetcode.lvl3.lc0582;
+
+import com.an7one.util.Constant;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
@@ -15,31 +11,39 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * @author: Leon
+ * <a href="https://leetcode.com/problems/kill-process/">LC0582</a>
+ * <p>
+ * Time Complexity:     O(V + E) ~ O(N)
+ * Space Complexity:    O(V + E) ~ O(N)
+ */
+@SuppressWarnings(Constant.WARNING.UNUSED)
 public class SolutionApproach0BFS {
     public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {
-        List<Integer> ans = new ArrayList<Integer>();
+        final List<Integer> ans = new ArrayList<>();
         // sanity check
         if (pid == null || ppid == null || pid.isEmpty() || ppid.isEmpty() || pid.size() != ppid.size())
             return ans;
 
         final int N = pid.size();
-        Map<Integer, Set<Integer>> graph = new HashMap<Integer, Set<Integer>>();
+        final Map<Integer, Set<Integer>> graph = new HashMap<>();
         for (int i = 0; i < N; ++i) {
             int parent = ppid.get(i), child = pid.get(i);
-            graph.putIfAbsent(parent, new HashSet<Integer>());
+            graph.putIfAbsent(parent, new HashSet<>());
             graph.get(parent).add(child);
         }
 
-        Deque<Integer> queue = new ArrayDeque<>();
-        queue.add(kill);
+        final Deque<Integer> queue = new ArrayDeque<>();
+        queue.addLast(kill);
 
         while (!queue.isEmpty()) {
-            int processId = queue.poll();
+            final int processId = queue.removeFirst();
             ans.add(processId);
 
             if (graph.containsKey(processId)) {
-                for (int process : graph.get(processId)) {
-                    queue.add(process);
+                for (final int process : graph.get(processId)) {
+                    queue.addLast(process);
                 }
             }
         }
--- src/main/java/com/an7one/leetcode/lvl3/lc0582/SolutionApproach0DFSRecursive.java
+++ src/main/java/com/an7one/leetcode/lvl3/lc0582/SolutionApproach0DFSRecursive.java
@@ -1,10 +1,6 @@
-/**
- * https://leetcode.com/problems/kill-process/
- * 
- * Time Complexity:     O(N)
- * Space Complexity:    O(N)
- */
 package com.an7one.leetcode.lvl3.lc0582;
+
+import com.an7one.util.Constant;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -13,18 +9,26 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * @author: Leon
+ * <a href="https://leetcode.com/problems/kill-process/">LC0582</a>
+ * <p>
+ * Time Complexity:     O(V + E) ~ O(N)
+ * Space Complexity:    O(V + E) ~ O(N)
+ */
+@SuppressWarnings(Constant.WARNING.UNUSED)
 public class SolutionApproach0DFSRecursive {
     public List<Integer> killProcess(List<Integer> pid, List<Integer> ppid, int kill) {
-        List<Integer> ans = new ArrayList<Integer>();
-        // santiy check
+        final List<Integer> ans = new ArrayList<>();
+        // sanity check
         if (pid == null || ppid == null || pid.isEmpty() || ppid.isEmpty() || pid.size() != ppid.size())
             return ans;
 
         final int N = pid.size();
-        Map<Integer, Set<Integer>> graph = new HashMap<Integer, Set<Integer>>();
+        final Map<Integer, Set<Integer>> graph = new HashMap<>();
         for (int i = 0; i < N; ++i) {
             int parent = ppid.get(i), child = pid.get(i);
-            graph.putIfAbsent(parent, new HashSet<Integer>());
+            graph.putIfAbsent(parent, new HashSet<>());
             graph.get(parent).add(child);
         }
 
@@ -34,7 +38,7 @@ public class SolutionApproach0DFSRecursive {
         return ans;
     }
 
-    private void dfs(int kill, Map<Integer, Set<Integer>> graph, List<Integer> res) {
+    private void dfs(final int kill, final Map<Integer, Set<Integer>> graph, final List<Integer> res) {
         if (!graph.containsKey(kill))
             return;
 
