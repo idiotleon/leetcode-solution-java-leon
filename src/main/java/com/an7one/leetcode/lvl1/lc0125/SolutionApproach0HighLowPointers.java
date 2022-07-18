2b63103b85897798a4ecabb820e21c6526d7217c
Leon <yanglyu.leon.7@gmail.com>
2022-07-17 23:34:17 -0700

    review

--- src/main/java/com/an7one/leetcode/lvl1/lc0125/SolutionApproach0HighLowPointers.java
+++ src/main/java/com/an7one/leetcode/lvl1/lc0125/SolutionApproach0HighLowPointers.java
@@ -4,19 +4,19 @@ import com.an7one.util.Constant;
 
 /**
  * @author: Leon
- * <a href="https://leetcode.com/problems/valid-palindrome/">valid-palindrome</a>
- *
+ * <a href="https://leetcode.com/problems/valid-palindrome/">LC0125</a>
+ * <p>
  * Time Complexity:     O(`L`)
  * Space Complexity:    O(1) / O(`L`)
  */
 @SuppressWarnings(Constant.WARNING.UNUSED)
-public class SolutionApproach0TwoPointers {
+public class SolutionApproach0HighLowPointers {
     public boolean isPalindrome(String s) {
         // sanity check
         if (s == null || s.isEmpty())
             return true;
         final int L = s.length();
-        char[] chs = s.toCharArray();
+        final char[] chs = s.toCharArray();
         int lo = 0, hi = L - 1;
         while (lo < hi) {
             while (!isAlphanumeric(chs[lo])) {
@@ -25,8 +25,9 @@ public class SolutionApproach0TwoPointers {
                     return true;
             }
 
-            while (!isAlphanumeric(chs[hi]))
+            while (!isAlphanumeric(chs[hi])) {
                 --hi;
+            }
 
             if (Character.toLowerCase(chs[lo++]) != Character.toLowerCase(chs[hi--]))
                 return false;
