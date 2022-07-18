2e605a7b9b193ebad2e5f652b62bd45a55dc1421
Leon <yanglyu.leon.7@gmail.com>
2022-07-17 23:02:23 -0700

    review

--- src/main/java/com/an7one/leetcode/lvl3/lc0103/SolutionApproach0BFS.java
+++ src/main/java/com/an7one/leetcode/lvl3/lc0103/SolutionApproach0BFS.java
@@ -1,49 +1,45 @@
+package com.an7one.leetcode.lvl3.lc0103;
+
+import java.util.*;
+
+import com.an7one.util.Constant;
+import com.an7one.util.data_structure.tree.TreeNode;
+
 /**
- * https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
- * 
+ * @author: Leon
+ * <a href="https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/">LC0103</a>
+ * <p>
  * Time Complexity:     O(N)
  * Space Complexity:    O(W)
  */
-package com.an7one.leetcode.lvl3.lc0103;
-
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
-
-import com.an7one.util.data_structure.tree.TreeNode;
-
+@SuppressWarnings(Constant.WARNING.UNUSED)
 public class SolutionApproach0BFS {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();
+        final List<List<Integer>> ans = new ArrayList<>();
         // sanity check
         if (root == null)
             return ans;
 
-        Deque<TreeNode> queue = new ArrayDeque<>();
-        queue.add(root);
+        final Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.addLast(root);
         int level = 0;
 
         while (!queue.isEmpty()) {
-            final int SIZE = queue.size();
-            LinkedList<Integer> intermediate = new LinkedList<>();
+            final int sizeQ = queue.size();
+            final List<Integer> intermediate = new ArrayList<>();
 
-            for (int i = 0; i < SIZE; ++i) {
-                TreeNode cur = queue.poll();
-                if (level % 2 == 0)
-                    intermediate.addLast(cur.val);
-                else
-                    intermediate.addFirst(cur.val);
+            for (int i = 0; i < sizeQ; ++i) {
+                final TreeNode cur = queue.removeFirst();
+                intermediate.add(cur.val);
 
                 if (cur.left != null)
                     queue.add(cur.left);
                 if (cur.right != null)
                     queue.add(cur.right);
             }
-
+            if (level % 2 != 0) Collections.reverse(intermediate);
             ++level;
-            ans.add(intermediate);
+            ans.add(new ArrayList<>(intermediate));
         }
 
         return ans;
