dafee66ee0409de44a05bba618376af023ac96a5
Leon <yanglyu.leon.7@gmail.com>
2022-08-19 23:25:32 -0700

    review

--- src/main/java/com/an7one/leetcode/lvl4/lc0549/SolutionApproach0Postorder.java
+++ src/main/java/com/an7one/leetcode/lvl4/lc0549/SolutionApproach0Postorder.java
@@ -1,46 +1,49 @@
-/**
- * https://leetcode.com/problems/binary-tree-longest-consecutive-sequence-ii/
- * 
- * Time Complexity:     O(N)
- * Space Compelxity:    O(H)
- * 
- * References:
- *  https://leetcode.com/problems/binary-tree-longest-consecutive-sequence-ii/discuss/101523/Easy-O(n)-Java-Solution-for-2-problems
- *  https://leetcode.com/problems/number-of-longest-increasing-subsequence/discuss/107293/JavaC++-Simple-dp-solution-with-explanation/177757
- */
 package com.an7one.leetcode.lvl4.lc0549;
 
+import com.an7one.util.Constant;
 import com.an7one.util.data_structure.tree.TreeNode;
 
+/**
+ * @author: Leon
+ * <a href="https://leetcode.com/problems/binary-tree-longest-consecutive-sequence-ii/">LC0549</a>
+ * <p>
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(H)
+ * <p>
+ * Reference:
+ * <a href="https://leetcode.com/problems/binary-tree-longest-consecutive-sequence-ii/discuss/101523/Easy-O(n)-Java-Solution-for-2-problems">LC Discussion</a>
+ * <a href="https://leetcode.com/problems/number-of-longest-increasing-subsequence/discuss/107293/JavaC++-Simple-dp-solution-with-explanation/177757">LC Discussion</a>
+ */
+@SuppressWarnings(Constant.WARNING.UNUSED)
 public class SolutionApproach0Postorder {
     private int maxLen;
-    
-    public int longestConsecutive(TreeNode root) {
+
+    public int longestConsecutive(final TreeNode root) {
         this.maxLen = 0;
         postorder(root);
         return maxLen;
     }
-    
-    private int[] postorder(TreeNode node){
-        if(node == null) return new int[2];
-        
-        int[] left = postorder(node.left), right = postorder(node.right);
-        
-        if(node.left != null){
-            if(node.left.val != node.val - 1) left[0] = 0;
-            if(node.left.val != node.val + 1) left[1] = 0;
+
+    private int[] postorder(final TreeNode node) {
+        if (node == null) return new int[2];
+
+        final int[] left = postorder(node.left), right = postorder(node.right);
+
+        if (node.left != null) {
+            if (node.left.val != node.val - 1) left[0] = 0;
+            if (node.left.val != node.val + 1) left[1] = 0;
         }
-        
-        if(node.right != null){
-            if(node.right.val != node.val - 1) right[0] = 0;
-            if(node.right.val != node.val + 1) right[1] = 0;
+
+        if (node.right != null) {
+            if (node.right.val != node.val - 1) right[0] = 0;
+            if (node.right.val != node.val + 1) right[1] = 0;
         }
-        
-        int inc = Math.max(left[0], right[0]) + 1;
-        int dec = Math.max(left[1], right[1]) + 1;
-        
+
+        final int inc = Math.max(left[0], right[0]) + 1;
+        final int dec = Math.max(left[1], right[1]) + 1;
+
         maxLen = Math.max(maxLen, inc + dec - 1);
-        
+
         return new int[]{inc, dec};
     }
 }
--- src/main/java/com/an7one/leetcode/lvl4/lc0549/SolutionApproach0Postorder1.java
+++ src/main/java/com/an7one/leetcode/lvl4/lc0549/SolutionApproach0Postorder1.java
@@ -1,41 +1,44 @@
-/**
- * https://leetcode.com/problems/binary-tree-longest-consecutive-sequence-ii/
- * 
- * Time Complexity:     O(N)
- * Space Compelxity:    O(H)
- * 
- * References:
- *  https://leetcode.com/problems/binary-tree-longest-consecutive-sequence-ii/discuss/101509/Java-recursively-compute-ascending-and-descending-sequence
- *  https://leetcode.com/problems/number-of-longest-increasing-subsequence/discuss/107293/JavaC++-Simple-dp-solution-with-explanation/177757
- */
 package com.an7one.leetcode.lvl4.lc0549;
 
+import com.an7one.util.Constant;
 import com.an7one.util.data_structure.tree.TreeNode;
 
+/**
+ * @author: Leon
+ * <a href="https://leetcode.com/problems/binary-tree-longest-consecutive-sequence-ii/">LC0549</a>
+ * <p>
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(H)
+ * <p>
+ * References:
+ * <a href="https://leetcode.com/problems/binary-tree-longest-consecutive-sequence-ii/discuss/101509/Java-recursively-compute-ascending-and-descending-sequence">LC Discussion</a>
+ * <a href="https://leetcode.com/problems/number-of-longest-increasing-subsequence/discuss/107293/JavaC++-Simple-dp-solution-with-explanation/177757">LC Discussion</a>
+ */
+@SuppressWarnings(Constant.WARNING.UNUSED)
 public class SolutionApproach0Postorder1 {
     private int maxLen;
-    
+
     public int longestConsecutive(TreeNode root) {
         this.maxLen = 0;
         postorder(root);
         return maxLen;
     }
-    
-    private int[] postorder(TreeNode node){
-        if(node == null) return new int[2];
+
+    private int[] postorder(TreeNode node) {
+        if (node == null) return new int[2];
         int inc = 1, dec = 1;
         int[] left = postorder(node.left), right = postorder(node.right);
-        
-        if(node.left != null){
-            if(node.left.val == node.val + 1) inc += left[0];
-            if(node.left.val == node.val - 1) dec += left[1];
+
+        if (node.left != null) {
+            if (node.left.val == node.val + 1) inc += left[0];
+            if (node.left.val == node.val - 1) dec += left[1];
         }
-        
-        if(node.right != null){
-            if(node.right.val == node.val + 1) inc = Math.max(inc, 1 + right[0]);
-            if(node.right.val == node.val - 1) dec = Math.max(dec, 1 + right[1]);
+
+        if (node.right != null) {
+            if (node.right.val == node.val + 1) inc = Math.max(inc, 1 + right[0]);
+            if (node.right.val == node.val - 1) dec = Math.max(dec, 1 + right[1]);
         }
-        
+
         maxLen = Math.max(maxLen, inc + dec - 1);
         return new int[]{inc, dec};
     }
