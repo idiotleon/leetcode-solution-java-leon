4fa541fc75c92df04113c7eb6a8d937b9d01a7cd
Leon <yanglyu.leon.7@gmail.com>
2022-09-03 00:03:24 -0700

    review

--- src/main/java/com/an7one/leetcode/lvl4/lc0298/SolutionApproach0Preorder.java
+++ src/main/java/com/an7one/leetcode/lvl4/lc0298/SolutionApproach0Preorder.java
@@ -1,30 +1,33 @@
+package com.an7one.leetcode.lvl4.lc0298;
+
+import com.an7one.util.Constant;
+import com.an7one.util.data_structure.tree.TreeNode;
+
 /**
- * https://leetcode.com/problems/binary-tree-longest-consecutive-sequence/
- * 
+ * @author: Leon
+ * <a href="https://leetcode.com/problems/binary-tree-longest-consecutive-sequence/">LC0298</a>
+ * <p>
  * Time Complexity:     O(N)
  * Space Complexity:    O(H)
  */
-package com.an7one.leetcode.lvl4.lc0298;
-
-import com.an7one.util.data_structure.tree.TreeNode;
-
+@SuppressWarnings(Constant.WARNING.UNUSED)
 public class SolutionApproach0Preorder {
-    private int max;
-    
+    private int longest;
+
     public int longestConsecutive(TreeNode root) {
         // sanity check !cannot be omitted!
-        if(root == null) return 0;
-        this.max = 0;
+        if (root == null) return 0;
+        this.longest = 0;
         preorder(root, 0, root.val + 1);
-        return max;
+        return longest;
     }
-    
-    private void preorder(TreeNode node, int sequence, int target){
-        if(node == null) return;
-        if(node.val == target) sequence++;
-        else sequence = 1;
-        max = Math.max(max, sequence);
-        preorder(node.left, sequence, node.val + 1);
-        preorder(node.right, sequence, node.val + 1);
+
+    private void preorder(TreeNode node, int len, int target) {
+        if (node == null) return;
+        if (node.val == target) ++len;
+        else len = 1;
+        longest = Math.max(longest, len);
+        preorder(node.left, len, node.val + 1);
+        preorder(node.right, len, node.val + 1);
     }
 }
--- src/main/java/com/an7one/leetcode/lvl4/lc0298/SolutionApproach0Preorder1.java
+++ src/main/java/com/an7one/leetcode/lvl4/lc0298/SolutionApproach0Preorder1.java
@@ -1,28 +1,31 @@
+package com.an7one.leetcode.lvl4.lc0298;
+
+import com.an7one.util.Constant;
+import com.an7one.util.data_structure.tree.TreeNode;
+
 /**
- * https://leetcode.com/problems/binary-tree-longest-consecutive-sequence/
- * 
+ * @author: Leon
+ * <a href="https://leetcode.com/problems/binary-tree-longest-consecutive-sequence/">LC0298</a>
+ * <p>
  * Time Complexity:     O(N)
  * Space Complexity:    O(H)
  */
-package com.an7one.leetcode.lvl4.lc0298;
-
-import com.an7one.util.data_structure.tree.TreeNode;
-
+@SuppressWarnings(Constant.WARNING.UNUSED)
 public class SolutionApproach0Preorder1 {
     public int longestConsecutive(TreeNode root) {
         // sanity check
-        if(root == null) return 0;
-        
+        if (root == null) return 0;
+
         return Math.max(dfs(root.left, 1, root.val + 1), dfs(root.right, 1, root.val + 1));
     }
-    
-    private int dfs(TreeNode node, int sequence, int target){
-        if(node == null) return sequence;
-        
-        sequence = (node.val == target) ? sequence + 1 : 1;
-        int left = dfs(node.left, sequence, node.val + 1);
-        int right = dfs(node.right, sequence, node.val + 1);
-        
-        return Math.max(sequence, Math.max(left, right));
+
+    private int dfs(TreeNode node, int len, int expected) {
+        if (node == null) return len;
+
+        len = (node.val == expected) ? len + 1 : 1;
+        int left = dfs(node.left, len, node.val + 1);
+        int right = dfs(node.right, len, node.val + 1);
+
+        return Math.max(len, Math.max(left, right));
     }
 }
