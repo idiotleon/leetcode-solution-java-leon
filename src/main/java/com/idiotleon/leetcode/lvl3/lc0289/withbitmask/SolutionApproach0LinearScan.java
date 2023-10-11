d3aa7ae679d4ecc25f4f43147b3c9d32000ce098
Leon <yanglyu.leon.7@gmail.com>
2023-10-11 12:02:31 -0700

    refactor

--- src/main/java/com/idiotleon/leetcode/lvl3/lc0289/withbitmask/SolutionApproach0LinearScan.java
+++ src/main/java/com/idiotleon/leetcode/lvl3/lc0289/withbitmask/SolutionApproach0LinearScan.java
@@ -1,27 +1,27 @@
+package com.idiotleon.leetcode.lvl3.lc0289.withbitmask;
+
+import static com.idiotleon.util.Constant.WARNING.UNUSED;
+
 /**
- * https://leetcode.com/problems/game-of-life/
- * 
+ * @author: Leon
+ * <a href="https://leetcode.com/problems/game-of-life/">LC289 Game of Life<w/a>
  * Time Complexity:     O(`NR` * `NC`)
  * Space Complexity:    O(1)
- * 
  * to acquire the current state:    board[row][col] & 1
  * to acquire the next state:       board[row][col] >> 1
- * 
- * References:
- *  https://leetcode.com/problems/game-of-life/discuss/73223/Easiest-JAVA-solution-with-explanation
+ * Reference:
+ * <a href="https://leetcode.com/problems/game-of-life/discuss/73223/Easiest-JAVA-solution-with-explanation">LC</a>
  */
-package com.idiotleon.leetcode.lvl3.lc0289.with_bitmask;
-
+@SuppressWarnings(UNUSED)
 public class SolutionApproach0LinearScan {
     private static final int INITIAL_DEAD = 0;
     private static final int INITIAL_LIVE = 1;
-    private final static int GOING_TO_DIE = 2;
-    private final static int GOING_TO_LIVE = 3;
+    private static final int GOING_TO_REPRODUCE = 2;
+    private static final int GOING_TO_LIVE = 3;
 
     public void gameOfLife(int[][] board) {
         // sanity check
-        if (board == null || board.length == 0)
-            return;
+        if (board == null || board.length == 0) return;
 
         final int NR = board.length, NC = board[0].length;
 
@@ -36,7 +36,7 @@ public class SolutionApproach0LinearScan {
                 }
 
                 if (board[row][col] == INITIAL_DEAD && lives == 3) {
-                    board[row][col] = GOING_TO_DIE; // to make the 2nd bit 1: 00 -> 10
+                    board[row][col] = GOING_TO_REPRODUCE; // to make the 2nd bit 1: 00 -> 10
                 }
             }
         }
