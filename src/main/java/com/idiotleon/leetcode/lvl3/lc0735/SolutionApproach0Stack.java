7baa0f3b141f1a849803d24a4f6bd80f67dcc7f8
Leon <yanglyu.leon.7@gmail.com>
2023-10-26 18:10:25 -0700

    review

--- src/main/java/com/idiotleon/leetcode/lvl3/lc0735/SolutionApproach0Stack.java
+++ src/main/java/com/idiotleon/leetcode/lvl3/lc0735/SolutionApproach0Stack.java
@@ -1,23 +1,26 @@
-/**
- * @author: Leon
- * https://leetcode.com/problems/asteroid-collision/
- * 
- * Time Complexity:     O(N)
- * Space Complexity:    O(N)
- */
 package com.idiotleon.leetcode.lvl3.lc0735;
+
+import com.idiotleon.util.Constant;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+/**
+ * @author: Leon
+ * <a href="https://leetcode.com/problems/asteroid-collision/">LC0735</a>
+ * <p>
+ * Time Complexity:     O(N)
+ * Space Complexity:    O(N)
+ */
+@SuppressWarnings(Constant.WARNING.UNUSED)
 public class SolutionApproach0Stack {
     public int[] asteroidCollision(int[] asteroids) {
         // sanity check
         // final int N = asteroids.length;
 
-        Deque<Integer> stack = new ArrayDeque<>();
+        final Deque<Integer> stack = new ArrayDeque<>();
 
-        for (int asteroid : asteroids) {
+        for (final int asteroid : asteroids) {
             if (asteroid > 0 || stack.isEmpty() || stack.peek() < 0) {
                 stack.push(asteroid);
             } else {
@@ -25,16 +28,17 @@ public class SolutionApproach0Stack {
                     stack.pop();
                 }
 
-                if (stack.isEmpty() || stack.peek() < 0)
+                if (stack.isEmpty() || stack.peek() < 0) {
                     stack.push(asteroid);
-                else if (stack.peek() == -asteroid)
+                } else if (stack.peek() == -asteroid) {
                     stack.pop();
+                }
             }
         }
 
         final int SIZE = stack.size();
         int idx = SIZE - 1;
-        int[] ans = new int[SIZE];
+        final int[] ans = new int[SIZE];
         while (!stack.isEmpty()) {
             ans[idx--] = stack.pop();
         }
